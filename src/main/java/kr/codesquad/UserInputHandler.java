bef58558601c29a9a8aa05e09fb4d11d41af9c71
rohsik2 <rohsik2@gmail.com>
2023-01-02 16:45:25 +0900

    feat(UserInputHandler): making user input function

---
+++ src/main/java/kr/codesquad/UserInputHandler.java
@@ -0,0 +1,24 @@
+package kr.codesquad;
+
+import java.util.Scanner;
+public class UserInputHandler {
+
+    public static int getMoney(){
+        Scanner s1 = new Scanner(System.in);
+        int money =0;
+        while(true){
+            System.out.print("얼마치 로또를 구매하실지 입력해 주세요 : ");
+            try {
+                money = s1.nextInt();
+                if(money % 1000 != 0 || money < 0){
+                    throw new IllegalStateException();
+                }
+                return money;
+            }
+            catch(Exception e){
+                System.out.println("0이상의 숫자를 1000단위로 입력해 주세요. 올바른 숫자형태를 입력해 주세요.");
+            }
+        }
+
+    }
+}
