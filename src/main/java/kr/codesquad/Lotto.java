0c8b193de7a0ace593b32fcf01cca7938fae29e0
ktykty0722 <ktykty0722@naver.com>
2023-01-02 17:05:06 +0900

    로또 구매 금액 입력 후 해당 갯수 만큼 로또 번호 생성

---
+++ src/main/java/kr/codesquad/Lotto.java
@@ -0,0 +1,21 @@
+package kr.codesquad;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Lotto {
+    public ArrayList<Integer> numberList = new ArrayList<>();
+
+    public Lotto() {
+        ArrayList<Integer> allNumber = new ArrayList<>();
+        for(int i=1;i<=45;i++) {
+            allNumber.add(i);
+        }
+        Collections.shuffle(allNumber);
+        ArrayList<Integer> lottoNum = new ArrayList<>();
+        lottoNum.addAll(allNumber.subList(0, 6));
+        Collections.sort(lottoNum);
+        this.numberList.addAll(lottoNum);
+    }
+
+}
--- src/main/java/kr/codesquad/Main.java
+++ src/main/java/kr/codesquad/Main.java
@@ -1,7 +1,20 @@
 package kr.codesquad;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("구입금액을 입력해 주세요.");
+        int money = sc.nextInt();
+        System.out.println(money/1000 + "개를 구매했습니다.");
+        ArrayList<Lotto> allLotto = new ArrayList<>();
+        for(int i=0;i<money/1000;i++) {
+            allLotto.add(new Lotto());
+        }
+
+
     }
 }
