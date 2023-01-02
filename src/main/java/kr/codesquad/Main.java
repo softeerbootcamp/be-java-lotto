82b8c1ab0c5a38820aeb60fd46e45fdc3d835a54
roopre <benny1020@naver.com>
2023-01-02 18:44:56 +0900

    [feat] main 함수에서 lottoGame start 메소드 호출

--- src/main/java/kr/codesquad/Main.java
+++ src/main/java/kr/codesquad/Main.java
@@ -1,7 +1,11 @@
 package kr.codesquad;
 
+import java.util.Scanner;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        LottoGame lottoGame = new LottoGame();
+        lottoGame.start();
     }
 }
