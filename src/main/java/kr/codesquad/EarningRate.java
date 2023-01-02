bacb8c11f808c5ad3c626b024a9ba0e0249aafe5
waterfog9580 <waterfog9580@gmail.com>
2023-01-02 23:07:02 +0900

    :adhesive_bandage: EarningRate 팩토리 변경

--- src/main/java/kr/codesquad/EarningRate.java
+++ src/main/java/kr/codesquad/EarningRate.java
@@ -8,9 +8,17 @@ public class EarningRate {
     this.value = value;
   }
 
-  public static EarningRate from(double value) {
-    return new EarningRate(value);
+  public static EarningRate of(
+      long totalWinningMoney,
+      int purchaseAmount
+  ) {
+    double rate = ((double)(totalWinningMoney - purchaseAmount) / (double)purchaseAmount) * 100;
+    return new EarningRate(rate);
   }
 
+  @Override
+  public String toString() {
+    return String.format("%.2f%%", value);
+  }
 
 }
