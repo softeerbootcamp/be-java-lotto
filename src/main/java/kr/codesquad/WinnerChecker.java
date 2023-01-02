65ef05bcc9d895eefe71642db16576289a7b2c2f
waterfog9580 <waterfog9580@gmail.com>
2023-01-02 18:11:22 +0900

    :sparkles: WinnerChecker 구현

---
+++ src/main/java/kr/codesquad/WinnerChecker.java
@@ -0,0 +1,28 @@
+package kr.codesquad;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class WinnerChecker {
+
+  private static final int[] winningNumberCount = {3, 4, 5, 6};
+
+  public Map<Integer, Integer> check(
+      List<LottoNumbers> numbersList,
+      LottoNumbers winningNumber
+  ) {
+    Map<Integer, Integer> map = Arrays.stream(winningNumberCount)
+                                      .boxed()
+                                      .collect(Collectors.toMap(j -> j, j -> 0, (a, b) -> b));
+
+    numbersList.stream()
+               .mapToInt(winningNumber::countMatch)
+               .filter(map::containsKey)
+               .forEach(matchCount -> map.put(matchCount, map.get(matchCount) + 1));
+
+    return map;
+  }
+
+}
