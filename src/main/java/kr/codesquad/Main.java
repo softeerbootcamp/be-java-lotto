e8246b746be21bd102cede61496ff7d2c3b64064
jun-min1623 <junmin1623@naver.com>
2023-01-02 16:35:26 +0900

    feat:my lotto setting

--- src/main/java/kr/codesquad/Main.java
+++ src/main/java/kr/codesquad/Main.java
@@ -2,8 +2,40 @@ package kr.codesquad;
 
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
+class My_lotto{
+    private int lotto_cnt;
+    private ArrayList<One_lotto> my_lottos;
 
+    public My_lotto(ArrayList<One_lotto> my_lottos,int lotto_cnt) {
+        setLotto_cnt(lotto_cnt);
+        this.my_lottos = setMy_lottos(this.lotto_cnt);
+    }
+
+    public void setLotto_cnt(int lotto_cnt) {
+        this.lotto_cnt = lotto_cnt;
+    }
+
+    public ArrayList<One_lotto> setMy_lottos(int lotto_cnt) {
+        my_lottos = new ArrayList<>(lotto_cnt);
+        List<Integer> numList = new ArrayList<>();
+        for(int i=1;i <= 45;i++){
+            numList.add(i);
+        }
+        for (One_lotto e_lotto:my_lottos
+             ) {
+            Collections.shuffle(numList);
+            int[] lottoNums = new int[6];
+            for (int i = 0; i < 6; i++) {
+                lottoNums[i] = numList.get(i);
+            }
+            e_lotto.setNumbers(lottoNums);
+        }
+        return my_lottos;
+    }
+}
 class One_lotto{
     private int[] numbers;
     public One_lotto(int[] numbers) {
