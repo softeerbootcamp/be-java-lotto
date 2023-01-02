fba0221a9ae20f25df5a1ca86f1c7f1fc7027fea
rohsik2 <rohsik2@gmail.com>
2023-01-02 17:51:36 +0900

    refactor(UserInputHandler): delete try, catch phrase for reducting indent level

--- src/main/java/kr/codesquad/UserInputHandler.java
+++ src/main/java/kr/codesquad/UserInputHandler.java
@@ -1,24 +1,32 @@
 package kr.codesquad;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 public class UserInputHandler {
 
-    public static int getMoney(){
+    public static int getOneNumber( int lower_bound, int upper_bound){
         Scanner s1 = new Scanner(System.in);
-        int money =0;
-        while(true){
-            System.out.print("구입금액을 입력해 주세요.");
-            try {
-                money = s1.nextInt();
-                if(money % 1000 != 0 || money < 0){
-                    throw new IllegalStateException();
-                }
-                return money;
-            }
-            catch(Exception e){
-                System.out.println("0이상의 숫자를 1000단위로 입력해 주세요. 올바른 숫자형태를 입력해 주세요.");
-            }
-        }
 
+        int money;
+        money = s1.nextInt();
+        if(money % 1000 != 0 || money < lower_bound || money > upper_bound){
+            throw new IllegalStateException();
+        }
+        return money;
+    }
+    public static int getMoney(){
+        System.out.println("구입금액을 입력해 주세요.");
+        return getOneNumber(0, Integer.MAX_VALUE);
+    }
+
+    public static ArrayList<Integer> getWinningNumbers() {
+        System.out.println("당첨 번호를 입력해 주세요.");
+        Scanner s1 = new Scanner(System.in);
+        String[] nums = s1.nextLine().split(",");
+        ArrayList<Integer> winningNumber = new ArrayList<>();
+        for(String num : nums){
+            winningNumber.add(Integer.parseInt(num.trim()));
+        }
+        return winningNumber;
     }
 }
