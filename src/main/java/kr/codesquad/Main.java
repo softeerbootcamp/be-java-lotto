8b3e64f7ffe68363a4bb0cdbf7cd1eede44e5699
ajongs <wonjong1030@naver.com>
2023-01-02 21:46:00 +0900

    feat(Main): LottoService 클래스의 buyLotto()를 실행시키는 로직 구현

--- src/main/java/kr/codesquad/Main.java
+++ src/main/java/kr/codesquad/Main.java
@@ -1,7 +1,14 @@
 package kr.codesquad;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws IOException {
+        LottoService lottoService = new LottoService(new InputView(), new OutputView(), new Lotto(new NumberGenerator()));
+        lottoService.buyLotto();
     }
 }
