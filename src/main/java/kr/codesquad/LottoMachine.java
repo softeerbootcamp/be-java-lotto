c979449c760ca2780ae3c48ed9e6bb69d81506e1
solchan98 <dev.sol0427@gmail.com>
2023-01-02 18:11:40 +0900

    refactor: ArrayList만 사용하도록 Map 및 Set 사용 부분 List로 로직 수정 반영

--- src/main/java/kr/codesquad/LottoMachine.java
+++ src/main/java/kr/codesquad/LottoMachine.java
@@ -3,18 +3,21 @@ package kr.codesquad;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 public class LottoMachine {
 
-    private final Set<Integer> numSet;
+    private final List<Integer> numList;
     private final int priceOfLotto;
 
     private final BufferedReader br;
 
     public LottoMachine(int priceOfLotto) {
-        this.numSet = new HashSet<>(45);
-        for (int idx = 1; idx <= 45; idx++) numSet.add(idx);
+        this.numList = new ArrayList<>(45);
+        for (int idx = 1; idx <= 45; idx++) numList.add(idx);
         this.priceOfLotto = priceOfLotto;
         this.br = new BufferedReader(new InputStreamReader(System.in));
     }
@@ -34,27 +37,27 @@ public class LottoMachine {
 
 
     public void checkWin(LottoTicket lottoTicket) {
-        Set<Integer> winNumSet = this.getWinNumberSet();
-        Map<Rank, Integer> rankStatus = new HashMap<>(lottoTicket.getLottoList().size());
+        List<Integer> winNumberList = this.getWinNumberList();
+        List<Integer> rankList = new ArrayList<>(Arrays.asList(0, 0, 0, 0, 0));
         for (List<Integer> lotto: lottoTicket.getLottoList()) {
-            int winNumber = this.calcTargetedNumberCount(lotto, winNumSet);
+            int winNumber = this.calcTargetedNumberCount(lotto, winNumberList);
             Rank rank = Rank.valueOf(winNumber);
             if (rank == null) continue;
-            int totalCnt = rankStatus.containsKey(rank) ? rankStatus.get(rank) + 1 : 1;
-            rankStatus.put(rank, totalCnt);
+            int totalCnt = rankList.get(rank.getRank()) + 1;
+            rankList.set(rank.getRank(), totalCnt);
         }
-        this.printResult(rankStatus, lottoTicket.getMoney());
+        this.printResult(rankList, lottoTicket.getMoney());
     }
 
     private List<Integer> shuffle() {
-        List<Integer> numberList = new ArrayList<>(numSet);
+        List<Integer> numberList = new ArrayList<>(numList);
         Collections.shuffle(numberList);
         return numberList.subList(0, 6);
     }
 
-    private Set<Integer> getWinNumberSet() {
+    private List<Integer> getWinNumberList() {
         System.out.println("\n당첨 번호를 입력하세요.");
-        Set<Integer> winNumSet = new HashSet<>(6);
+        List<Integer> winNumSet = new ArrayList<>(6);
         try {
             String[] winNumArr  = br.readLine().split(" ");
             for (String winNum: winNumArr) winNumSet.add(Integer.parseInt(winNum));
@@ -64,7 +67,7 @@ public class LottoMachine {
         }
     }
 
-    private int calcTargetedNumberCount(List<Integer> lotto, Set<Integer> winNumSet) {
+    private int calcTargetedNumberCount(List<Integer> lotto, List<Integer> winNumSet) {
         int winNumCnt = 0;
         for (Integer winNum: winNumSet) {
             if (lotto.contains(winNum)) winNumCnt++;
@@ -72,12 +75,12 @@ public class LottoMachine {
         return winNumCnt;
     }
 
-    private void printResult(Map<Rank, Integer> rankStatus, int money) {
+    private void printResult(List<Integer> rankStatus, int money) {
         System.out.println("\n당첨 통계\n---------");
         int totalPrice = 0;
         List<Rank> rankList = List.of(Rank.FOURTH, Rank.THIRD, Rank.SECOND, Rank.FIRST);
         for (Rank rank: rankList) {
-            int targetedCnt = rankStatus.getOrDefault(rank, 0);
+            int targetedCnt = rankStatus.get(rank.getRank());
             totalPrice += rank.getWinningMoney() * targetedCnt;
             System.out.println(rank.getCountOfMatch() + "개 일치 (" + rank.getWinningMoney() + "원) - "
                     + targetedCnt + "개");
--- src/main/java/kr/codesquad/Rank.java
+++ src/main/java/kr/codesquad/Rank.java
@@ -1,17 +1,23 @@
 package kr.codesquad;
 
 public enum Rank {
-    FIRST(6, 2000000000),
-    SECOND(5, 1500000),
-    THIRD(4, 50000),
-    FOURTH(3, 5000);
+    FIRST(6, 2000000000, 1),
+    SECOND(5, 1500000, 2),
+    THIRD(4, 50000, 3),
+    FOURTH(3, 5000, 4);
 
     private int countOfMatch;
     private int winningMoney;
+    private int rank;
 
-    Rank(int countOfMatch, int winningMoney) {
+    Rank(int countOfMatch, int winningMoney, int rank) {
         this.countOfMatch = countOfMatch;
         this.winningMoney = winningMoney;
+        this.rank = rank;
+    }
+
+    public int getRank() {
+        return this.rank;
     }
 
     public int getCountOfMatch() {
