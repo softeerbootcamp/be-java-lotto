97fcfd0725b2fbcf02064b837c322665578ca641
jun-min1623 <junmin1623@naver.com>
2023-01-02 16:15:09 +0900

    feat:todo list 1

--- src/main/java/kr/codesquad/Main.java
+++ src/main/java/kr/codesquad/Main.java
@@ -1,7 +1,16 @@
 package kr.codesquad;
 
+
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
+    public static final int LOTTO_PRICE = 1000;
+
+    // todo list 1
+    public static int ret_lotto_cnt(int money){
+        return money%1000;
+    }
+
 }
