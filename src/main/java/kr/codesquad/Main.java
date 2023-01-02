93823349ccb426cdb47bfc5b220c5cc870fc4bba
jun-min1623 <junmin1623@naver.com>
2023-01-02 18:24:14 +0900

    feat:setjackpotnum 분리

--- src/main/java/kr/codesquad/Main.java
+++ src/main/java/kr/codesquad/Main.java
@@ -52,6 +52,7 @@ public class Main {
     public static final int LOTTO_PRICE = 1000;
     public static final int LOTTO_CNT = 6;
     public static final int[] JACKPOT_CNT = {0,0,0,0};
+    public static ArrayList<Integer> JACKPOT_NUM;
     // todo list 1
     public static int ret_lotto_cnt(int money){
         return money/LOTTO_PRICE;
@@ -81,7 +82,17 @@ public class Main {
         prof = (sum-money)/money;
         return prof;
     }
-
+    public static void setJackpotNum(){
+        System.out.printf("당첨 번호를 입력해 주세요.\n");
+        Scanner s2 = new Scanner(System.in);
+        String str = s2.nextLine();
+        String[] strArr= str.split(",");
+        JACKPOT_NUM = new ArrayList<>(LOTTO_CNT);
+        for(int i=0;i<LOTTO_CNT;i++){
+            JACKPOT_NUM.add(Integer.parseInt(strArr[i]));
+        }
+        Collections.sort(JACKPOT_NUM);
+    }
     public static void main(String[] args) {
         int money,cnt;
         System.out.println("구입 금액을 입력해 주세요.");
@@ -91,16 +102,8 @@ public class Main {
         System.out.printf("%d개를 구매했습니다.\n",cnt);
         My_lotto myLotto = new My_lotto(cnt);
         // todo list 2 까지 해결
-        System.out.printf("당첨 번호를 입력해 주세요.\n");
-        Scanner s2 = new Scanner(System.in);
-        String str = s2.nextLine();
-        String[] strArr= str.split(",");
-        ArrayList<Integer> jackpot_num = new ArrayList<>(LOTTO_CNT);
-        for(int i=0;i<LOTTO_CNT;i++){
-            jackpot_num.add(Integer.parseInt(strArr[i]));
-        }
-        Collections.sort(jackpot_num);
-        finding_jackpot(myLotto,jackpot_num);
+        setJackpotNum();
+        finding_jackpot(myLotto,JACKPOT_NUM);
         System.out.printf("총 수익률은 : %f",calculate_profit(JACKPOT_CNT,money)*100);
         System.out.print("%");
         System.out.println("입니다.");
