f3633b54e4854ab0e050bfbc68ccbf2a18dec654
solchan98 <dev.sol0427@gmail.com>
2023-01-02 16:56:47 +0900

    feat: 당첨 번호 입력받는 getWinNumberSet 메서드 구현 (사용자가 호출 할 메서드에서 호출)

--- src/main/java/kr/codesquad/LottoMachine.java
+++ src/main/java/kr/codesquad/LottoMachine.java
@@ -1,5 +1,8 @@
 package kr.codesquad;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
 
 public class LottoMachine {
@@ -7,10 +10,13 @@ public class LottoMachine {
     private final Set<Integer> numSet;
     private final int price;
 
+    private final BufferedReader br;
+
     public LottoMachine(int price) {
         this.numSet = new HashSet<>(45);
         for (int idx = 1; idx <= 45; idx++) numSet.add(idx);
         this.price = price;
+        this.br = new BufferedReader(new InputStreamReader(System.in));
     }
 
     public List<List<Integer>> buy(int total) {
@@ -28,5 +34,17 @@ public class LottoMachine {
         List<Integer> numberList = new ArrayList<>(numSet);
         Collections.shuffle(numberList);
         return numberList.subList(0, 6);
+    }
+
+    private Set<Integer> getWinNumberSet() {
+        System.out.println("당첨 번호를 입력하세요.");
+        Set<Integer> winNumSet = new HashSet<>(6);
+        try {
+            String[] winNumArr  = br.readLine().split(" ");
+            for (String winNum: winNumArr) winNumSet.add(Integer.parseInt(winNum));
+            return winNumSet;
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
