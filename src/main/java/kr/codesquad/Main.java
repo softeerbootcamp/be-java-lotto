d78796263eb897eff5e3fbd1799b5a4d7cc703f5
Toa <ld980105@snu.ac.kr>
2023-01-02 15:55:44 +0900

    feat : 입력 금액 받기

--- src/main/java/kr/codesquad/Main.java
+++ src/main/java/kr/codesquad/Main.java
@@ -1,7 +1,14 @@
 package kr.codesquad;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+    }
+
+    private static String getInput(){
+        System.out.println("구입금액을 입력해 주세요.");
+        Scanner sc = new Scanner(System.in);
+        return sc.next();
     }
 }
