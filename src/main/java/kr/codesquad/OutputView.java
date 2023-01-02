f35b300f6e3754609c0b81aaf332ecfc30d1c34c
ajongs <wonjong1030@naver.com>
2023-01-02 21:36:38 +0900

    feat(OutputView): 구매한 로또의 1개의 열을 출력하는 기능 구현

--- src/main/java/kr/codesquad/OutputView.java
+++ src/main/java/kr/codesquad/OutputView.java
@@ -1,10 +1,18 @@
 package kr.codesquad;
 
+import java.util.List;
+
 public class OutputView {
     public void printInputMoneyText() {
         System.out.println("구입금액을 입력해 주세요.");
     }
     public void printBoughtLottoCount(long count) {
         System.out.println(count+"개를 구매했습니다.");
+    }
+    private void printNumbers(List<Integer> list) {
+        for (int i = 0; i < list.size() - 1; i++) {
+            System.out.print(list.get(i) + ", ");
+        }
+        System.out.print(list.get(list.size() - 1));
     }
 }
