d7ad6b3d3ab4b5e245f36eb81e968bec6209cbdd
ajongs <wonjong1030@naver.com>
2023-01-02 18:18:32 +0900

    feat(Lotto): 로또 전체 생성기능 구현

--- src/main/java/kr/codesquad/Lotto.java
+++ src/main/java/kr/codesquad/Lotto.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 public class Lotto {
     private List<List<Integer>> numbers;
+    private NumberGenerator numberGenerator;
 
     Lotto() {
         this.numbers = new ArrayList<>();
@@ -12,5 +13,10 @@ public class Lotto {
     public long calculateCount(long money) {
         return money / 1000;
     }
-
+    public List<List<Integer>> generateLotto(long count) {
+        for (int i = 0; i < count; i++) {
+            numbers.add(numberGenerator.numberGenerate());
+        }
+        return numbers;
+    }
 }
