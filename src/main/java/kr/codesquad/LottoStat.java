a6b604368835c8b465d3e11f052495cd5df84119
rohsik2 <rohsik2@gmail.com>
2023-01-02 17:50:05 +0900

    refactor(LottoStat): print method를 조금 더 보기 좋도록 변환

--- src/main/java/kr/codesquad/LottoStat.java
+++ src/main/java/kr/codesquad/LottoStat.java
@@ -1,2 +1,42 @@
-package kr.codesquad;public class LottoStat {
+package kr.codesquad;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class LottoStat {
+
+    private long total_money;
+    private int[] winnings = {0,0,0,0,0,0,0};
+    private ArrayList<Lotto> lottos;
+    private ArrayList<Integer> winningNums;
+    public LottoStat(ArrayList<Lotto> lottos, ArrayList<Integer> winningNums){
+        this.lottos = lottos;
+        this.winningNums = winningNums;
+        System.out.println(winningNums);
+
+        calcResult();
+    }
+
+    public void calcResult() {
+        for(int i =0; i<winnings.length; i++){
+            winnings[i] = 0;
+        }
+        for(Lotto lotto : lottos){
+            int count = lotto.getRightCount(winningNums);
+            winnings[count] ++;
+            total_money += Lotto.getMoneyByCount(count);
+        }
+    }
+
+    public void printResult(){
+        System.out.println("당첨 통계");
+        System.out.println("---------");
+        System.out.println(String.format("3개 일치 (5000원)- %d개", winnings[3]));
+        System.out.println(String.format("4개 일치 (50000원)- %d개", winnings[4]));
+        System.out.println(String.format("5개 일치 (15000원)- %d개", winnings[5]));
+        System.out.println(String.format("6개 일치 (2000000원)- %d개", winnings[6]));
+        double roi = (double)(total_money - lottos.size() * 1000) / (lottos.size() * 1000) * 100;
+        System.out.println(String.format("총 수익률은 %.2f%입니다.", roi));
+    }
+
 }
