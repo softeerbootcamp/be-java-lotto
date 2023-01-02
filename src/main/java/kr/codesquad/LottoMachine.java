a5cadb1248351777db16a01984e9fad2a9402d4a
solchan98 <dev.sol0427@gmail.com>
2023-01-02 16:25:51 +0900

    feat: 로또 기계 로또 구매 메서드 buy 구현

--- src/main/java/kr/codesquad/LottoMachine.java
+++ src/main/java/kr/codesquad/LottoMachine.java
@@ -5,12 +5,23 @@ import java.util.*;
 public class LottoMachine {
 
     private final Set<Integer> numSet;
-    private int price = 1000;
+    private final int price;
 
     public LottoMachine(int price) {
         this.numSet = new HashSet<>(45);
         for (int idx = 1; idx <= 45; idx++) numSet.add(idx);
         this.price = price;
+    }
+
+    public List<List<Integer>> buy(int total) {
+        int cnt = total / price;
+
+        List<List<Integer>> lottoList = new ArrayList<>(cnt);
+        for (int idx = 0; idx < cnt; idx++) {
+            lottoList.add(shuffle());
+        }
+
+        return lottoList;
     }
 
     private List<Integer> shuffle() {
