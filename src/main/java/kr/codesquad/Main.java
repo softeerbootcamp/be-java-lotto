a09d39d5f87ef13d82f7d1e338ed50a1c0c5c42c
pjs0418 <tony180322@gmail.com>
2023-01-02 17:21:40 +0900

    Implement input winning lotto number function

--- src/main/java/kr/codesquad/Main.java
+++ src/main/java/kr/codesquad/Main.java
@@ -7,17 +7,18 @@ import java.util.Scanner;
 
 public class Main {
     static List<List<Integer>> lottoList = new ArrayList<>();
+    static List<Integer> winNumList;
 
     public static void main(String[] args) {
         int count = getLottoCount();
         System.out.println(count + "개를 구매했습니다.");
 
         shuffle(count);
+        inputWinNum();
     }
 
     public static int getLottoCount() {
         System.out.println("구입금액을 입력해 주세요.");
-
         Scanner sc = new Scanner(System.in);
         return sc.nextInt() / 1000;
     }
@@ -32,5 +33,15 @@ public class Main {
             Collections.sort(lottoList.get(i));
             System.out.println(lottoList.get(i).toString());
         }
+    }
+
+    public static void inputWinNum() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("당첨 번호를 입력해 주세요.");
+        String winNum = sc.nextLine();
+        String[] temp = winNum.split(",");
+
+        winNumList = new ArrayList<>();
+        for(int i = 0;i < 6;i++) winNumList.add(Integer.parseInt(temp[i]));
     }
 }
