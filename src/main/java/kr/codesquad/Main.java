f0d2b58e968d616cc38e4b2e254dc7eaba59bc12
waterfog9580 <waterfog9580@gmail.com>
2023-01-02 18:30:16 +0900

    :sparkles: Main 구현

--- src/main/java/kr/codesquad/Main.java
+++ src/main/java/kr/codesquad/Main.java
@@ -1,7 +1,10 @@
 package kr.codesquad;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
+
+  public static void main(String[] args) {
+    App app = new App(new Console(), new WinnerChecker(), new LottoNumbersFactory());
+    app.run();
+  }
+
 }
