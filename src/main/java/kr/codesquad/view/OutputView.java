11c70c79dfeefa48078c7132de5802bc8767f1e0
Soomin-Lim <augustth511@gmail.com>
2023-01-02 18:34:27 +0900

    feat: 당첨 통계 메시지 출력 기능 구현

--- src/main/java/kr/codesquad/view/OutputView.java
+++ src/main/java/kr/codesquad/view/OutputView.java
@@ -26,6 +26,8 @@ public class OutputView {
     }
 
     public static void printResult(Map<Integer, Integer> result, double profitRate) {
+        System.out.println("당첨 통계\n----------");
+
         System.out.println("3개 일치 (5000원)- " + result.get(3) + "개");
         System.out.println("4개 일치 (50000원)- " + result.get(4) + "개");
         System.out.println("5개 일치 (1500000원)- " + result.get(5) + "개");
