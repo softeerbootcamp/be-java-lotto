fb7ba5e20a52b3a1249742f74c36cc0df99f79dc
rohsik2 <rohsik2@gmail.com>
2023-01-02 17:23:25 +0900

    fix(Lotto): deleting bonus number param

--- src/main/java/kr/codesquad/Lotto.java
+++ src/main/java/kr/codesquad/Lotto.java
@@ -3,25 +3,22 @@ package kr.codesquad;
 import java.util.ArrayList;
 
 public class Lotto {
-
     private final ArrayList<Integer> lottoNums;
 
     public Lotto(){
         lottoNums = Utility.generateRandomSixNumbers();
     }
 
-    public int getRightCount(ArrayList<Integer> prize_nums, int prize_bonus_num){
+    public int getRightCount(ArrayList<Integer> prize_nums){
         int count = 0;
         for(Integer expect_no : lottoNums){
             if(prize_nums.contains(expect_no))
                 count += 1;
         }
-
         return count;
     }
 
     public static int getMoneyByCount(int correct_count){
-
         if(correct_count == 6){
             return 2000000000;
         }
