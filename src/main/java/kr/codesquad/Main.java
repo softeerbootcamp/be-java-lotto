e14ef314a4978b0e926f7648c3b2c0efdd7d9f16
jun-min1623 <junmin1623@naver.com>
2023-01-02 16:22:15 +0900

    feat:one lotto

--- src/main/java/kr/codesquad/Main.java
+++ src/main/java/kr/codesquad/Main.java
@@ -1,7 +1,19 @@
 package kr.codesquad;
 
 
+import java.util.ArrayList;
 
+
+class One_lotto{
+    private int[] numbers;
+    public One_lotto(int[] numbers) {
+        setNumbers(numbers);
+    }
+
+    public void setNumbers(int[] numbers) {
+        this.numbers = numbers;
+    }
+}
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
