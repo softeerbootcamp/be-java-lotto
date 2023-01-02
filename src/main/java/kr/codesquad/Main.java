07963e3f48ea675a0d6b7db3d3dd7f2df228cbd2
rohsik2 <rohsik2@gmail.com>
2023-01-02 17:50:47 +0900

    feat(main): writing main lottery program algorithms

--- src/main/java/kr/codesquad/Main.java
+++ src/main/java/kr/codesquad/Main.java
@@ -1,7 +1,23 @@
 package kr.codesquad;
 
+import java.util.Scanner;
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int n_lotto = UserInputHandler.getMoney() / 1000;
+        ArrayList<Lotto> lottos = generateLottos(n_lotto);
+        ArrayList<Integer> winningNumbers = UserInputHandler.getWinningNumbers();
+        LottoStat lottoStat = new LottoStat(lottos, winningNumbers);
+        lottoStat.printResult();
     }
+
+    public static ArrayList<Lotto> generateLottos(int n_lotto){
+        ArrayList<Lotto> lottos = new ArrayList<>();
+        for(int i =0; i<n_lotto; i++){
+            lottos.add(new Lotto());
+        }
+        return lottos;
+    }
+
 }
