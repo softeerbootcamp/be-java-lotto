5879d4e78b32bf0452ade16f1fb1dbc4e168a173
Soomin-Lim <augustth511@gmail.com>
2023-01-02 18:31:51 +0900

    feat: 당첨 통계 출력 기능 구현

--- src/main/java/kr/codesquad/view/OutputView.java
+++ src/main/java/kr/codesquad/view/OutputView.java
@@ -3,6 +3,7 @@ package kr.codesquad.view;
 import kr.codesquad.model.Lotto;
 
 import java.util.List;
+import java.util.Map;
 
 public class OutputView {
 
@@ -22,5 +23,14 @@ public class OutputView {
 
     public static void printWinningLottoReadMessage() {
         System.out.println("당첨 번호를 입력해 주세요.");
+    }
+
+    public static void printResult(Map<Integer, Integer> result, double profitRate) {
+        System.out.println("3개 일치 (5000원)- " + result.get(3) + "개");
+        System.out.println("4개 일치 (50000원)- " + result.get(4) + "개");
+        System.out.println("5개 일치 (1500000원)- " + result.get(5) + "개");
+        System.out.println("6개 일치 (2000000000원)- " + result.get(6) + "개");
+
+        System.out.println("총 수익률은 " + String.format("%.2f", profitRate) + "%입니다.");
     }
 }
