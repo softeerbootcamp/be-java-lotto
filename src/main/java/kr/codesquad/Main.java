b4c8530f351fd03f63a6d7820f85794d07bd47d9
jun-min1623 <junmin1623@naver.com>
2023-01-02 16:53:09 +0900

    feat:todo list 2

--- src/main/java/kr/codesquad/Main.java
+++ src/main/java/kr/codesquad/Main.java
@@ -4,12 +4,13 @@ package kr.codesquad;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Scanner;
 
 class My_lotto{
     private int lotto_cnt;
     private ArrayList<One_lotto> my_lottos;
 
-    public My_lotto(ArrayList<One_lotto> my_lottos,int lotto_cnt) {
+    public My_lotto(int lotto_cnt) {
         setLotto_cnt(lotto_cnt);
         this.my_lottos = setMy_lottos(this.lotto_cnt);
     }
@@ -24,14 +25,14 @@ class My_lotto{
         for(int i=1;i <= 45;i++){
             numList.add(i);
         }
-        for (One_lotto e_lotto:my_lottos
-             ) {
+        for (int j=0;j<lotto_cnt;j++) {
             Collections.shuffle(numList);
             int[] lottoNums = new int[6];
             for (int i = 0; i < 6; i++) {
                 lottoNums[i] = numList.get(i);
             }
-            e_lotto.setNumbers(lottoNums);
+            One_lotto tmplotto = new One_lotto(lottoNums);
+            my_lottos.add(tmplotto);
         }
         return my_lottos;
     }
@@ -47,14 +48,23 @@ class One_lotto{
     }
 }
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
+
     public static final int LOTTO_PRICE = 1000;
 
     // todo list 1
     public static int ret_lotto_cnt(int money){
-        return money%LOTTO_PRICE;
+        return money/LOTTO_PRICE;
     }
+    public static void main(String[] args) {
+        int money,cnt;
+        System.out.println("구입 금액을 입력해 주세요.");
+        Scanner sc = new Scanner(System.in);
+        money = sc.nextInt();
+        cnt = ret_lotto_cnt(money);
+        System.out.printf("%d개를 구매했습니다.\n",cnt);
+        My_lotto myLotto = new My_lotto(cnt);
+        System.out.printf("");
 
+
+    }
 }
