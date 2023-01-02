2ce784af0bf931e87505468b85cfe1a6973c0bde
jieonkim23 <rentalhub@rentalhubui-MacBookPro.local>
2023-01-03 03:13:36 +0900

    stream 사용

--- src/main/java/kr/codesquad/Lotto.java
+++ src/main/java/kr/codesquad/Lotto.java
@@ -3,6 +3,7 @@ package kr.codesquad;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class Lotto {
@@ -11,26 +12,15 @@ public class Lotto {
     private static final int LOTTO_NUM_CNT = 6;
 
     Lotto(){
-        List<Integer> temp = IntStream.range(1, LOTTO_NUM_MAX+1)
-        ArrayList<Integer> n = new ArrayList<>();
-        for(int i=1; i<=LOTTO_NUM_MAX; i++)
-            n.add(i);
-        Collections.shuffle(n);
-        nums = new ArrayList<Integer>();
-        for(int i=0; i<6; i++)
-            nums.add(n.get(i));
+        List<Integer> temp = IntStream.range(1, LOTTO_NUM_MAX+1).boxed().collect(Collectors.toList());
+        Collections.shuffle(temp);
+        nums = temp.subList(0,LOTTO_NUM_CNT+1);
         Collections.sort(nums);
     }
 
-    public int checkWin(int[] win, int bonus) {
-        int cnt = 0;
-        for (int i = 0; i < LOTTO_NUM_CNT; i++) {
-            cnt += nums.contains(win[i]) ? 1 : 0;
-        }
-        if(cnt == 5 && nums.contains(bonus)){
-
-        }
-        return cnt;
+    public int checkWin(List<Integer> win) {
+        long cnt = win.stream().filter(w -> nums.contains(w)).count();
+        return (int)cnt;
     }
     public void printLotto(){
         System.out.println(nums);
--- src/main/java/kr/codesquad/LottoSystem.java
+++ src/main/java/kr/codesquad/LottoSystem.java
@@ -1,29 +1,27 @@
 package kr.codesquad;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class LottoSystem {
     int money;
-    int[] winNums;
-    int bonus;
+    double rate;
+    List<Integer> winNums;
     Scanner sc;
     List<Lotto> lottos = new ArrayList<>();
     private static final int COST = 1000;
     int[] result = new int[7];
-    double rate;
 
     public void getMoney() {
         System.out.println("구입금액을 입력해주세요.");
         sc = new Scanner(System.in);
         money = Integer.parseInt(sc.nextLine());
         System.out.println(money / COST + "개를 구매했습니다.");
-
-    }
-    public void createLotto(){
-
     }
     public void getLottos() {
         for (int i = 0; i < money / COST; i++) {
@@ -32,19 +30,16 @@ public class LottoSystem {
             l.printLotto();
         }
     }
-
     public void getWin() {
         sc = new Scanner(System.in);
         System.out.println("\n당첨 번호를 입력해주세요.");
-        String[] s = sc.nextLine().replaceAll(" ", "").split(",");
-        winNums = Stream.of(s).mapToInt(Integer::parseInt).toArray();
-        System.out.println("보너스 볼을 입력해주세요.");
-        bonus = Integer.parseInt(sc.nextLine());
+        List<String> s = Arrays.asList(sc.nextLine().replaceAll(" ", "").split(","));
+        winNums = s.stream().map(Integer::parseInt).collect(Collectors.toList());
     }
 
     public void getResult() {
         for (int i = 0; i < money / COST; i++) {
-            int cnt = lottos.get(i).checkWin(winNums, bonus);
+            int cnt = lottos.get(i).checkWin(winNums);
             result[cnt]++;
         }
         long earnMoney = 5000l * result[3] + 50000l * result[4] + 1500000l * result[5] + 2000000000l * result[6];
