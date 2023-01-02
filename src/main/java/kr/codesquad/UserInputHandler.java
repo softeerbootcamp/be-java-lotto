9f6d174589ce3675b9e955236f3a97cd24dd150b
rohsik2 <rohsik2@gmail.com>
2023-01-02 18:03:02 +0900

    refactor(UserInputHandler): adding some bound checking and change array to List

--- src/main/java/kr/codesquad/UserInputHandler.java
+++ src/main/java/kr/codesquad/UserInputHandler.java
@@ -1,6 +1,8 @@
 package kr.codesquad;
 
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Scanner;
 public class UserInputHandler {
 
@@ -10,7 +12,7 @@ public class UserInputHandler {
         int money;
         money = s1.nextInt();
         if(money % 1000 != 0 || money < lower_bound || money > upper_bound){
-            throw new IllegalStateException();
+            throw new IllegalArgumentException();
         }
         return money;
     }
@@ -22,11 +24,13 @@ public class UserInputHandler {
     public static ArrayList<Integer> getWinningNumbers() {
         System.out.println("당첨 번호를 입력해 주세요.");
         Scanner s1 = new Scanner(System.in);
-        String[] nums = s1.nextLine().split(",");
+        List<String> nums = List.of(s1.nextLine().split(","));
         ArrayList<Integer> winningNumber = new ArrayList<>();
         for(String num : nums){
             winningNumber.add(Integer.parseInt(num.trim()));
         }
+        if(winningNumber.size() != 6)
+            throw new IllegalArgumentException();
         return winningNumber;
     }
 }
