c3c5b73f22e7f57b8c1c3ae03c28d081af0742b6
Toa <ld980105@snu.ac.kr>
2023-01-03 16:58:17 +0900

    feat : printWinningLottos 구현

--- src/main/java/kr/codesquad/Console.java
+++ src/main/java/kr/codesquad/Console.java
@@ -1,7 +1,11 @@
 package kr.codesquad;
 
+import kr.codesquad.LottoService.Lotto;
+import kr.codesquad.LottoService.LottoStore;
+
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
 import java.util.stream.Collectors;
 
@@ -28,6 +32,11 @@ public class Console {
         System.out.println(lottoCount + "개를 구매했습니다.");
     }
 
+    public void printLottos(List<Lotto> lottos) {
+        for (Lotto lotto : lottos)
+            System.out.println(lotto);
+    }
+
     public void inputWinningNumber() {
         System.out.println("당첨 번호를 입력해 주세요.");
         Scanner sc = new Scanner(System.in);
@@ -39,6 +48,15 @@ public class Console {
         System.out.println("보너스 볼을 입력해 주세요.");
         Scanner sc = new Scanner(System.in);
         bonusNumber = sc.nextInt();
+    }
+
+    public void printWinningLottos(Map<String, Integer> winningLottos) {
+        System.out.println("당첨 통계");
+        System.out.println("---------");
+        for (Rank rank : Rank.values()){
+            String line = String.format("%d개 일치 (%d원)- %d개", rank.getCountOfMatch(), rank.getWinningMoney(), winningLottos.get(rank.toString()));
+            System.out.println(line);
+        }
     }
 
     public List<Integer> getWinningNumberList() {
