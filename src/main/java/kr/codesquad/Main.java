f2e2033ad02fc088d7721082cbe05fd88ad12be3
jieonkim23 <kwldjs1023@gmail.com>
2023-01-03 17:58:48 +0900

    Main클래스의 LottoSystem 메소드 호출을 변경함: LottoSystem의 makeLottos생성 및 변경에따라 main에서 호출하는 메소드명도 수정하였음.

--- src/main/java/kr/codesquad/Main.java
+++ src/main/java/kr/codesquad/Main.java
@@ -3,7 +3,7 @@ package kr.codesquad;
 public class Main {
     public static void main(String[] args) {
         LottoSystem ls = new LottoSystem();
-        ls.getLottos();
+        ls.makeLottos();
         ls.getResult();
     }
 }
