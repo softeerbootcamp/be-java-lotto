fc020c75e73a38cc140f901ff7fe5e2493a2bc77
jieonkim23 <kwldjs1023@gmail.com>
2023-01-03 11:45:07 +0900

    map 변경

--- src/main/java/kr/codesquad/LottoSystem.java
+++ src/main/java/kr/codesquad/LottoSystem.java
@@ -1,9 +1,6 @@
 package kr.codesquad;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 import java.util.stream.Collector;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -15,7 +12,8 @@ public class LottoSystem {
     Scanner sc;
     List<Lotto> lottos = new ArrayList<>();
     private static final int COST = 1000;
-    int[] result = new int[7];
+    //int[] result = new int[7];
+    private Map<Integer, Integer> results = new HashMap<>();
 
     public void getMoney() {
         System.out.println("구입금액을 입력해주세요.");
@@ -38,21 +36,25 @@ public class LottoSystem {
     }
 
     public void getResult() {
+        results.put(0,0); results.put(1,0);results.put(2,0);
+        results.put(3,0);results.put(4,0);
+        results.put(5,0);results.put(6,0);
+
         for (int i = 0; i < money / COST; i++) {
             int cnt = lottos.get(i).checkWin(winNums);
-            result[cnt]++;
+            results.put(cnt, results.get(cnt)+1);
         }
-        long earnMoney = 5000l * result[3] + 50000l * result[4] + 1500000l * result[5] + 2000000000l * result[6];
+        long earnMoney = 5000l * results.get(3) + 50000l * results.get(4) + 1500000l * results.get(5) + 2000000000l * results.get(6);
         rate = (earnMoney - money) / (double) money * 100;
     }
 
     public void printResult() {
         System.out.println("\n당첨 통계");
         System.out.println("---------");
-        System.out.printf("3개 일치 (5000원)- %d개\n", result[3]);
-        System.out.printf("4개 일치 (50000원)- %d개\n", result[4]);
-        System.out.printf("5개 일치 (1500000원)- %d개\n", result[5]);
-        System.out.printf("6개 일치 (2000000000원)- %d개\n", result[6]);
+        System.out.printf("3개 일치 (5000원)- %d개\n", results.get(3));
+        System.out.printf("4개 일치 (50000원)- %d개\n", results.get(4));
+        System.out.printf("5개 일치 (1500000원)- %d개\n", results.get(5));
+        System.out.printf("6개 일치 (2000000000원)- %d개\n", results.get(6));
         System.out.printf("총 수익률은 %.2f%%입니다.\n", rate);
     }
 }
