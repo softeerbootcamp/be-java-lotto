758db2eaae578bf1789d6ad858518e53eef908c0
roopre <benny1020@naver.com>
2023-01-03 11:03:57 +0900

    [Fix] main 메서드에서 로또 구매, 로또 추첨, 로또 결과 확인 메서드 호출하도록 수정

--- src/main/java/kr/codesquad/Main.java
+++ src/main/java/kr/codesquad/Main.java
@@ -1,11 +1,14 @@
 package kr.codesquad;
 
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
         LottoGame lottoGame = new LottoGame();
-        lottoGame.start();
+        List<Lotto> purchasedLottoList = lottoGame.purchaseLottoList();
+        Lotto winningLotto = lottoGame.createWinningLottery();
+        lottoGame.checkMyLotto(purchasedLottoList, winningLotto);
     }
 }
