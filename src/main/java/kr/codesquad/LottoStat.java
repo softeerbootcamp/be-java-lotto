5d4dcb02908f2b39d8297f2a7a21bb79a481d26a
rohsik2 <rohsik2@gmail.com>
2023-01-03 11:02:24 +0900

    fix(LottoStat): printing method using (index) count -> Mapped (Enum) prize

--- src/main/java/kr/codesquad/LottoStat.java
+++ src/main/java/kr/codesquad/LottoStat.java
@@ -4,6 +4,8 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import static kr.codesquad.Prize.*;
+
 public class LottoStat {
 
     private long total_money;
@@ -35,11 +37,11 @@ public class LottoStat {
     public void printResult(){
         System.out.println("당첨 통계");
         System.out.println("---------");
-        System.out.println(String.format("3개 일치 (5000원)- %d개", winnings[3]));
-        System.out.println(String.format("4개 일치 (50000원)- %d개", winnings[4]));
-        System.out.println(String.format("5개 일치 (15000원)- %d개", winnings[5]));
-        System.out.println(String.format("5개 일치, 보너스 볼 일치(30000000원) - %d개", winnings[6]));
-        System.out.println(String.format("6개 일치 (2000000원)- %d개", winnings[7]));
+        System.out.println(String.format("3개 일치 (5000원)- %d개", winnings.get(FIFTH)));
+        System.out.println(String.format("4개 일치 (50000원)- %d개", winnings.get(FORTH) ));
+        System.out.println(String.format("5개 일치 (15000원)- %d개", winnings.get(THIRD) ));
+        System.out.println(String.format("5개 일치, 보너스 볼 일치(30000000원) - %d개", winnings.get(SECOND) ));
+        System.out.println(String.format("6개 일치 (2000000원)- %d개", winnings.get(FIRST) ));
         double roi = (double)(total_money - lottos.size() * 1000L) / (lottos.size() * 1000) * 100;
         System.out.println(String.format("총 수익률은 %.2f%입니다.", roi));
     }
