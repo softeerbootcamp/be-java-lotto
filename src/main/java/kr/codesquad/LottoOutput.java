415e9e65a34bc019aab88dce7ef99a1254809f03
jieonkim23 <kwldjs1023@gmail.com>
2023-01-03 18:29:28 +0900

    Rank의 수정에 따라 Output 수정: LottoOutput의 printResult를 Rank에 맞춰 수정함.

--- src/main/java/kr/codesquad/LottoOutput.java
+++ src/main/java/kr/codesquad/LottoOutput.java
@@ -11,7 +11,7 @@ public class LottoOutput {
     public static void printResult(Map<Rank, Integer> result, double rate){
         System.out.println("\n당첨 통계");
         System.out.println("---------");
-        result.forEach((r,c)-> System.out.printf("%d개 일치 (%d원)- %d개\n", r.getCnt(), r.getPrize(), c));
+        result.forEach((r,c)-> System.out.printf("%s (%d원)- %d개\n", r.getWords(), r.getPrize(), c));
         System.out.printf("총 수익률은 %.2f%%입니다.\n", rate);
     }
 }
