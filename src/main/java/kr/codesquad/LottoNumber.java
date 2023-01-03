8e1d6adaabd86e2006e21c2c0c518a5c90028b98
River-Mt <rivermt1998@gmail.com>
2023-01-03 15:05:44 +0900

    Refactor: add LOTTO_MAX_NUMBER, LOTTO_MIN_NUMBER, LOTTO_SIZE - 상수화 할 수 있는 값들 상수화

--- src/main/java/kr/codesquad/LottoNumber.java
+++ src/main/java/kr/codesquad/LottoNumber.java
@@ -5,15 +5,19 @@ import java.util.Collections;
 import java.util.List;
 
 public class LottoNumber {
+    private static final int LOTTO_MAX_NUMBER = 45;
+    private static final int LOTTO_MIN_NUMBER = 1;
+    private static final int LOTTO_SIZE = 6;
+
     List<Integer> lotto = new ArrayList<>();
 
     public LottoNumber() {
         List<Integer> nums = new ArrayList<>();
-        for (int i = 1; i <= 45; i++) {
+        for (int i = LOTTO_MIN_NUMBER; i <= LOTTO_MAX_NUMBER; i++) {
             nums.add(i);
         }
         Collections.shuffle(nums);
-        for (int i = 0; i < 6; i++) {
+        for (int i = 0; i < LOTTO_SIZE; i++) {
             lotto.add(nums.get(i));
         }
         Collections.sort(lotto);
