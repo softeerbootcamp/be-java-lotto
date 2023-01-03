a3e42ce35c9353fa1251b31502399363c346f30b
rohsik2 <rohsik2@gmail.com>
2023-01-03 10:33:35 +0900

    feat: enum class for prize size

--- src/main/java/kr/codesquad/Prize.java
+++ src/main/java/kr/codesquad/Prize.java
@@ -1,5 +1,14 @@
 package kr.codesquad;
 
 public enum Prize {
-    FIRST, SECOND, THIRD, FORTH;
+    FIRST(2000000000), SECOND(1500000), THIRD(50000), FORTH(5000), NOTHING(0);
+    private int money;
+
+    Prize(int initMoney){
+        money = initMoney;
+    }
+
+    public int getMoney(){
+        return money;
+    }
 }
