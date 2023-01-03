91e70e26cdd0dc18170938cb09839d9a74ab72f9
pjs0418 <tony180322@gmail.com>
2023-01-03 10:58:13 +0900

    Lotto Class 생성자 구현

--- src/main/java/kr/codesquad/Lotto.java
+++ src/main/java/kr/codesquad/Lotto.java
@@ -1,4 +1,12 @@
 package kr.codesquad;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Lotto {
+    private final List<List<Integer>> lottoList;
+
+    public Lotto(List<List<Integer>> lottoList) {
+        this.lottoList = lottoList;
+    }
 }
