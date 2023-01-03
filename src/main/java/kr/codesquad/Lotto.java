8c6cd009a83c187c80b76205312aa78b323fe475
roopre <benny1020@naver.com>
2023-01-03 16:48:17 +0900

    [Feat] 로또 숫자 수 리턴하는 메서드 구현

--- src/main/java/kr/codesquad/Lotto.java
+++ src/main/java/kr/codesquad/Lotto.java
@@ -1,35 +1,38 @@
 package kr.codesquad;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class Lotto {
-    private final static int NUMBER_COUNT = 6;
+	private final static int NUMBER_COUNT = 6;
 
-    private List<Integer> numbers;
+	private List<Integer> numbers;
 
-    private Lotto(List<Integer> lotto) {
-        numbers = lotto;
-    }
+	private Lotto(List<Integer> lotto) {
+		numbers = lotto;
+	}
 
-    public static Lotto newOne(List<Integer> lotto) {
-        return new Lotto(lotto);
-    }
+	public static Lotto of(List<Integer> lotto) {
+		return new Lotto(lotto);
+	}
 
-    public List<Integer> getNumbers() {
-        return numbers;
-    }
+	public List<Integer> getNumbers() {
+		return numbers;
+	}
 
-    public String toString() {
+	public int getNumbersSize() {
+		return numbers.size();
+	}
 
-        StringBuilder ret = new StringBuilder();
-        ret.append('[');
-        for (int i = 0; i < NUMBER_COUNT - 1; i++) {
-            ret.append(numbers.get(i));
-            ret.append(", ");
-        }
-        ret.append(numbers.get(NUMBER_COUNT - 1));
-        ret.append(']');
-        return ret.toString();
-    }
+	public String toString() {
+
+		StringBuilder ret = new StringBuilder();
+		ret.append('[');
+		for (int i = 0; i < NUMBER_COUNT - 1; i++) {
+			ret.append(numbers.get(i));
+			ret.append(", ");
+		}
+		ret.append(numbers.get(NUMBER_COUNT - 1));
+		ret.append(']');
+		return ret.toString();
+	}
 }
