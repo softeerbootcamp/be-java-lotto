0972f8c764d7b44ed6fd0551d0b03607ce58d178
ajongs <wonjong1030@naver.com>
2023-01-03 15:56:17 +0900

    style(Main): 필요없는 import 문 제거

--- src/main/java/kr/codesquad/Main.java
+++ src/main/java/kr/codesquad/Main.java
@@ -1,10 +1,6 @@
 package kr.codesquad;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws IOException {
