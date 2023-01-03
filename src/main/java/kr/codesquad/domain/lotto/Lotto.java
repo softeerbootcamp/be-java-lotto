3c16cadda0d1a4199e11651e177a658b1414c6c6
waterfog9580 <waterfog9580@gmail.com>
2023-01-03 15:35:36 +0900

    :sparkles: 보너스 번호 구현

--- src/main/java/kr/codesquad/domain/lotto/Lotto.java
+++ src/main/java/kr/codesquad/domain/lotto/Lotto.java
@@ -1,31 +1,42 @@
 package kr.codesquad.domain.lotto;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
 
   private final List<Integer> numbers;
+  private final int bonusNumber;
 
-  public static final int LOTTO_PRICE = 1_000;
-
-  public Lotto(List<Integer> numbers) {
+  public Lotto(
+      List<Integer> numbers,
+      int bonusNumber
+  ) {
     this.numbers = numbers;
+    this.bonusNumber = bonusNumber;
   }
 
-  public List<Integer> getNumbers() {
-    return numbers;
-  }
-
-  public static Lotto from(List<Integer> numbers) {
-    return new Lotto(numbers);
+  public static Lotto of(
+      List<Integer> numbers,
+      int bonusNumber
+  ) {
+    return new Lotto(numbers, bonusNumber);
   }
 
   public String toString() {
-    return numbers.toString();
+    return numbers.toString() + String.format("\t\t보너스 번호 [%d]", bonusNumber);
   }
 
-  public boolean isContainNumber(int number) {
-    return numbers.contains(number);
+  public int countMatch(Lotto target) {
+    Set<Integer> set = new HashSet<>(target.numbers);
+    return (int)this.numbers.stream()
+                            .filter(set::contains)
+                            .count();
+  }
+
+  public boolean isBonusMatch(Lotto target) {
+    return this.bonusNumber == target.bonusNumber;
   }
 
 }
