5420fdc6938677c4987a471ca0c4d1a58c3d7490
roopre <benny1020@naver.com>
2023-01-03 16:50:31 +0900

    [Feat] matchCount를 구하는 정적 메서드 구현

--- src/main/java/kr/codesquad/LottoMatchType.java
+++ src/main/java/kr/codesquad/LottoMatchType.java
@@ -1,26 +1,39 @@
 package kr.codesquad;
 
+import java.util.Arrays;
+
 public enum LottoMatchType {
-    THREE_MATCH(3, 5000),
-    FOUR_MATCH(4, 50000),
-    FIVE_MATCH(5, 1500000),
-    SIX_MATCH(6, 2000000000),
+	MISS_MATCH(0, 0),
+	THREE_MATCH(3, 5000),
+	FOUR_MATCH(4, 50000),
+	FIVE_MATCH(5, 1500000),
+	SIX_MATCH(6, 2000000000),
 
-    BONUS_MATCH(7, 30000000);
+	BONUS_MATCH(7, 30000000);
 
-    LottoMatchType(int matchCount, int money) {
-        this.matchCount = matchCount;
-        this.money = money;
-    }
+	LottoMatchType(int matchCount, int money) {
+		this.matchCount = matchCount;
+		this.money = money;
+	}
 
-    public int getMatchCount() {
-        return matchCount;
-    }
+	public static LottoMatchType getLottoMatchTypeByMatchCount(int matchCount) {
+		if (matchCount < 3) {
+			return MISS_MATCH;
+		}
+		return Arrays.stream(LottoMatchType.values())
+			.filter(matchType -> matchType.matchCount == matchCount)
+			.findFirst()
+			.orElseThrow(IllegalArgumentException::new);
+	}
 
-    public int getMoney() {
-        return money;
-    }
+	public int getMatchCount() {
+		return matchCount;
+	}
 
-    private final int matchCount;
-    private final int money;
+	public int getMoney() {
+		return money;
+	}
+
+	private final int matchCount;
+	private final int money;
 }
