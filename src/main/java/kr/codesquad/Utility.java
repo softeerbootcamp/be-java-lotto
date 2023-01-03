9aa992e126cebf452337cb9854cb35bf8e360c89
jun-min1623 <junmin1623@naver.com>
2023-01-03 18:09:17 +0900

    refactor:시간복잡도 개선(Utility)

--- src/main/java/kr/codesquad/Utility.java
+++ src/main/java/kr/codesquad/Utility.java
@@ -6,6 +6,10 @@ import java.util.Collections;
 public class Utility {
     private static ArrayList<Integer> allLottoNum;
 
+    public Utility() {
+        setAllLottoNum();
+    }
+
     public void setAllLottoNum() {
         this.allLottoNum = new ArrayList<>();
         for (int i = 1; i <= 45; i++) {
@@ -14,7 +18,6 @@ public class Utility {
     }
 
     public ArrayList<Integer> retRandLottoNum() {
-        setAllLottoNum();
         Collections.shuffle(this.allLottoNum);
         ArrayList<Integer> lottoNums = new ArrayList<>(Lotto_Info.LOTTO_SIZE.getValue());
         for (int i = 0; i < Lotto_Info.LOTTO_SIZE.getValue(); i++) {
