356a4e92bce200b57069f7945bdf7bc8ab2a49cc
Jeong <63828202+JeongA-Shin@users.noreply.github.com>
2023-01-04 08:43:18 +0900

    로또2단계 - 보너스 번호 추가 (#40)
    * temp
    
    * 보너스볼 입력받기 구현
    
    * 일치 개수를 통해 2등 카운트, 출력 함수 수정
    
    * 일치 개수로 해당 rank 찾는 메서드 구현
    
    * enum map 업데이트 함수 구현
    
    * 통계 출력 부분 enum 활용해서 수정
    
    * 필요 없는 출력 부분 삭제
    
    * user 클래스 생성
    
    * 사용자 입력한 숫자에 대하여 처리 구현
    
    * 로또 자동 생성만 관여하도록 클래스 분리
    
    * 분리된 클래스들로 구현되도록 리팩토링
    
    Co-authored-by: macbook16 <macbook16@macbook16ui-noteubug.local>

--- src/main/java/kr/codesquad/Main.java
+++ src/main/java/kr/codesquad/Main.java
@@ -2,118 +2,118 @@ package kr.codesquad;
 
 import java.util.*;
 
+enum Rank{
+
+  FIRST(6,2000000000),
+  SECOND(5,30000000),
+  THIRD(5,1500000),
+  FOURTH(4,50000),
+  FIFTH(3,500);
+
+  private int countOfMatch;
+  private int winningMoney;
+
+  private Rank(int countOfMatch, int winningMoney){
+    this.countOfMatch = countOfMatch;
+    this.winningMoney= winningMoney;
+  }
+
+  public int getCountOfMatch(){
+    return countOfMatch;
+  }
+
+  public int getWinningMoney(){
+    return winningMoney;
+  }
+
+}
+
 class Lotto{
 
-  private List<List<Integer>> lottos;
-  private ArrayList<Integer> inputs;
-  private int lottoCnt;
-  List<Integer> numList;
-  int match3=0;
-  int match4=0;
-  int match5=0;
-  int match6=0;
+  User user;
+  LottoMachine machine;
+  Map<Rank, Integer> rankMap = new EnumMap<>(Rank.class);
 
-  public Lotto() {
-    lottos = new ArrayList<>();
-    inputs = new ArrayList<>();
-    numList = new ArrayList<>();
-    for(int j=1;j<46;j++) numList.add(j);
-    match3=0;
-    match4=0;
-    match5=0;
-    match6=0;
+  public void initialMap(){
+    Rank[] ranks = Rank.values();
+    for(Rank rank : ranks){
+      rankMap.put(rank,0);
+    }
+  }
+
+  public Lotto(LottoMachine machine, User user) {
+    this.machine = machine;
+    this.user = user;
+    initialMap();
   }
 
   public void start(){
     //1. 당첨 금액 입력 받기
-    Integer amountOfMoney = getAmountOfMoney();
+    Integer amountOfMoney = machine.getAmountOfMoney();
     //2. 전체 복권 개수 계산해서 변수에 저장
-    getLottoCount(amountOfMoney);
+    Integer lottoCnt = machine.getLottoCount(amountOfMoney);
     //3. 셔플로 복권 만들기
-    getLottos();
-    //4. 사용자로부터 복권 입력 받기
-    getLottoInput();
+    List<List<Integer>> lottos = machine.getLottos();
+    //4. 사용자로부터 복권 입력 - 지난 주 당첨 번호- 받기
+    List<Integer> inputs = user.getLottoInput();
+    //5. 보너스 볼 입력 받기
+    Integer bonusBall = user.getBonusLottoInput();
     //5. 비교
-    compareLottos(lottos,inputs);
+    compareLottos(lottoCnt,lottos,inputs,bonusBall);
     //6. 결과 출력
-    printResult();
-  }
-
-  public Integer getAmountOfMoney(){
-    System.out.println("구입금액을 입력해주세요. ");
-    Scanner sc = new Scanner(System.in);
-    int amountOfMoney = sc.nextInt();
-    return amountOfMoney;
-  }
-
-  public void getLottoCount(int amountOfMoney){
-    lottoCnt = amountOfMoney/1000; // 총 로또 개수
-    System.out.println("총"+lottoCnt+"개를 구매했습니다.");
+    printResult(lottoCnt);
   }
 
 
-  public void getLottos(){
+  public void compareLottos(int lottoCnt, List<List<Integer>> lottoLists, List<Integer> inputList,int bonusBall){
     for(int i=0;i<lottoCnt;i++){
-      getLottoByShuffle();
-    }
-    System.out.println(lottos);
-  }
-
-  public void getLottoByShuffle(){
-    Collections.shuffle(numList); //1에서 46까지를 셔플
-    List<Integer> shuffledNumList =  new ArrayList<>(numList.subList(0,6));
-    Collections.sort(shuffledNumList);
-    lottos.add(shuffledNumList);
-  }
-
-  public void getLottoInput(){
-    System.out.println("당첨 번호를 입력해주세요.");
-    Scanner sc = new Scanner(System.in);
-    String inputNumbers = sc.nextLine();
-    for(String input : inputNumbers.split(", ")){
-      inputs.add(Integer.valueOf(input));
-    }
-    //System.out.println(inputs);
-  }
-
-  public void compareLottos(List<List<Integer>> lottoLists, List<Integer> inputList){
-    for(int i=0;i<lottoCnt;i++){
-      System.out.println(lottoLists.get(i));
-      getMatchCount(lottoLists.get(i), inputList);//각 로또마다 매치 개수 구하기
+      getMatchCount(lottoLists.get(i), inputList,bonusBall);//각 로또마다 매치 개수 구하기
     }
   }
 
-  public void getMatchCount(List<Integer> lotto, List<Integer> input){
-    //현재 하나의 로또에 대해서 매치 개수 구하는 것임
+  public boolean isBonusInLotto(List<Integer> lotto,int bonusBall){
+    if(lotto.contains(bonusBall)) return true;
+    return false;
+  }
+
+  public void updateRankMap(Rank rank){
+    int oldVal = rankMap.get(rank);
+    rankMap.put(rank,++oldVal);
+  }
+
+  public void getMatchCount(List<Integer> lotto, List<Integer> input,Integer bonusBall){
     int matchCnt = 0;
-    for(int num : lotto){
-      matchCnt = calcMatchCount(input,num,matchCnt);
-    }
-    System.out.println(matchCnt);
-    if(matchCnt == 3) match3++;
-    else if(matchCnt ==4) match4++;
-    else if(matchCnt == 5) match5++;
-    else if(matchCnt ==6) match6++;
-
+    boolean isBonus = isBonusInLotto(lotto,bonusBall);
+    for(int num : input) matchCnt = calcMatchCount(lotto,num,matchCnt);//하나의 로또에 대해 사용자의 각 숫자 중 몇 개가 일치되는 것인지 카운트
+    if(matchCnt == 3) updateRankMap(Rank.FIFTH);
+    else if(matchCnt ==4) updateRankMap(Rank.FOURTH);
+    else if(matchCnt ==5 && isBonus) updateRankMap(Rank.SECOND);
+    else if(matchCnt == 5) updateRankMap(Rank.THIRD);
+    else if(matchCnt ==6) updateRankMap(Rank.FIRST);
   }
 
   public int calcMatchCount(List<Integer> list, Integer num, int resMatchCnt){
     if(list.contains(num)) resMatchCnt++;
     return resMatchCnt;
   }
-  public void printResult(){
+
+  public void calcRate(int lottoCnt){
+    double sumOfWin = rankMap.get(Rank.FIFTH) * Rank.FIFTH.getWinningMoney() + rankMap.get(Rank.FOURTH) * Rank.FOURTH.getWinningMoney()
+                    + rankMap.get(Rank.THIRD) * Rank.THIRD.getWinningMoney() + rankMap.get(Rank.SECOND) * Rank.SECOND.getWinningMoney()
+                    + rankMap.get(Rank.FIRST) * Rank.FIRST.getWinningMoney();
+    double cost = lottoCnt*1000;
+    System.out.println("총 수익률은 "+String.format("%.2f",((sumOfWin - cost) / cost) * 100)+"% 입니다");
+  }
+
+  public void printResult(int lottoCnt){
     System.out.println("당첨 통계");
     System.out.println("-----");
-    System.out.println("3개 일치 (5000원) - " + match3);
-    System.out.println("4개 일치 (50000원) - " + match4);
-    System.out.println("5개 일치 (1500000원) - " + match5);
-    System.out.println("6개 일치 (2000000000원) - " + match6);
-
-    double sumOfWin = match3 * 5000 + match4 * 50000 + match5 * 1500000 + match6 * 2000000000;
-    double cost = lottoCnt*1000;
-
-    System.out.println(String.format("%.2f",((sumOfWin - cost) / cost) * 100)+"%");
-
+    System.out.println("3개 일치 (5000원) - " + rankMap.get(Rank.FIFTH));
+    System.out.println("4개 일치 (50000원) - " + rankMap.get(Rank.FOURTH));
+    System.out.println("5개 일치 (1500000원) - " + rankMap.get(Rank.THIRD));
+    System.out.println("5개 일치, 보너스 볼 일치 (30000000원) - " + rankMap.get(Rank.SECOND));
+    System.out.println("6개 일치 (2000000000원) - " + rankMap.get(Rank.FIRST));
+    calcRate(lottoCnt);
   }
 
 }
@@ -121,8 +121,9 @@ class Lotto{
 public abstract class Main {
 
   public static void main(String[] args) {
-    Lotto lotto = new Lotto();
+    LottoMachine machine = new LottoMachine();
+    User user = new User();
+    Lotto lotto = new Lotto(machine,user);
     lotto.start();
   }
-
 }
