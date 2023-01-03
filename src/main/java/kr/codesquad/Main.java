8d9041938ee895beeddd127a9848bc4b37121c80
Kimtaewan <65708461+ktykty0722@users.noreply.github.com>
2023-01-04 08:39:21 +0900

    [김태완-로또 1단계] 리팩토링 (#32)
    * 입출력을 담당하는 Console 추가 후 입출력 메소드 이동, Main에 있던 Lotto 관련 기능들 Lotto 메소드로 이동
    
    * 코드 Reformat
    
    * 당첨 번호 Lotto 클래스로 사용하도록 수정
    
    * fixed untracked files
    
    * fix gitignore

--- src/main/java/kr/codesquad/Main.java
+++ src/main/java/kr/codesquad/Main.java
@@ -1,63 +1,23 @@
 package kr.codesquad;
 
-import java.util.ArrayList;
+import kr.codesquad.io.Console;
+
 import java.util.List;
-import java.util.Scanner;
 
 public class Main {
-    public static Scanner sc = new Scanner(System.in);
-    public static int money;
-    public static List<Lotto> allLotto = new ArrayList<>();
-    public static String winNumInput;
-    public static WinNum winNum;
-    public static int[] correctCnt = new int[7];
-    public static int[] reward = {0, 0, 0, 5000, 50000, 1500000, 2000000000};
+    private static Console console = new Console();
+    private static Lotto lotto = new Lotto();
+    public static final int LOTTO_PRICE = 1000;
+
     public static void main(String[] args) {
-        inputMoney();
-        makeLottoNum();
-        showLottoNum();
-        inputWinNum();
-        lottoResult();
-        showLottoResult();
+        int money = console.inputMoney();
+        int lottoAmount = money / LOTTO_PRICE;
+        console.printAmount(lottoAmount);
+        List<Lotto> allLotto = lotto.makeLottoNum(lottoAmount);
+        console.printLottoNum(allLotto);
+        Lotto winNum = console.inputWinNum();
+        int[] correctCnt = lotto.lottoResult(winNum, allLotto);
+        console.printLottoResult(money, correctCnt);
     }
 
-    static void inputMoney() {
-        System.out.println("구입금액을 입력해 주세요.");
-        money = sc.nextInt();
-        System.out.println(money/1000 + "개를 구매했습니다.");
-    }
-    static void makeLottoNum() {
-        for(int i=0;i<money/1000;i++) {
-            Lotto lotto = new Lotto();
-            allLotto.add(lotto);
-        }
-    }
-    static void showLottoNum() {
-        for(int i=0;i<allLotto.size();i++) {
-            System.out.println(allLotto.get(i).numberList);
-        }
-        System.out.println();
-    }
-    static void inputWinNum() {
-        sc.nextLine();
-        System.out.println("당첨 번호를 입력해 주세요.");
-        winNumInput = sc.nextLine();
-        winNum = new WinNum(winNumInput);
-    }
-    static void lottoResult() {
-        for(int i=0;i<allLotto.size();i++) {
-            correctCnt[allLotto.get(i).correctNumCnt(winNum)]++;
-        }
-        System.out.println();
-    }
-    static void showLottoResult() {
-        System.out.println("당첨 통계\n---------");
-        int rewardSum = 0;
-        for (int i=3;i<=6;i++) {
-            System.out.println(i + "개 일치 (" + reward[i] + ") - " + correctCnt[i] + "개");
-            rewardSum += reward[i] * correctCnt[i];
-        }
-        double ans = (((double) (rewardSum - money)) / (double) money) * 100.0;
-        System.out.println("총 수익률은 " + Math.floor(ans * 100) / 100.0 + "%입니다.");
-    }
 }
