e2338a8b889028c53dcf540355896478003cd05b
i5046821854 <o5046821854@gmail.com>
2023-01-03 14:49:15 +0900

    day2 - (5) 당첨 여부 판독, 당첨금 확인의 로직을 담당하는 CalculateMatch 클래스 생성

---
+++ src/main/java/kr/codesquad/CalculateMatch.java
@@ -0,0 +1,21 @@
+package kr.codesquad;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class CalculateMatch {
+
+    private ArrayList<ArrayList<Integer>> randomLottoList;
+    private ArrayList<Integer> myLottoList;
+    private static HashMap<Integer, Integer> hitNums = new HashMap<>();
+    private static int bonusNum;
+    private static int BONUM_NUM = 777;
+    private static Fee[] feeList = {Fee.FIRST, Fee.SECOND, Fee.THIRD, Fee.FOURTH, Fee.FIFTH};
+
+    public CalculateMatch(ArrayList<ArrayList<Integer>> randomLottoList, ArrayList<Integer> myLottoList, int bonusNum) {
+        this.randomLottoList = randomLottoList;
+        this.myLottoList = myLottoList;
+        this.bonusNum = bonusNum;
+    }
+
+}
