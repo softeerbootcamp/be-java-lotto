76d7cb81c0fc23cf3920f45d9c023d082cde609e
rentalhub-mac78 <kksy2423@naver.com>
2023-01-03 17:13:34 +0900

    Feat: WinningLotto에 보너스번호 추가

--- src/main/java/kr/codesquad/model/WinningLotto.java
+++ src/main/java/kr/codesquad/model/WinningLotto.java
@@ -3,7 +3,15 @@ package kr.codesquad.model;
 import java.util.List;
 
 public class WinningLotto extends Lotto {
-    public WinningLotto(List<Integer> winningNumbers) {
+
+    private final int bonusBall;
+
+    public WinningLotto(List<Integer> winningNumbers, int bonusBall) {
+        this.bonusBall = bonusBall;
         this.numbers = winningNumbers;
+    }
+
+    public int getBonusBall() {
+        return bonusBall;
     }
 }
