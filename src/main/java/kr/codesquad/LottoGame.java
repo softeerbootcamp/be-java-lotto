271ed4cc6b2a05fefe64e2c200dded4b119a04bd
roopre <benny1020@naver.com>
2023-01-03 10:58:28 +0900

    [Fix] start 메소드에서 로또 구매 메소드랑 추첨 메소드 분리

--- src/main/java/kr/codesquad/LottoGame.java
+++ src/main/java/kr/codesquad/LottoGame.java
@@ -1,9 +1,6 @@
 package kr.codesquad;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class LottoGame {
@@ -11,7 +8,7 @@ public class LottoGame {
     private final static int LOTTO_PRICE = 1000;
     private final static int LOTTO_NUMBER_COUNT = 6;
 
-    private static ArrayList<Integer> result = new ArrayList<>(LOTTO_NUMBER_COUNT);
+    private static ArrayList<Integer> result;
     private static int firstWin = 0;
     private static int secondWin = 0;
     private static int thirdWin = 0;
@@ -22,13 +19,14 @@ public class LottoGame {
     public void start() {
         double purchaseAmount = getPurchaseAmount();
         int purchaseLottoCount = getLottoCount(purchaseAmount);
-        ArrayList<Lotto> purchasedLottoList = purchaseLottoList(purchaseLottoCount);
+        List<Lotto> purchasedLottoList = purchaseLottoList();
         Lotto winningLotto = createWinningLottery();
         compareLotto(purchasedLottoList, winningLotto);
         printResult(purchaseLottoCount);
     }
 
     public LottoGame() {
+        result = new ArrayList<>(Arrays.asList(0, 0, 0, 0, 0, 0, 0));
         lottoNumbers = new ArrayList<>();
         for (int i = 1; i <= 45; i++) {
             lottoNumbers.add(i);
@@ -41,21 +39,11 @@ public class LottoGame {
         for (int num : purchasedLotto.getNumbers()) {
             matchNum = setMatchNum(winningLotto, matchNum, num);
         }
-        if (matchNum == LOTTO_NUMBER_COUNT) {
-            firstWin += 1;
-        }
-        if (matchNum == LOTTO_NUMBER_COUNT - 1) {
-            secondWin += 1;
-        }
-        if (matchNum == LOTTO_NUMBER_COUNT - 2) {
-            thirdWin += 1;
-        }
-        if (matchNum == LOTTO_NUMBER_COUNT - 3) {
-            fourthWin += 1;
-        }
+
+        result.set(matchNum, result.get(matchNum) + 1);
     }
 
-    private static int setMatchNum(Lotto winningLotto, int matchNum, int num) {
+    public int setMatchNum(Lotto winningLotto, int matchNum, int num) {
         if (winningLotto.getNumbers().contains(num)) {
             matchNum += 1;
         }
@@ -65,14 +53,14 @@ public class LottoGame {
     public void printResult(int purchaseAmount) {
         System.out.println("당첨 통계");
         System.out.println("-----");
-        System.out.println("3개 일치 (5000원) - " + fourthWin);
-        System.out.println("4개 일치 (50000원) - " + thirdWin);
-        System.out.println("5개 일치 (1500000원) - " + secondWin);
-        System.out.println("6개 일치 (2000000000원) - " + firstWin);
+        System.out.println("3개 일치 (5000원) - " + result.get(3));
+        System.out.println("4개 일치 (50000원) - " + result.get(4));
+        System.out.println("5개 일치 (1500000원) - " + result.get(5));
+        System.out.println("6개 일치 (2000000000원) - " + result.get(6));
         double output = fourthWin * 5000 + thirdWin * 50000 + secondWin * 1500000 + firstWin * 2000000000;
         double input = purchaseAmount * 1000;
 
-        System.out.println(String.format("%.2f",((output - input) / input) * 100)+"%");
+        System.out.println(String.format("%.2f", ((output - input) / input) * 100) + "%");
     }
 
     public void compareLotto(List<Lotto> purchasedLottoList, Lotto winningLotto) {
@@ -102,7 +90,10 @@ public class LottoGame {
         return lottoCount;
     }
 
-    public ArrayList<Lotto> purchaseLottoList(int lottoCount) {
+    public ArrayList<Lotto> purchaseLottoList() {
+        double purchaseAmount = getPurchaseAmount();
+        int lottoCount = getLottoCount(purchaseAmount);
+
         ArrayList<Lotto> lottoList = new ArrayList<Lotto>();
         for (int i = 0; i < lottoCount; i++) {
             lottoList.add(getNewLotto());
