861eb290f39b7ab4e188ad538674dff82a6f603b
macbook16 <macbook16@macbook16ui-noteubug.local>
2023-01-03 12:03:49 +0900

    일치 개수를 통해 2등 카운트, 출력 함수 수정

--- src/main/java/kr/codesquad/Main.java
+++ src/main/java/kr/codesquad/Main.java
@@ -9,10 +9,12 @@ class Lotto{
   private int lottoCnt;
   private int bonusBall;
   List<Integer> numList;
-  int match3=0;
-  int match4=0;
-  int match5=0;
-  int match6=0;
+  int match3;
+  int match4;
+  int match5;
+
+  int secondWin;
+  int match6;
 
   public Lotto() {
     lottos = new ArrayList<>();
@@ -22,6 +24,7 @@ class Lotto{
     match3=0;
     match4=0;
     match5=0;
+    secondWin=0;
     match6=0;
   }
 
@@ -37,6 +40,7 @@ class Lotto{
     //5. 보너스 볼 입력 받기
     this.bonusBall = getBonusLottoInput();
     System.out.println(this.bonusBall);
+    System.out.println(inputs);
     //5. 비교
     compareLottos(lottos,inputs);
     //6. 결과 출력
@@ -77,13 +81,13 @@ class Lotto{
     for(String input : inputNumbers.split(", ")){
       inputs.add(Integer.valueOf(input));
     }
-    //System.out.println(inputs);
   }
 
   public Integer getBonusLottoInput() {
     System.out.println("보너스 볼을 입력해주세요. ");
     Scanner sc = new Scanner(System.in);
     int bonusBall = sc.nextInt();
+    //inputs.add(bonusBall);
     return bonusBall;
   }
 
@@ -94,18 +98,21 @@ class Lotto{
     }
   }
 
+  public boolean isBonusInLotto(List<Integer> lotto){
+    if(lotto.contains(bonusBall)) return true;
+    return false;
+  }
+
   public void getMatchCount(List<Integer> lotto, List<Integer> input){
-    //현재 하나의 로또에 대해서 매치 개수 구하는 것임
     int matchCnt = 0;
-    for(int num : lotto){
-      matchCnt = calcMatchCount(input,num,matchCnt);
-    }
-    System.out.println(matchCnt);
+    boolean isBonus = isBonusInLotto(lotto);
+    for(int num : input) matchCnt = calcMatchCount(lotto,num,matchCnt);//하나의 로또에 대해 사용자의 각 숫자 중 몇 개가 일치되는 것인지 카운트
+    //System.out.println(matchCnt);
     if(matchCnt == 3) match3++;
     else if(matchCnt ==4) match4++;
+    else if(matchCnt ==5 && isBonus) secondWin++;
     else if(matchCnt == 5) match5++;
     else if(matchCnt ==6) match6++;
-
   }
 
   public int calcMatchCount(List<Integer> list, Integer num, int resMatchCnt){
@@ -118,6 +125,7 @@ class Lotto{
     System.out.println("3개 일치 (5000원) - " + match3);
     System.out.println("4개 일치 (50000원) - " + match4);
     System.out.println("5개 일치 (1500000원) - " + match5);
+    System.out.println("5개 일치, 보너스 볼 일치 (30000000원) - " + secondWin);
     System.out.println("6개 일치 (2000000000원) - " + match6);
 
     double sumOfWin = match3 * 5000 + match4 * 50000 + match5 * 1500000 + match6 * 2000000000;
