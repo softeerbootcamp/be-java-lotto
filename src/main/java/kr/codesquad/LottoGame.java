32a272300019e77590c99181e399806790cf1177
rohsik2 <rohsik2@gmail.com>
2023-01-03 14:22:00 +0900

    feat(LottoGame): make game as a class

---
+++ src/main/java/kr/codesquad/LottoGame.java
@@ -0,0 +1,51 @@
+package kr.codesquad;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LottoGame {
+    private List<Lotto> lottos;
+    private LottoStat lottoStat;
+
+    private Lotto winning;
+    private int bonusNo;
+    public LottoGame(){
+        lottos = new ArrayList<>();
+    }
+    public void start(){
+        buyLottos();
+        getWinning();
+        printResult();
+    }
+
+    public void printResult(){
+        lottoStat = new LottoStat(lottos, winning, bonusNo);
+        lottoStat.printResult();
+    }
+
+    public void getWinning(){
+        List<Integer> winningNumbers = UserInputHandler.getSixNumber();
+        bonusNo = UserInputHandler.getBonusNumber(winningNumbers);
+        winning = new Lotto(winningNumbers);
+    }
+
+
+    public void buyLottos(){
+        int n_lotto = UserInputHandler.getMoney() / 1000;
+        int n_left_lotto = n_lotto - buyManualLotto(n_lotto, lottos);
+        buyAutoLotto(n_left_lotto, lottos);
+    }
+
+    public void buyAutoLotto(int n_lotto, List<Lotto> lottos){
+        for(int i =0; i<n_lotto; i++){
+            lottos.add(new Lotto());
+        }
+        System.out.println(String.format("%d개를 구매했습니다.", lottos.size()));
+    }
+
+    public int buyManualLotto(int max_lotto, List<Lotto> lottos){
+        List<Lotto> lottoList = UserInputHandler.buyManualLotto(max_lotto);
+        lottos.addAll(lottoList);
+        return lottoList.size();
+    }
+}
