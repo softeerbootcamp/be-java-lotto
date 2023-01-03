857ab5da01ce86e5ef91e731df165ad3f8ff5291
pjs0418 <tony180322@gmail.com>
2023-01-03 10:54:04 +0900

    LottoSystem Class 생성자 구현

--- src/main/java/kr/codesquad/LottoSystem.java
+++ src/main/java/kr/codesquad/LottoSystem.java
@@ -1,4 +1,13 @@
 package kr.codesquad;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class LottoSystem {
+    private final List<Integer> numList;
+
+    public LottoSystem() {
+        this.numList = new ArrayList<>();
+        for(int i = 1;i <= 45;i++) this.numList.add(i);
+    }
 }
