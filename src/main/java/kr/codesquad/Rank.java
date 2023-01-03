a8b01db09e23d41bc408279a4ff654650b2d576d
jieonkim23 <kwldjs1023@gmail.com>
2023-01-03 18:27:53 +0900

    Rank의 words인스턴스(출력부분에 나오는 일치여부)를 추가: 출력시 용이하게 하기 위해 Rank에 일치 설명 string 인스턴스를 추가하였음.

--- src/main/java/kr/codesquad/Rank.java
+++ src/main/java/kr/codesquad/Rank.java
@@ -1,18 +1,20 @@
 package kr.codesquad;
 
 public enum Rank {
-    FIRST(6, 2000000000),
-    SECOND(5, 30000000),
-    THIRD(5, 1500000),
-    FOURTH(4, 50000),
-    FIFTH(3, 5000);
+    FIRST(6, 2000000000, "6개 일치"),
+    SECOND(5, 30000000, "5개 일치, 보너스 볼 일치"),
+    THIRD(5, 1500000, "5개 일치"),
+    FOURTH(4, 50000, "4개 일치"),
+    FIFTH(3, 5000, "3개 일치");
 
     private  final int cnt;
     private  final int prize;
+    private final String words;
 
-    Rank(int cnt, int prize) {
+    Rank(int cnt, int prize, String words) {
         this.cnt = cnt;
         this.prize = prize;
+        this.words = words;
     }
     public int getCnt(){
         return cnt;
@@ -20,6 +22,7 @@ public enum Rank {
     public int getPrize(){
         return prize;
     }
+    public String getWords(){ return words; }
     public static Rank valueOf(int cnt, boolean bonus) {
         if(cnt < 3) return null;
         Rank[] ranks = values();
