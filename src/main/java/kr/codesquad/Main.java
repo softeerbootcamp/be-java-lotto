2e0f8e80160414e2bcd58d1b0cda0a0d1f6e32cd
roopre <benny1020@naver.com>
2023-01-03 15:27:55 +0900

    [Refactor] 컨벤션 맞추기

--- src/main/java/kr/codesquad/Main.java
+++ src/main/java/kr/codesquad/Main.java
@@ -1,16 +1,15 @@
 package kr.codesquad;
 
 import java.util.List;
-import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
-        LottoGame lottoGame = new LottoGame();
-        List<Lotto> purchasedLottoList = lottoGame.purchaseLottoList();
-        Lotto winningLotto = lottoGame.createWinningLottery();
-        int bonusBall = lottoGame.getBonusBall();
-        LottoResult lottoResult = lottoGame.checkMyLotto(purchasedLottoList, winningLotto, bonusBall);
-        lottoResult.printResult(purchasedLottoList.size());
-    }
+	public static void main(String[] args) {
+		LottoGame lottoGame = new LottoGame();
+		List<Lotto> purchasedLottoList = lottoGame.purchaseLottoList();
+		Lotto winningLotto = lottoGame.createWinningLottery();
+		int bonusBall = lottoGame.getBonusBall();
+		LottoResult lottoResult = lottoGame.checkMyLotto(purchasedLottoList, winningLotto, bonusBall);
+		lottoResult.printResult(purchasedLottoList.size());
+	}
 }
