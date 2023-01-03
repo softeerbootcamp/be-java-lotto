5cbcce0095c67cf43ff76ac496789bb3d179a078
ktykty0722 <ktykty0722@naver.com>
2023-01-03 17:03:49 +0900

    Lotto 내부 메소드 삭제

--- src/main/java/kr/codesquad/Lotto.java
+++ src/main/java/kr/codesquad/Lotto.java
@@ -1,53 +1,16 @@
 package kr.codesquad;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 public class Lotto {
-    public static final int LOTTO_NUM_BOUND = 45;
-    public static final int LOTTO_NUM_COUNT = 6;
+
     public List<Integer> numberList = new ArrayList<>();
 
     public int correctNumCnt(Lotto winNum) {
         List<Integer> temp = this.numberList;
         temp.retainAll(winNum.numberList);
         return temp.size();
-    }
-
-    public List<Lotto> makeLottoNum(int amount) {
-        List<Lotto> lottoList = new ArrayList<>();
-        for (int i = 0; i < amount; i++) {
-            Lotto lotto = makeRandomLotto();
-            lottoList.add(lotto);
-        }
-        return lottoList;
-    }
-
-    public Lotto makeRandomLotto() {
-        Lotto lotto = new Lotto();
-        lotto.numberList.addAll(makeRandomNum());
-        return lotto;
-    }
-
-    private List<Integer> makeRandomNum() {
-        List<Integer> allNumber = new ArrayList<>();
-        for (int i = 1; i <= LOTTO_NUM_BOUND; i++) {
-            allNumber.add(i);
-        }
-        Collections.shuffle(allNumber);
-        List<Integer> randNum = new ArrayList<>();
-        randNum.addAll(allNumber.subList(0, LOTTO_NUM_COUNT));
-        Collections.sort(randNum);
-        return randNum;
-    }
-
-    public int[] lottoResult(Lotto winNum, List<Lotto> lottoList) {
-        int[] correctCnt = new int[7];
-        for (int i = 0; i < lottoList.size(); i++) {
-            correctCnt[lottoList.get(i).correctNumCnt(winNum)]++;
-        }
-        return correctCnt;
     }
 
 }
