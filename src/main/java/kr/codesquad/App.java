fa123d77ad990bdc0824a2576804bb7973fc7e26
waterfog9580 <waterfog9580@gmail.com>
2023-01-03 11:27:54 +0900

    :recycle: WinnerChecker 제거에 따른 리팩토링

--- src/main/java/kr/codesquad/App.java
+++ src/main/java/kr/codesquad/App.java
@@ -4,49 +4,43 @@ import java.util.List;
 
 import kr.codesquad.domain.earningRate.EarningRate;
 import kr.codesquad.domain.lotto.Lotto;
-import kr.codesquad.domain.lotto.LottoShop;
-import kr.codesquad.domain.winningLotto.WinningLotto;
-import kr.codesquad.domain.winningLotto.WinningResult;
+import kr.codesquad.domain.lotto.LottoFactory;
+import kr.codesquad.domain.winningResult.WinningResult;
 import kr.codesquad.io.Console;
 
 public class App implements Runnable {
 
   private final Console console;
-  private final LottoShop lottoShop;
+  private final LottoFactory lottoFactory;
 
   public App(
       Console console,
-      LottoShop lottoShop
+      LottoFactory lottoFactory
   ) {
     this.console = console;
-    this.lottoShop = lottoShop;
+    this.lottoFactory = lottoFactory;
   }
+
+  public static final int LOTTO_PRICE = 1_000;
 
   @Override
   public void run() {
-    int purchaseMoney = console.inputPurchaseMoney();
-    List<Lotto> lottos = purchaseLotto(purchaseMoney);
+    console.printInputAmount();
+    int purchaseAmount = console.inputPurchaseAmount();
+    int purchaseCount = purchaseAmount / LOTTO_PRICE;
+    console.printPurchaseCount(purchaseCount);
+    List<Lotto> lottos = lottoFactory.generateList(purchaseCount);
 
-    WinningLotto winningLotto = inputWinningLotto();
-    WinningResult winningResult = WinningResult.createResult(lottos, winningLotto);
+    console.printLottoNumbersList(lottos);
+    console.printInputWinningNumber();
+    Lotto winningNumber = console.inputWinningNumbers();
+
+    WinningResult winningResult = new WinningResult();
+    winningResult.calculateResult(lottos, winningNumber);
     console.printWinningResult(winningResult);
 
-    EarningRate earningRate = EarningRate.of(winningResult.getTotalWinningMoney(), purchaseMoney);
+    EarningRate earningRate = EarningRate.of(winningResult.getTotalWinningMoney(), purchaseAmount);
     console.printEarningRate(earningRate);
-  }
-
-  private List<Lotto> purchaseLotto(int money) {
-    List<Lotto> lottos = lottoShop.purchase(money);
-    console.printPurchaseCount(lottos.size());
-    console.printLottoNumbersList(lottos);
-    return lottos;
-  }
-
-  private WinningLotto inputWinningLotto() {
-    List<Integer> winningNumbers = console.inputWinningNumbers();
-    Lotto winningLottoNumbers = Lotto.from(winningNumbers);
-    int bonusNumber = console.inputBonusNumber();
-    return new WinningLotto(winningLottoNumbers, bonusNumber);
   }
 
 }
--- src/main/java/kr/codesquad/Main.java
+++ src/main/java/kr/codesquad/Main.java
@@ -1,13 +1,12 @@
 package kr.codesquad;
 
 import kr.codesquad.domain.lotto.LottoFactory;
-import kr.codesquad.domain.lotto.LottoShop;
 import kr.codesquad.io.Console;
 
 public class Main {
 
   public static void main(String[] args) {
-    App app = new App(new Console(), new LottoShop(new LottoFactory()));
+    App app = new App(new Console(), new LottoFactory());
     app.run();
   }
 
