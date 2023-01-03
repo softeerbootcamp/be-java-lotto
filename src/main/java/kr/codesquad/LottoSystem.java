6fa143682a590492f76c32d80959b4b5759d9509
pjs0418 <tony180322@gmail.com>
2023-01-03 11:26:22 +0900

    당첨된 번호의 개수를 구하는 countWinNum 함수 구현

--- src/main/java/kr/codesquad/LottoSystem.java
+++ src/main/java/kr/codesquad/LottoSystem.java
@@ -8,6 +8,8 @@ import java.util.Scanner;
 public class LottoSystem {
     private final List<Integer> numList;
 
+    private static int lottoCount;
+
     public LottoSystem() {
         this.numList = new ArrayList<>();
         for(int i = 1;i <= 45;i++) this.numList.add(i);
@@ -16,16 +18,16 @@ public class LottoSystem {
     public Lotto buyLotto() {
         System.out.println("구입금액을 입력해 주세요.");
         Scanner sc = new Scanner(System.in);
-        int count = sc.nextInt() / 1000;
-        System.out.println(count + "개를 구매했습니다.");
+        lottoCount = sc.nextInt() / 1000;
+        System.out.println(lottoCount + "개를 구매했습니다.");
 
-        return new Lotto(shuffle(count));
+        return new Lotto(shuffle(lottoCount));
     }
 
-    public List<List<Integer>> shuffle(int count) {
+    public List<List<Integer>> shuffle(int lottoCount) {
         List<List<Integer>> lottoList = new ArrayList<>();
 
-        for(int i = 0;i < count;i++) {
+        for(int i = 0;i < lottoCount;i++) {
             Collections.shuffle(numList);
             List<Integer> subList = new ArrayList<>(numList.subList(0, 6));
 
@@ -45,5 +47,23 @@ public class LottoSystem {
 
         List<Integer> winNumList = new ArrayList<>();
         for(int i = 0;i < 6;i++) winNumList.add(Integer.parseInt(temp[i]));
+
+        for(int i = 0; i < lottoCount; i++) {
+            int winNumCount = countWinNum(lotto, winNumList, i);
+        }
+    }
+
+    public int countWinNum(Lotto lotto, List<Integer> winNumList, int index) {
+        List<List<Integer>> lottoList = lotto.getLottoList();
+        int winNumCount = 0;
+
+        if(lottoList.get(index).contains(winNumList.get(0))) winNumCount++;
+        if(lottoList.get(index).contains(winNumList.get(1))) winNumCount++;
+        if(lottoList.get(index).contains(winNumList.get(2))) winNumCount++;
+        if(lottoList.get(index).contains(winNumList.get(3))) winNumCount++;
+        if(lottoList.get(index).contains(winNumList.get(4))) winNumCount++;
+        if(lottoList.get(index).contains(winNumList.get(5))) winNumCount++;
+
+        return winNumCount;
     }
 }
