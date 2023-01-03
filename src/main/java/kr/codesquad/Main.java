51b43fd2743646320ddbabb91520cfbf25c96ed3
pjs0418 <33286894+pjs0418@users.noreply.github.com>
2023-01-04 08:38:42 +0900

    1주차 로또 1단계 - 기본 기능 구현 완료 (#31)
    * LottoSystem Class 생성
    
    * LottoSystem Class 생성자 구현
    
    * Lotto Class 생성
    
    * Lotto Class 생성자 구현
    
    * 발급받은 로또 번호에 대한 정보를 불러오는 getLottoList 함수 구현
    
    * buyLotto 함수에서 구입 금액 입력받는 부분 구현
    
    * 자동으로 로또 번호를 발급하는 기능의 shuffle 함수 구현
    
    * checkLotto 함수에서 당첨 금액 입력받는 부분 구현
    
    * 당첨된 번호의 개수를 구하는 countWinNum 함수 구현
    
    * Main 정리
    
    * 로또 당첨 결과 저장하는 setResult 함수 구현
    
    * 당첨 결과 출력하는 printResult 함수 구현
    
    * 수익률 소수점 아래 2자리까지 출력되도록 수정
    
    * 수익률 퍼센트로 나올 수 있도록 수정
    
    * 로또 당첨 결과 관리하는 함수 조건문 수정

--- src/main/java/kr/codesquad/Main.java
+++ src/main/java/kr/codesquad/Main.java
@@ -1,65 +1,9 @@
 package kr.codesquad;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Scanner;
-
 public class Main {
-    static List<List<Integer>> lottoList = new ArrayList<>();
-    static List<Integer> winNumList;
-
     public static void main(String[] args) {
-        int count = getLottoCount();
-        System.out.println(count + "개를 구매했습니다.");
-
-        shuffle(count);
-        inputWinNum();
-
-        for(int i = 0;i < count;i++) {
-            int winCount = countWin(i);
-        }
-    }
-
-    public static int getLottoCount() {
-        System.out.println("구입금액을 입력해 주세요.");
-        Scanner sc = new Scanner(System.in);
-        return sc.nextInt() / 1000;
-    }
-
-    public static void shuffle(int count) {
-        List<Integer> numList = new ArrayList<>();
-        for(int i = 1;i <= 45;i++) numList.add(i);
-
-        for(int i = 0;i < count;i++) {
-            Collections.shuffle(numList);
-            List<Integer> subList = new ArrayList<>(numList.subList(0, 6));
-            lottoList.add(subList);
-            Collections.sort(lottoList.get(i));
-            System.out.println(lottoList.get(i));
-        }
-    }
-
-    public static void inputWinNum() {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("당첨 번호를 입력해 주세요.");
-        String winNum = sc.nextLine();
-        String[] temp = winNum.split(", ");
-
-        winNumList = new ArrayList<>();
-        for(int i = 0;i < 6;i++) winNumList.add(Integer.parseInt(temp[i]));
-    }
-
-    public static int countWin(int index) {
-        int winCount = 0;
-
-        if(lottoList.get(index).contains(winNumList.get(0))) winCount++;
-        if(lottoList.get(index).contains(winNumList.get(1))) winCount++;
-        if(lottoList.get(index).contains(winNumList.get(2))) winCount++;
-        if(lottoList.get(index).contains(winNumList.get(3))) winCount++;
-        if(lottoList.get(index).contains(winNumList.get(4))) winCount++;
-        if(lottoList.get(index).contains(winNumList.get(5))) winCount++;
-
-        return winCount;
+        LottoSystem lottoSystem = new LottoSystem();
+        Lotto lotto = lottoSystem.buyLotto();
+        lottoSystem.checkLotto(lotto);
     }
 }
