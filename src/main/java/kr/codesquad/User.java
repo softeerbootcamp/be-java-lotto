e5c43d90a842fae9ba13dccf2f5e0793bdb5ac12
macbook16 <macbook16@macbook16ui-noteubug.local>
2023-01-03 16:44:01 +0900

    사용자 입력한 숫자에 대하여 처리 구현

--- src/main/java/kr/codesquad/User.java
+++ src/main/java/kr/codesquad/User.java
@@ -8,24 +8,24 @@ public class User{
     private ArrayList<Integer> inputs;
     private int bonusBall;
 
-
     public User() {
         inputs = new ArrayList<>();
     }
 
-    public void getLottoInput(){
+    public ArrayList<Integer> getLottoInput(){
         System.out.println("당첨 번호를 입력해주세요.");
         Scanner sc = new Scanner(System.in);
         String inputNumbers = sc.nextLine();
         for(String input : inputNumbers.split(", ")){
             inputs.add(Integer.valueOf(input));
         }
+        return inputs;
     }
 
     public Integer getBonusLottoInput() {
         System.out.println("보너스 볼을 입력해주세요. ");
         Scanner sc = new Scanner(System.in);
-        int bonusBall = sc.nextInt();
+        this.bonusBall = sc.nextInt();
         return bonusBall;
     }
 
