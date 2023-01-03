90604ffd133851b15c3dbdf76bfb01727d8fa2ce
ajongs <wonjong1030@naver.com>
2023-01-03 10:17:27 +0900

    feat(OutputView): 얻은 수익 계산 기능 구현

--- src/main/java/kr/codesquad/OutputView.java
+++ src/main/java/kr/codesquad/OutputView.java
@@ -25,4 +25,14 @@ public class OutputView {
     public void printWinningNumberText() {
         System.out.println("당첨 번호를 입력해 주세요.");
     }
+
+    
+    public void printEarnedRate(double rate) {
+        System.out.println(rate);
+        if (rate < 0) {
+            System.out.println("총 수익률은 " + (Math.ceil(rate * 100) / 100) + "%입니다.");
+            return;
+        }
+        System.out.println("총 수익률은 " + (Math.floor(rate * 100) / 100) + "%입니다.");
+    }
 }
