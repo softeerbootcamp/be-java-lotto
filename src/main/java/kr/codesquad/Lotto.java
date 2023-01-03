71d0f6793219fc08a06e409226dc4824b6267b11
ajongs <wonjong1030@naver.com>
2023-01-03 10:23:48 +0900

    feat(Lotto): 로또 통계를 관리하기 위한 result Map과 result Map의 키를 관리하는 resultIndex Map 멤버변수 추가

--- src/main/java/kr/codesquad/Lotto.java
+++ src/main/java/kr/codesquad/Lotto.java
@@ -1,15 +1,39 @@
 package kr.codesquad;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Lotto {
     private List<List<Integer>> numbers;
     private NumberGenerator numberGenerator;
+    private List<Integer> winningNumbers;
+    private Map<Integer, Integer> result;
+    private Map<Integer, Integer> resultIndex;
 
     Lotto(NumberGenerator numberGenerator) {
         this.numbers = new ArrayList<>();
         this.numberGenerator = numberGenerator;
+        result = new HashMap<>();
+        resultIndex = new HashMap<>();
+        resultIndex.put(3, 5000);
+        resultIndex.put(4, 50000);
+        resultIndex.put(5, 1500000);
+        resultIndex.put(6, 2000000000);
+
+        result.put(5000, 0);
+        result.put(50000, 0);
+        result.put(1500000, 0);
+        result.put(2000000000, 0);
+
+    }
+    public Map<Integer, Integer> getResult() {
+        return result;
+    }
+
+    public Map<Integer, Integer> getResultIndex() {
+        return resultIndex;
     }
     public long calculateCount(long money) {
         return money / 1000;
