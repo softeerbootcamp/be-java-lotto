e68492478c1f941e9bfbad0685f584ff969b8daa
River-Mt <rivermt1998@gmail.com>
2023-01-03 16:27:48 +0900

    Feat: add OutputView - 출력부문을 담당하는 OutputView 생성

---
+++ src/main/java/kr/codesquad/view/OutputView.java
@@ -0,0 +1,16 @@
+package kr.codesquad.view;
+
+public class OutputView {
+    public void showRequestTotalPrice() {
+        System.out.println("구입금액을 입력해 주세요.");
+    }
+
+    public void showLottoAmount(int amount) {
+        System.out.println(amount + "개를 구매했습니다.");
+    }
+
+    public void showLottoNumbers() {
+
+    }
+
+}
