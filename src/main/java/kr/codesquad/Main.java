65a65b2e7956acf6324f4be002ba8992d34ecd6e
i5046821854 <o5046821854@gmail.com>
2023-01-03 16:52:41 +0900

    day2 - (16) UserInput을 담당하는 클래스 생성

--- src/main/java/kr/codesquad/Main.java
+++ src/main/java/kr/codesquad/Main.java
@@ -3,46 +3,30 @@ package kr.codesquad;
 import java.util.*;
 
 
-public class Main {
+class UserInput {
 
-    private static ArrayList<Integer> allNums;
-    private static int purchasedPrice = 0;
-    private static int numOfLotto = 0;
-    private static Scanner scan;
-    private static int bonus = 0;
+    private Scanner scan;
+    private UserInfo user;
 
-    //initial method
-    private static void initMethod(){
-        allNums = new ArrayList<>();
+    public UserInput(UserInfo user){
+        this.user = user;
         scan = new Scanner(System.in);
-        for(int i = 1; i < 46; i++)
-            allNums.add(i);
     }
 
-    //generate random numbers of lotto
-    private static ArrayList<Integer> lottoGenerate() {
-        Collections.shuffle(allNums);
-        ArrayList<Integer> tempList = new ArrayList<Integer>();
-        for(int j = 0;  j < 6; j++)
-            tempList.add(allNums.get(j));
-        System.out.println(tempList);
-        return tempList;
-    }
-
-
-
-    //enter the total price of purchasing
-    private static int enterPurchasePrice(){
+    //구매 금액 입력
+    public void enterPurchasePrice(){
         System.out.println("구입 금액을 입력해 주세요.");
-        purchasedPrice = scan.nextInt();
+        int purchasedPrice = scan.nextInt();
+        int numOfLotto = purchasedPrice / 1000;
         scan.nextLine();
-        numOfLotto = purchasedPrice / 1000;
+        user.setPurchasedPrice(purchasedPrice);
+        user.setNumOfLotto(numOfLotto);
         System.out.println(numOfLotto + "개를 구매했습니다.");
-        return numOfLotto;
     }
 
-    //enter numbers of result
-    private static ArrayList<Integer> enterResultNumbers(){
+
+    //당첨 번호 입력
+    public ArrayList<Integer> enterResultNumbers(){
         System.out.println("\n당첨 번호를 입력해 주세요.");
         String numStr = scan.nextLine();
         List<String> split = Arrays.asList(numStr.split(","));
@@ -53,32 +37,46 @@ public class Main {
         return tempList;
     }
 
-    private static void enterBonusNum(){
+    //보너스 번호 입력
+    public void enterBonusNum(){
         System.out.println("보너스볼을 입력해 주세요.");
-        bonus = scan.nextInt();
+        int bonusNum = scan.nextInt();
+        user.setBonus(bonusNum);
+    }
+
+}
+
+public class Main {
+
+    //initial method
+    private static void initMethod(){
+
+        UserInfo user = new UserInfo();
+        UserInput userInput = new UserInput(user);
+        Lotto randomLotto = new Lotto();
+        Lotto myLotto = new Lotto();
+        CalculateMatch calculateMatch = new CalculateMatch();
+
+        //구매 금액 입력
+        userInput.enterPurchasePrice();
+        //로또 번호 생성 (랜덤)
+        for(int i = 0 ; i < user.getNumOfLotto(); i++)
+            randomLotto.addRandomLotto();
+        //당첨 번호 입력
+        myLotto.addMyLotto(userInput.enterResultNumbers());
+        //보너스 번호 입력
+        userInput.enterBonusNum();
+
+        //당첨 여부 확인
+        calculateMatch.startCalculate(randomLotto, myLotto, user.getNumOfLotto(), user.getBonus());
+        //결과 출력
+        calculateMatch.printResult(user.getPurchasedPrice());
+
     }
 
 
     public static void main(String[] args) {
         initMethod();
-        enterPurchasePrice();
-
-        Lotto randomLotto = new Lotto();
-        Lotto myLotto = new Lotto();
-
-        for(int i = 0 ; i < numOfLotto; i++){
-            randomLotto.addLotto(lottoGenerate());
-        }
-
-        myLotto.addLotto(enterResultNumbers());
-        enterBonusNum();
-
-        ArrayList<ArrayList<Integer>> randomLottoList = randomLotto.getLottoList();
-        ArrayList<Integer> myLottoList = myLotto.getLottoList().get(0);
-        CalculateMatch calculateMatch = new CalculateMatch(randomLottoList, myLottoList, bonus);
-
-        calculateMatch.startCalculate(numOfLotto);
-        calculateMatch.printResult(purchasedPrice);
     }
 
 
