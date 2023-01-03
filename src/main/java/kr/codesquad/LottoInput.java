9669c4aed37cf5144f75d15d7ca3789a7f1d4178
jieonkim23 <kwldjs1023@gmail.com>
2023-01-03 16:34:58 +0900

    당첨Lotto 입력함수중 로또 입력부분을 분리

--- src/main/java/kr/codesquad/LottoInput.java
+++ src/main/java/kr/codesquad/LottoInput.java
@@ -8,16 +8,19 @@ import java.util.stream.Collectors;
 import static kr.codesquad.LottoSystem.COST;
 
 public class LottoInput {
-    private static Scanner sc = new Scanner(System.in);
+    private static final Scanner sc = new Scanner(System.in);
     public static int enterMoney(){
         System.out.println("구입금액을 입력해주세요.");
         int money = Integer.parseInt(sc.nextLine());
         System.out.println(money / COST + "개를 구매했습니다.");
         return money;
     }
+
     public static List<Integer> enterWin(){
-        sc = new Scanner(System.in);
         System.out.println("\n당첨 번호를 입력해주세요.");
+        return enterLotto();
+    }
+    public static List<Integer> enterLotto(){
         List<String> s = Arrays.asList(sc.nextLine().replaceAll(" ", "").split(","));
         return s.stream().map(Integer::parseInt).collect(Collectors.toList());
     }
