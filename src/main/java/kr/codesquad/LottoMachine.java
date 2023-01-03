23fc452fd502e36793efc6a7e77f00b09135b014
sanghoon <54069713+sangho0n@users.noreply.github.com>
2023-01-04 08:51:43 +0900

    Step2 제출 (#43)
    * step1을 객체별로 구분하여 구현
    
    * 로또 추첨기에 보너스볼 추가
    보너스볼 입력받는 부분 추가
    유저가 2등일 때에도 수익률을 계산할 수 있도록 구현
    
    * 당첨 등수를 나타내는 열거체 생성
    
    * 결과 출력하는 부분을 enum을 사용하여 구현
    if문을 연달아서 사용하지 않아도 됨
    
    * printf 형식지정자 수정
    소숫점 아래 2자리까지 출력
    퍼센트 기호가 나타나도록 수정
    
    * main함수 가독성 향상
    main함수를 좀 더 읽기 편하도록 코드의 순서 조정
    
    * 규칙에 맞게 코드 수정
    들여쓰기 레벨 최대 1로 맞춤
    열거체 위치를 Main.java로 옮김
    
    * 주석 추가
    코드 이해를 쉽게 하기 위해 주석 추가
    
    * 규칙에 맞게 코드 수정
    로또 번호 입력받는 부분을 한 줄을 한번에 받도록 설정
    
    * 열거체를 잘 활용할 수 있도록 수정
    열거체의 변수 이름을  val1, val2가 아닌 실제 의미하는 것을 바탕으로 변수명 수정
    수익률 계산 시 열거체를 활용하도록 수정
    
    * 열거체 가독성 향상
    
    Co-authored-by: RENTALHUB-16M1MAX <rentalhub-16m1max@RENTALHUB-16M1MAXui-MacBookPro.local>

---
+++ src/main/java/kr/codesquad/LottoMachine.java
@@ -0,0 +1,85 @@
+package kr.codesquad;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+public class LottoMachine {
+    private ArrayList<Integer> winNums;
+    private int bonusBall;
+
+    public ArrayList<Integer> getWinNums() {
+        return winNums;
+    }
+
+    public void setWinNums(ArrayList<Integer> winNums) {
+        this.winNums = winNums;
+    }
+
+    public int getBonusBall() {
+        return bonusBall;
+    }
+
+    public void setBonusBall(int bonusBall) {
+        this.bonusBall = bonusBall;
+    }
+
+    public ArrayList<Integer> GetResult(ArrayList<ArrayList<Integer>> Tickets)
+    {
+        ArrayList<Integer> Result = new ArrayList<Integer>();
+        for(int i = 0; i < 5; i++)
+        {
+            // 초기값 설정
+            Result.add(0);
+        }
+
+        for(int i = 0; i < Tickets.size(); i++)
+        {
+            // 각 번호를 당첨 번호와 비교
+            CompareTicketWithWinNum(Tickets.get((i)), Result);
+        }
+        return Result;
+    }
+
+    private void CompareTicketWithWinNum(ArrayList<Integer> Ticket, ArrayList<Integer> Result)
+    {
+        // 당첨 번호와 같은 숫자의 수
+        int count = CountValidNums(Ticket);
+        // 보너스 번호가 있는지 없는지
+        boolean isBonus = JudgeBonus(Ticket);
+
+        // 위 두 정보를 이용하여 순위 결정
+        String rank = DecideRank(count, isBonus);
+
+        if(rank.equals("Out of rank")) return;
+
+        int EnumIdx = LottoStatus.valueOf(rank).ordinal();
+        Result.set(EnumIdx, Result.get(EnumIdx)+1);
+    }
+
+    private int CountValidNums(ArrayList<Integer> Ticket)
+    {
+        int ret = 0;
+        for(int j = 0; j < 6; j++)
+        {
+            if(Ticket.contains(winNums.get(j))) ret++;
+        }
+        return ret;
+    }
+
+    private boolean JudgeBonus(ArrayList<Integer> Ticket)
+    {
+        if(Ticket.contains(bonusBall))
+            return true;
+        return false;
+    }
+
+    private String DecideRank(int c, boolean isBonus)
+    {
+        if(c == 3)              return "Fifth";
+        if(c == 4)              return "Fourth";
+        if(c == 5 && !isBonus)  return "Third";
+        if(c == 5 && isBonus)   return "Second";
+        if(c == 6)              return "First";
+        return "Out of rank";
+    }
+}
---
+++ src/main/java/kr/codesquad/User.java
@@ -0,0 +1,93 @@
+package kr.codesquad;
+import java.util.ArrayList;
+import java.util.Collections;
+
+
+public class User {
+
+    private int cash; // 구매 금액
+    private int buyNum; // 구매 개수
+    private ArrayList<ArrayList<Integer>> lottoTickets = new ArrayList<ArrayList<Integer>>();
+
+    public int getCash() {
+        return cash;
+    }
+
+    public void setCash(int cash) {
+        this.cash = cash;
+    }
+
+    public int getBuyNum() {
+        return buyNum;
+    }
+
+    public void setBuyNum(int buyNum) {
+        this.buyNum = buyNum;
+    }
+
+
+    public ArrayList<ArrayList<Integer>> getLottoTickets() {
+        return lottoTickets;
+    }
+
+    public void MakeRandomLottoTickets() {
+        for(Integer i = 0; i < buyNum; i++)
+        {
+            // 임의의 6개의 번호 입력 후
+            lottoTickets.add(GetRandomSixNums());
+            // 오름차순으로 정렬
+            Collections.sort(lottoTickets.get(i));
+        }
+    }
+
+    private ArrayList<Integer> GetRandomSixNums()
+    {
+        ArrayList<Integer> ret = new ArrayList<Integer>();
+
+        // 1부터 45까지의 정수를 담고 있는 arraylist
+        ArrayList<Integer> Numbers = new ArrayList<Integer>();
+        for(Integer i = 1; i <= 45; i++) Numbers.add(i);
+        Collections.shuffle(Numbers);
+
+        // 앞 6자리 추출
+        for(int i = 0; i < 6; i++) {
+            ret.add(Numbers.get(i));
+        }
+
+        return ret;
+    }
+
+    public void CalculateBuyNum()
+    {
+        // 구매 개수 구하기
+        buyNum = cash / 1000;
+    }
+
+    public void PrintResult(ArrayList<Integer> Result)
+    {
+        System.out.println();
+        System.out.println("당첨 통계");
+        System.out.println("-------------");
+
+        for(LottoStatus stat : LottoStatus.values())
+        {
+            System.out.printf("%s, (%d)- %d개\n", stat.getStatusString(), stat.getWinningAmout(), Result.get(stat.ordinal()));
+        }
+//          System.out.printf("3개 일치 (5000원)- %d개\n", Result.get(0));
+//          System.out.printf("4개 일치 (50000원)- %d개\n", Result.get(1));
+//          System.out.printf("5개 일치 (1500000원)- %d개\n", Result.get(2));
+//          System.out.printf("5개 일치, 보너스 볼 일치(30000000원)- %d개\n", Result.get(3));
+//          System.out.printf("6개 일치 (2000000000원)- %d개\n", Result.get(4));
+
+        float rate = (float)(
+                    LottoStatus.Fifth.getWinningAmout()*Result.get(0) +
+                    LottoStatus.Fourth.getWinningAmout()*Result.get(1) +
+                    LottoStatus.Third.getWinningAmout()*Result.get(2) +
+                    LottoStatus.Second.getWinningAmout()*Result.get(3) +
+                    LottoStatus.First.getWinningAmout()*Result.get(4) - buyNum*1000) /
+                (float) (buyNum*1000)
+                * 100;
+
+        System.out.printf("총 수익률은 %.2f%% 입니다.\n", rate);
+    }
+}
