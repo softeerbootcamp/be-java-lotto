bda47aad3eff1972a9384bccdd8a4d6f78faf1fa
i5046821854 <o5046821854@gmail.com>
2023-01-03 14:27:11 +0900

    day2 - (1) 로또의 정보를 생성하고 반환하기 위한 전용 클래스 생성

---
+++ src/main/java/kr/codesquad/Lotto.java
@@ -0,0 +1,13 @@
+package kr.codesquad;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Lotto {
+
+    private ArrayList<ArrayList<Integer>> lottoList = new ArrayList<ArrayList<Integer>>();
+    private static ArrayList<Integer> allNums;
+    private static int type;
+    private static Scanner scan;
+
+}
