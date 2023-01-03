69ace28939de7f954eeba60ff5a8b83d4a14e6ff
pjs0418 <tony180322@gmail.com>
2023-01-03 14:13:08 +0900

    Rank enum class 생성자 구현

--- src/main/java/kr/codesquad/Rank.java
+++ src/main/java/kr/codesquad/Rank.java
@@ -1,4 +1,17 @@
 package kr.codesquad;
 
 public enum Rank {
+    FIRST(6, 2000000000),
+    SECOND(5, 30000000),
+    THIRD(5, 1500000),
+    FOURTH(4, 50000),
+    FIFTH(3, 5000);
+
+    private final int countOfMatch;
+    private final int winningMoney;
+
+    private Rank(int countOfMatch, int winningMoney) {
+        this.countOfMatch = countOfMatch;
+        this.winningMoney = winningMoney;
+    }
 }
