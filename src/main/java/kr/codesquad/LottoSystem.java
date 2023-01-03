cdf569db5814ee215b3c3266f8e20e7bbec8a5b6
pjs0418 <tony180322@gmail.com>
2023-01-03 11:03:18 +0900

    buyLotto 함수에서 구입 금액 입력받는 부분 구현

--- src/main/java/kr/codesquad/LottoSystem.java
+++ src/main/java/kr/codesquad/LottoSystem.java
@@ -2,6 +2,7 @@ package kr.codesquad;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class LottoSystem {
     private final List<Integer> numList;
@@ -9,5 +10,12 @@ public class LottoSystem {
     public LottoSystem() {
         this.numList = new ArrayList<>();
         for(int i = 1;i <= 45;i++) this.numList.add(i);
+    }
+
+    public Lotto buyLotto() {
+        System.out.println("구입금액을 입력해 주세요.");
+        Scanner sc = new Scanner(System.in);
+        int count = sc.nextInt() / 1000;
+        System.out.println(count + "개를 구매했습니다.");
     }
 }
