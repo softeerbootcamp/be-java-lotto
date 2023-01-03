c6102360678f35a9cf5d714df21e7a998c84beed
River-Mt <rivermt1998@gmail.com>
2023-01-03 17:58:28 +0900

    Feat: add LottoController makeWinLotto, getWinLotto - 당첨로또를 입력받아 생성하는 메서드

--- src/main/java/kr/codesquad/controller/LottoController.java
+++ src/main/java/kr/codesquad/controller/LottoController.java
@@ -1,8 +1,13 @@
 package kr.codesquad.controller;
 
+import kr.codesquad.domain.Lotto;
 import kr.codesquad.domain.Lottos;
+import kr.codesquad.domain.WinLotto;
 import kr.codesquad.view.InputView;
 import kr.codesquad.view.OutputView;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class LottoController {
 
@@ -16,6 +21,7 @@ public class LottoController {
         totalPrice = requestMoney();
         amount = totalPrice / LOTTO_PRICE;
         Lottos lottos = purchaseLotto(amount);
+        WinLotto winLotto  = makeWinLotto();
     }
 
     private int requestMoney() {
@@ -28,6 +34,20 @@ public class LottoController {
         Lottos lottos = new Lottos(amount);
         OutputView.showLottoNumbers(lottos);
         return lottos;
+    }
+
+    private WinLotto makeWinLotto() {
+        OutputView.showRequestWinNumber();
+        Lotto winLotto = getWinLotto();
+        return new WinLotto(winLotto);
+    }
+
+    private Lotto getWinLotto() {
+        List<Integer> newList = InputView.inputWinLotto()
+                .stream()
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+        return new Lotto(newList);
     }
 
 }
