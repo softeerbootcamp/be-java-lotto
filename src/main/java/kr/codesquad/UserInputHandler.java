f34f1446d769626ed0cab455846888b0d615bd7c
rohsik2 <rohsik2@gmail.com>
2023-01-03 11:44:52 +0900

    refactor : using Utility not inline if statement

--- src/main/java/kr/codesquad/UserInputHandler.java
+++ src/main/java/kr/codesquad/UserInputHandler.java
@@ -1,25 +1,23 @@
 package kr.codesquad;
 
+import kr.codesquad.Exceptions.CustomException;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 public class UserInputHandler {
 
-    // lower bound and upper_bound are included in available range
     public static int getOneNumber( int lower_bound, int upper_bound){
         Scanner s1 = new Scanner(System.in);
-        int userinput;
-        userinput = s1.nextInt();
-        if(userinput < lower_bound || userinput > upper_bound){
-            throw new IllegalArgumentException();
-        }
-        return userinput;
+        String userInputStr = s1.nextLine();
+        int num = Utility.parseIntWithRange(userInputStr, lower_bound, upper_bound);
+        return num;
     }
     public static int getMoney(){
         System.out.println("구입금액을 입력해 주세요.");
         int money = getOneNumber(0, Integer.MAX_VALUE);
         if (money % 1000 != 0)
-            throw new IllegalArgumentException();
+            throw new CustomException("구입 금액은 1000의 배수여야 합니다.");
         return money;
     }
 
@@ -29,10 +27,12 @@ public class UserInputHandler {
         List<String> nums = List.of(s1.nextLine().split(","));
         ArrayList<Integer> winningNumber = new ArrayList<>();
         for(String num : nums){
-            winningNumber.add(Integer.parseInt(num.trim()));
+            winningNumber.add(Utility.parseIntWithRange(num.trim(), 1, 45));
         }
         if(winningNumber.size() != 6)
-            throw new IllegalArgumentException();
+            throw new CustomException("당첨 번호가 6개의 숫자가 아닙니다.");
+        if(Utility.isDuplicated(winningNumber))
+            throw new CustomException("당첨 번호에 중복된 숫자가 있습니다.");
         return winningNumber;
     }
 
@@ -40,7 +40,7 @@ public class UserInputHandler {
         System.out.println("보너스 볼을 입력해 주세요.");
         int bonus = getOneNumber(1, 45);
         if(winningNumbers.contains(bonus))
-            throw new IllegalArgumentException();
+            throw new CustomException("보너스 숫자가 당첨번호에 포함되어 있습니다.");
         return bonus;
     }
 }
