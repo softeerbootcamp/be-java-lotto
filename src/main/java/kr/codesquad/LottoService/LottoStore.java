f202472627bebac42615d6ae390901e2e394997a
Toa <ld980105@snu.ac.kr>
2023-01-03 18:35:47 +0900

    feature : LottoStore 클래스 구현

---
+++ src/main/java/kr/codesquad/LottoService/LottoStore.java
@@ -0,0 +1,60 @@
+package kr.codesquad.LottoService;
+
+import kr.codesquad.LottoService.LottoFactory.LottoFactory;
+import kr.codesquad.Rank;
+
+import java.util.*;
+
+public class LottoStore {
+    private final LottoFactory lottoFactory;
+    private List<Lotto> lottos;
+    private Map<String, Integer> winningLottos;
+
+    public LottoStore(LottoFactory lottoFactory, List<Lotto> lottos, Map<String, Integer> winningLottos) {
+        this.lottoFactory = lottoFactory;
+        this.lottos = lottos;
+        this.winningLottos = winningLottos;
+        for (Rank rank : Rank.values()){
+            winningLottos.put(rank.toString(), 0);
+        }
+    }
+
+    public void setAutomaticLotto(long automaticLottoCount) {
+        for (long i = 0; i < automaticLottoCount; i++) {
+            Lotto currLotto = lottoFactory.generateLotto("automatic");
+            lottos.add(currLotto);
+        }
+    }
+
+    public void setManualLotto(long manualLottoCount) {
+
+    }
+
+    public Map<String, Integer> getWinningLottos() {
+        return winningLottos;
+    }
+
+    public List<Lotto> getLottos() {
+        return lottos;
+    }
+
+    public void setWinningNumbers(List<Integer> wonNumberList, int bonusNumber){
+        Set<Integer> wonNumberSet = new HashSet<>(wonNumberList);
+        for (Lotto lotto : lottos) {
+            String currRank = lotto.getMatchRank(wonNumberSet, bonusNumber).toString();
+            winningLottos.put(currRank, winningLottos.get(currRank) + 1);
+        }
+    }
+
+    public void calculateEarningRate(long lottoCount){
+        long earningSum = 0;
+        for (Rank rank :Rank.values()){
+            earningSum += (long) rank.getWinningMoney() / 1000 * winningLottos.get(rank.toString());
+        }
+        float earingRate = 0;
+        if (lottoCount != 0) {
+            earingRate = (earningSum - lottoCount)/(float)lottoCount * 100;
+        }
+        System.out.printf("총 수익률은 %.2f%%입니다.", Math.ceil(earingRate * 100)/100);
+    }
+}
