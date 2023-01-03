338cfa8a1fb26a12831aaee74ee8bcb9195e1e50
jun-min1623 <junmin1623@naver.com>
2023-01-03 14:35:28 +0900

    feat:main함수 최적화

--- src/main/java/kr/codesquad/Main.java
+++ src/main/java/kr/codesquad/Main.java
@@ -8,17 +8,20 @@ import static kr.codesquad.Jackpot_Handler.*;
 import static kr.codesquad.Lotto_Info.LOTTO_PRICE;
 
 public class Main {
+    public static int money,cnt;
     // todo list 1
     public static int ret_lotto_cnt(int money){
         return money/LOTTO_PRICE.getValue();
     }
-
-    public static void main(String[] args) {
+    public static void start(){
         System.out.println("구입 금액을 입력해 주세요.");
         Scanner sc = new Scanner(System.in);
         int money = sc.nextInt();
         int cnt = ret_lotto_cnt(money);
         System.out.printf("%d개를 구매했습니다.\n",cnt);
+    }
+    public static void main(String[] args) {
+        start();
         My_lotto myLotto = new My_lotto(cnt);
         setJackpotNum();
         finding_jackpot(myLotto,JACKPOT_NUM);
