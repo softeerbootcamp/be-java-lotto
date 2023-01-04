d931ae94f27d21d175ebddab94bb8094aa421092
pjs0418 <tony180322@gmail.com>
2023-01-04 11:12:58 +0900

    LottoChecker class 생성자 구현

--- src/main/java/kr/codesquad/LottoChecker.java
+++ src/main/java/kr/codesquad/LottoChecker.java
@@ -1,4 +1,12 @@
 package kr.codesquad;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class LottoChecker {
+    private final List<Integer> result;
+
+    public LottoChecker() {
+        this.result = new ArrayList<>(List.of(0, 0, 0, 0, 0));
+    }
 }
