a81a1bd6d2404140cb692098e7abbcadf0e54eda
pjs0418 <tony180322@gmail.com>
2023-01-04 11:04:14 +0900

    로또 당첨 번호를 입력받는 inputWinNum 함수 구현

--- src/main/java/kr/codesquad/UserInput.java
+++ src/main/java/kr/codesquad/UserInput.java
@@ -1,5 +1,7 @@
 package kr.codesquad;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class UserInput {
@@ -12,5 +14,15 @@ public class UserInput {
     public int inputMoney() {
         System.out.println("구입금액을 입력해 주세요.");
         return sc.nextInt();
+    }
+
+    public List<Integer> inputWinNum() {
+        System.out.println("지난 주 당첨 번호를 입력해 주세요.");
+        String[] winNum = sc.nextLine().split(", ");
+        
+        List<Integer> winNumList = new ArrayList<>();
+        for(int i = 0;i < 6;i++) winNumList.add(Integer.parseInt(winNum[i]));
+
+        return winNumList;
     }
 }
