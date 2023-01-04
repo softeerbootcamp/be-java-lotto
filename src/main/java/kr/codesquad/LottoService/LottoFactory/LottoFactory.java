9cb24f36b8f921acb99fa4f68ca6c3869b953ee6
Toa <ld980105@snu.ac.kr>
2023-01-05 02:22:45 +0900

    feat : 수동로또 생성 구현

--- src/main/java/kr/codesquad/LottoService/LottoFactory/LottoFactory.java
+++ src/main/java/kr/codesquad/LottoService/LottoFactory/LottoFactory.java
@@ -1,17 +1,22 @@
 package kr.codesquad.LottoService.LottoFactory;
 
 import kr.codesquad.LottoService.Lotto;
+import kr.codesquad.View.IOManager;
+import kr.codesquad.View.IOManagerImpl;
 
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class LottoFactory{
-    private List<Integer> numbers;
     private static final int LOTTO_START_NUMBER = 1;
     private static final int LOTTO_LAST_NUMBER = 45;
     private static final String ILLEGAL_ARGUMENT_MESSAGE = "유효하지 않은 로또 종류입니다";
-    public LottoFactory() {
+    private final List<Integer> numbers;
+    private final IOManager ioManager;
+
+    public LottoFactory(IOManager ioManager) {
+        this.ioManager = ioManager;
         numbers = new ArrayList<>();
         for (int number = LOTTO_START_NUMBER; number <= LOTTO_LAST_NUMBER; number++) {
             numbers.add(number);
@@ -21,11 +26,10 @@ public class LottoFactory{
     public Lotto generateLotto(String type) {
         if (type.equals("automatic")) {
             Collections.shuffle(numbers);
-//            return new Lotto(new ArrayList<>(numbers.subList(0, LOTTO_SIZE)));
-            return null;
+            return Lotto.of(numbers);
         }
         if (type.equals("manual")) {
-            return null;
+            return Lotto.ofComma(ioManager.inputManualNumber());
         }
         throw new IllegalArgumentException(ILLEGAL_ARGUMENT_MESSAGE);
     }
