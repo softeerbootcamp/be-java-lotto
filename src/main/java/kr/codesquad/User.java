2d3071692c3d8b0b96ea463083d58c587023aa10
macbook16 <macbook16@macbook16ui-noteubug.local>
2023-01-04 14:36:05 +0900

    부모 클래스 메서드 오버라이드 구현

--- src/main/java/kr/codesquad/User.java
+++ src/main/java/kr/codesquad/User.java
@@ -3,48 +3,44 @@ package kr.codesquad;
 import java.util.*;
 
 
-public class User{
+public class User extends BaseLotto{
 
     private ArrayList<Integer> inputs;
-
-    private ArrayList<ArrayList<Integer>> manualLottos;
-
     private int bonusBall;
 
     public User() {
-
-        inputs = new ArrayList<>();
-        manualLottos=new ArrayList<>();
+        this.inputs = new ArrayList<>();
+        this.lottoList=new ArrayList<>();
     }
 
-    public Integer getManualLottoCount(int amountOfMoney){
+    @Override
+    public ArrayList<ArrayList<Integer>> getLottoList() {
+        System.out.println("수동으로 구매할 번호를 입력해주세요.");
+        for(int i=0;i<this.lottoCnt;i++){
+            ArrayList<Integer> manualLotto = getManualLotto();
+            this.lottoList.add(manualLotto);
+        }
+        return this.lottoList;
+    }
+
+    @Override
+    public Integer getLottoCnt(int amountOfMoney, int manualLottoCount) {
         //to do : 총 가능한 로또 개수를 넘어서서 구입되지 않도록 막아야 함
         System.out.println("수동으로 구매할 로또 수를 입력해 주세요. ");
         Scanner sc = new Scanner(System.in);
-        int manualLottoCnt = sc.nextInt();
-        return manualLottoCnt;
+        this.lottoCnt = sc.nextInt();
+        return this.lottoCnt;
     }
 
     public ArrayList<Integer> getManualLotto(){
         Scanner sc = new Scanner(System.in);
-        //to-do : input 넘버가 중복이 되며 안 됨
+        //to-do : input 넘버가 중복이 되면 안 됨
         String inputNumbers = sc.nextLine();
         ArrayList<Integer> manualLotto=new ArrayList<>();
         for(String input : inputNumbers.split(", ")){
             manualLotto.add(Integer.valueOf(input));
         }
         return manualLotto;
-    }
-
-    public ArrayList<ArrayList<Integer>> getManualLottos(int manualLottoCnt){
-        System.out.println("수동으로 구매할 번호를 입력해주세요.");
-        for(int i=0;i<manualLottoCnt;i++){
-            System.out.println(i);
-            ArrayList<Integer> manualLotto = getManualLotto();
-            this.manualLottos.add(manualLotto);
-        }
-
-        return this.manualLottos;
     }
 
 
