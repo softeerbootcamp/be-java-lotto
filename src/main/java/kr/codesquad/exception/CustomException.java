04e4167478f163876b05442beab29dfe59e8aa01
macbook16 <macbook16@macbook16ui-noteubug.local>
2023-01-04 12:57:58 +0900

    customException 필요 없는 주석 제거

--- src/main/java/kr/codesquad/exception/CustomException.java
+++ src/main/java/kr/codesquad/exception/CustomException.java
@@ -1,12 +1,7 @@
 package kr.codesquad.exception;
 
 public class CustomException extends RuntimeException {
-
-    //private final String message;
-
     public CustomException(ErrorCode errorCode){
-        //this.errorCode = errorCode;
-        //this.message = errorCode.getMessage();
         super(errorCode.getMessage());
     }
 }
