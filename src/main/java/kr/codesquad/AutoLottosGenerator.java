7257d3fc86f9a623e11a4a1305e5f14ac3a83476
jieonkim23 <kwldjs1023@gmail.com>
2023-01-05 01:59:20 +0900

    AutoLottosGenerator 클래스 생성: LottosGenerator를 implements한 클래스를 생성함.

---
+++ src/main/java/kr/codesquad/AutoLottosGenerator.java
@@ -0,0 +1,14 @@
+package kr.codesquad;
+
+import java.util.List;
+
+public class AutoLottosGenerator implements LottosGenerator{
+    private static final int LOTTO_NUM_MAX = 45;
+    private static final int LOTTO_NUM_CNT = 6;
+
+    @Override
+    public List<Lotto> generate(Money money) {
+
+        return null;
+    }
+}
