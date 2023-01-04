8ea9769d161969210c0e08db871ac5fbc3870ef8
Toa <ld980105@snu.ac.kr>
2023-01-05 02:21:15 +0900

    feat : 입출력 관련 인터페이스, 클래스 구현

---
+++ src/main/java/kr/codesquad/View/IOManagerImpl.java
@@ -0,0 +1,73 @@
+package kr.codesquad.View;
+
+import kr.codesquad.LottoService.Lotto;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.security.spec.ECField;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class IOManagerImpl implements IOManager {
+    private final Scanner sc;
+
+    public IOManagerImpl() {
+        this.sc = new Scanner(System.in);
+    }
+
+    @Override
+    public int inputMoney() {
+        System.out.println("구입금액을 입력해 주세요.");
+        try{
+            return Integer.parseInt(sc.nextLine());
+        }
+        catch (NumberFormatException e){
+            throw new IllegalArgumentException("숫자로 입력해주세요.");
+        }
+    }
+
+    @Override
+    public int inputManualLottoCount() {
+        System.out.println("\n수동으로 구매할 로또 수를 입력해 주세요.");
+        try{
+            return Integer.parseInt(sc.nextLine());
+        }
+        catch (NumberFormatException e){
+            throw new IllegalArgumentException("숫자로 입력해주세요.");
+        }
+    }
+
+    @Override
+    public String inputWinningNumber() {
+        System.out.println("지난 주 당첨 번호를 입력해 주세요.");
+        return sc.nextLine();
+    }
+
+    @Override
+    public String inputManualNumber() {
+        return sc.nextLine();
+    }
+
+    @Override
+    public int inputBonusNumber() {
+        System.out.println("보너스 볼을 입력해 주세요.");
+        try{
+            return Integer.parseInt(sc.nextLine());
+        }
+        catch (NumberFormatException e){
+            throw new IllegalArgumentException("숫자로 입력해주세요.");
+        }
+    }
+
+    @Override
+    public void printLottoCount(int lottoAutoCount, int lottoManualCount) {
+        System.out.println("수동으로 "+lottoAutoCount + "장, 자동으로 " + lottoManualCount + "개를 구매했습니다.");
+    }
+
+    @Override
+    public void printLottoList(List<Lotto> lottoList) {
+    }
+}
