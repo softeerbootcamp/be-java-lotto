85de6d7a0ef38fc3493d861ca9b6f043ca56026d
Toa <ld980105@snu.ac.kr>
2023-01-05 02:25:03 +0900

    feat : Main 클래스에 IOManger 추가

--- src/main/java/kr/codesquad/Main.java
+++ src/main/java/kr/codesquad/Main.java
@@ -3,6 +3,8 @@ package kr.codesquad;
 import kr.codesquad.LottoService.LottoFactory.LottoFactory;
 import kr.codesquad.LottoService.LottoStore;
 import kr.codesquad.View.Console;
+import kr.codesquad.View.IOManager;
+import kr.codesquad.View.IOManagerImpl;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -10,7 +12,8 @@ import java.util.HashMap;
 public class Main {
     public static void main(String[] args) throws Exception {
         Console console = new Console();
-        LottoStore lottoStore = new LottoStore(new LottoFactory(), new ArrayList<>(), new HashMap<>());
+        IOManager ioManager = new IOManagerImpl();
+        LottoStore lottoStore = new LottoStore(new LottoFactory(ioManager), new ArrayList<>(), new HashMap<>());
         App app = new App(console, lottoStore);
         app.run();
     }
