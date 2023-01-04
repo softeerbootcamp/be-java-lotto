1b2ab444dc44466d8e1d9cd7db81e5786fb8cb05
roopre <benny1020@naver.com>
2023-01-04 18:24:25 +0900

    Refactor: 로또 숫자 클래스 LottoNumber Class 구현

---
+++ src/main/java/kr/codesquad/LottoNumber.java
@@ -0,0 +1,30 @@
+package kr.codesquad;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class LottoNumber {
+	private final int number;
+	private static Map<Integer, LottoNumber> numberMap = new HashMap<>();
+
+	static {
+		for (int i = 0; i < 46; i++) {
+			numberMap.put(i, new LottoNumber(i));
+		}
+	}
+
+	private LottoNumber(int number) {
+		if (number < 0 || number > 45) {
+			throw new IllegalArgumentException("잘못된 로또 번호 입력입니다.");
+		}
+		this.number = number;
+	}
+
+	public static LottoNumber of(String number) {
+		return numberMap.get(Integer.parseInt(number));
+	}
+
+	public static LottoNumber of(int number) {
+		return numberMap.get(number);
+	}
+}
