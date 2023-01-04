158697dddede4e494fc1baa19ff5ea3475d5e40a
pjs0418 <tony180322@gmail.com>
2023-01-04 10:46:53 +0900

    LottoMachine class 생성자 구현

--- src/main/java/kr/codesquad/LottoMachine.java
+++ src/main/java/kr/codesquad/LottoMachine.java
@@ -1,4 +1,15 @@
 package kr.codesquad;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class LottoMachine {
+    private final int price;
+    private final List<Integer> numList;
+
+    public LottoMachine(int price) {
+        this.price = price;
+        this.numList = new ArrayList<>();
+        for(int i = 1;i <= 45;i++) this.numList.add(i);
+    }
 }
