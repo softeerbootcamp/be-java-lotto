9e580f9b8e9a77b8321336360d34772dc948f983
i5046821854 <o5046821854@gmail.com>
2023-01-04 11:12:50 +0900

    day3 - (10) ResultLotto에서 유저에게 인풋을 받고, 이를 addLotto에 넘겨주는 메소드 구현

--- src/main/java/kr/codesquad/impl/ResultLotto.java
+++ src/main/java/kr/codesquad/impl/ResultLotto.java
@@ -3,11 +3,27 @@ package kr.codesquad.impl;
 import kr.codesquad.templates.LottoTmpl;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
 
 public class ResultLotto implements LottoTmpl {
 
     private ArrayList<ArrayList<Integer>> lottoList = new ArrayList<ArrayList<Integer>>();
     private int bonumsNum;
+
+    //유저에게 인풋을 받아 이를 로또 리스트에 저장
+    public void enterResultNumbers(){
+        System.out.println("\n당첨 번호를 입력해 주세요.");
+        Scanner scan = new Scanner(System.in);
+        String numStr = scan.nextLine();
+        List<String> split = Arrays.asList(numStr.split(","));
+        ArrayList<Integer> tempList = new ArrayList<Integer>();
+        if(split.size() != 6)
+            throw new RuntimeException("올바르지 않은 입력입니다");
+        split.forEach(item->tempList.add(Integer.parseInt(item)));
+        addLotto(tempList);
+    }
 
     //주어진 6개의 숫자 리스트를 로또 리스트 ArrayList에 add
     @Override
