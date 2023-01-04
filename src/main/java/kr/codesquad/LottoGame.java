de3289662fc50e5e8cb10b60121eda80a608730b
macbook16 <macbook16@macbook16ui-noteubug.local>
2023-01-04 18:05:39 +0900

    로또 게임 인터페이스 메서드 추가

--- src/main/java/kr/codesquad/LottoGame.java
+++ src/main/java/kr/codesquad/LottoGame.java
@@ -5,6 +5,8 @@ import java.util.List;
 public interface LottoGame {
     boolean isBonusInLotto(List<Integer> lotto, int bonusBall);
     void updateRankMap(Rank rank);
+    void checkRankForUpdate(Rank rank,int matchCnt);
+    void updateRankByMatchCnt(int matchCnt);
     void getMatchCount(List<Integer> lotto, List<Integer> input,Integer bonusBall);
     int calcMatchCount(List<Integer> list, Integer num, int resMatchCnt);
     void calcRate(int lottoCnt);
