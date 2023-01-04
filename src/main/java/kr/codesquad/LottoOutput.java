d361d9a509d45532521699118373f7717a80b8f3
jieonkim23 <kwldjs1023@gmail.com>
2023-01-05 03:47:47 +0900

    LottoOutput을 새로 추가한 클래스와 수정한 클래스를 활용하는 것으로 변경

--- src/main/java/kr/codesquad/LottoOutput.java
+++ src/main/java/kr/codesquad/LottoOutput.java
@@ -1,17 +1,13 @@
 package kr.codesquad;
-import java.util.List;
-import java.util.Map;
 
 public class LottoOutput {
-    public static void printLottos(List<Lotto> lottos){
-        for(Lotto lotto : lottos){
-            lotto.printLotto();
-        }
+    public static void printLottos(LottoGame game){
+        System.out.println(game.toString());
     }
-    public static void printResult(Map<Rank, Integer> result, double rate){
+    public static void printResult(LottoResult result, double rate){
         System.out.println("\n당첨 통계");
         System.out.println("---------");
-        result.forEach((r,c)-> System.out.printf("%s (%d원)- %d개\n", r.getWords(), r.getPrize(), c));
+        result.result.forEach((r,c)->System.out.println(r.toString()+"- "+c+"개"));
         System.out.printf("총 수익률은 %.2f%%입니다.\n", rate);
     }
 }
