d251f59044da1e14fd3bf746834379a6808b5719
jieonkim23 <kwldjs1023@gmail.com>
2023-01-05 03:54:33 +0900

    Main을 새로 추가한 클래스와 수정한 클래스를 활용하는 것으로 변경

--- src/main/java/kr/codesquad/Main.java
+++ src/main/java/kr/codesquad/Main.java
@@ -2,8 +2,11 @@ package kr.codesquad;
 
 public class Main {
     public static void main(String[] args) {
-        LottoSystem ls = new LottoSystem();
-        ls.makeLottos();
-        ls.getResult();
+        Money money = LottoInput.enterMoney();
+        LottoGame game = new LottoGame(money);
+        LottoOutput.printLottos(game);
+        WinningLotto winningLotto = new WinningLotto(LottoInput.enterWinLottoNumber(), LottoInput.enterBonus());
+        LottoResult lottoResult = game.match(winningLotto);
+        LottoOutput.printResult(lottoResult, money.calProfitRate(lottoResult.calPrize()));
     }
 }
