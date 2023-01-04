c9ebeae50a30277ea1c5f99b03805777f74b8085
macbook16 <macbook16@macbook16ui-noteubug.local>
2023-01-04 16:50:38 +0900

    접근 제어자 제거

--- src/main/java/kr/codesquad/LottoGame.java
+++ src/main/java/kr/codesquad/LottoGame.java
@@ -3,10 +3,10 @@ package kr.codesquad;
 import java.util.List;
 
 public interface LottoGame {
-    public boolean isBonusInLotto(List<Integer> lotto, int bonusBall);
-    public void updateRankMap(Rank rank);
-    public void getMatchCount(List<Integer> lotto, List<Integer> input,Integer bonusBall);
-    public int calcMatchCount(List<Integer> list, Integer num, int resMatchCnt);
-    public void calcRate(int lottoCnt);
-    public void printResult(int lottoCnt);
+    boolean isBonusInLotto(List<Integer> lotto, int bonusBall);
+    void updateRankMap(Rank rank);
+    void getMatchCount(List<Integer> lotto, List<Integer> input,Integer bonusBall);
+    int calcMatchCount(List<Integer> list, Integer num, int resMatchCnt);
+    void calcRate(int lottoCnt);
+    void printResult(int lottoCnt);
 }
