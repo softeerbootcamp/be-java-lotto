43dc7fdc1465b9c4c358d63dc5733ef25f88da51
macbook16 <macbook16@macbook16ui-noteubug.local>
2023-01-04 18:02:59 +0900

    유저 입력시 예외 처리 구현

--- src/main/java/kr/codesquad/User.java
+++ src/main/java/kr/codesquad/User.java
@@ -1,5 +1,8 @@
 package kr.codesquad;
 
+import kr.codesquad.exception.CustomException;
+import kr.codesquad.exception.ErrorCode;
+
 import java.util.*;
 
 
@@ -25,10 +28,22 @@ public class User extends BaseLotto{
 
     @Override
     public Integer getLottoCnt(int amountOfMoney, int manualLottoCount) {
-        //to do : 총 가능한 로또 개수를 넘어서서 구입되지 않도록 막아야 함
-        System.out.println("수동으로 구매할 로또 수를 입력해 주세요. ");
-        Scanner sc = new Scanner(System.in);
-        this.lottoCnt = sc.nextInt();
+        do {
+            System.out.print("수동으로 구매할 로또 수를 입력해 주세요. ");
+            try {
+                Scanner sc = new Scanner(System.in);
+                this.lottoCnt = sc.nextInt();
+                if(this.lottoCnt*1000>amountOfMoney) throw new CustomException(ErrorCode.MONEY_NOT_ENOUGH);
+            }catch(InputMismatchException e){
+                System.out.println("유효하지 않은 값입니다. 다시 값을 입력해주세요.");
+                continue; //다시 반복문의 처음으로 돌아가 입력값을 다시 입력받음
+            }catch(Exception e){
+                System.out.println("입력에 에러가 발생했습니다. 다시 값을 입력해주세요");
+                continue;
+            }
+            break; // do-while을 통한 입력 벗어남
+        } while (true);
+
         return this.lottoCnt;
     }
 
@@ -37,17 +52,23 @@ public class User extends BaseLotto{
         //to-do : input 넘버가 중복이 되면 안 됨
         String inputNumbers = sc.nextLine();
         ArrayList<Integer> manualLotto=new ArrayList<>();
-        for(String input : inputNumbers.split(", ")){
-            manualLotto.add(Integer.valueOf(input));
+        if(inputNumbers.split(", ").length != 6) throw new CustomException(ErrorCode.ILLEGAL_LOTTO_NUM_COUNT);
+        try{
+            for(String input : inputNumbers.split(", ")){
+                manualLotto.add(Integer.valueOf(input));
+            }
+        }catch(Exception e){
+            System.out.println("입력 양식을 다시 확인해주세요");
         }
+
         return manualLotto;
     }
-
 
     public ArrayList<Integer> getLottoInput(){
         System.out.println("당첨 번호를 입력해주세요.");
         Scanner sc = new Scanner(System.in);
         String inputNumbers = sc.nextLine();
+        if(inputNumbers.split(", ").length != 6) throw new CustomException(ErrorCode.ILLEGAL_LOTTO_NUM_COUNT);
         for(String input : inputNumbers.split(", ")){
             inputs.add(Integer.valueOf(input));
         }
@@ -56,8 +77,12 @@ public class User extends BaseLotto{
 
     public Integer getBonusLottoInput() {
         System.out.println("보너스 볼을 입력해주세요. ");
-        Scanner sc = new Scanner(System.in);
-        this.bonusBall = sc.nextInt();
+        try{
+            Scanner sc = new Scanner(System.in);
+            this.bonusBall = sc.nextInt();
+        }catch(InputMismatchException e){
+            System.out.println("유효하지 않은 값입니다. 다시 값을 입력해주세요.");
+        }
         return bonusBall;
     }
 
