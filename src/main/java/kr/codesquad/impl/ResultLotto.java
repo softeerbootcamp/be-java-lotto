0373dbd63b0e0c07120d52988391776c52fd7205
i5046821854 <o5046821854@gmail.com>
2023-01-04 11:06:47 +0900

    day3 - (8) ResultLotto에서 로또 리스트 ArrayList에 값을 저장하는 메소드 구현

--- src/main/java/kr/codesquad/impl/ResultLotto.java
+++ src/main/java/kr/codesquad/impl/ResultLotto.java
@@ -9,11 +9,13 @@ public class ResultLotto implements LottoTmpl {
     private ArrayList<ArrayList<Integer>> lottoList = new ArrayList<ArrayList<Integer>>();
     private int bonumsNum;
 
+    //주어진 6개의 숫자 리스트를 로또 리스트 ArrayList에 add
     @Override
-    public void addLotto(ArrayList<Integer> lottoList) {
-
+    public void addLotto(ArrayList<Integer> givenList) {
+        lottoList.add(givenList);
     }
 
+    //로또 리스트를 리턴
     @Override
     public ArrayList<ArrayList<Integer>> getLottoList() {
         return lottoList;
