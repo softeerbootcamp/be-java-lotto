84cf658b690b220bf75ab0e07074cf08bb75d240
macbook16 <macbook16@macbook16ui-noteubug.local>
2023-01-04 12:55:41 +0900

    CustomException 클래스 구현

--- src/main/java/kr/codesquad/exception/CustomException.java
+++ src/main/java/kr/codesquad/exception/CustomException.java
@@ -1,2 +1,12 @@
-package kr.codesquad.exception;public class CustomException {
+package kr.codesquad.exception;
+
+public class CustomException extends RuntimeException {
+
+    //private final String message;
+
+    public CustomException(ErrorCode errorCode){
+        //this.errorCode = errorCode;
+        //this.message = errorCode.getMessage();
+        super(errorCode.getMessage());
+    }
 }
