6c85997637ed2576114d9939cf2fcc621448f646
rentalhub <rentalhub@rentalhubui-MacBookPro.local>
2023-01-04 11:01:13 +0900

    conflict로 인한 복구

--- src/main/java/kr/codesquad/Main.java
+++ src/main/java/kr/codesquad/Main.java
@@ -6,6 +6,10 @@
 package kr.codesquad;
 
 import kr.codesquad.Controller.LottoController;
+<<<<<<< HEAD
+=======
+import kr.codesquad.Model.LottoService;
+>>>>>>> 1a9bb7d (conflict로 인한 복구)
 
 public class Main {
     public Main() {
