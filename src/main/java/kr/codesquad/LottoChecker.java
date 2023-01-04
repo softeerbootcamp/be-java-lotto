3a5fa28a6dcb398b5f8e8c29b94cd8b1ea362649
pjs0418 <tony180322@gmail.com>
2023-01-04 12:03:35 +0900

    당첨 결과를 출력하는 기능의 printResult 함수 구현

--- src/main/java/kr/codesquad/LottoChecker.java
+++ src/main/java/kr/codesquad/LottoChecker.java
@@ -16,7 +16,7 @@ public class LottoChecker {
         for(List<Integer> eachLottoList : lottoList)
             setResult(result, countWinNum(eachLottoList, winNumList), checkBonus(eachLottoList, winNumList.get(6)));
 
-        printResult(result);
+        printResult(result, lottoList.size());
     }
 
     public int countWinNum(List<Integer> eachLottoList, List<Integer> winNumList) {
@@ -42,5 +42,23 @@ public class LottoChecker {
         else if(winNumCount == Rank.SECOND.getCountOfMatch() && isBonus) result.set(3, result.get(3) + 1);
         else if(winNumCount == Rank.THIRD.getCountOfMatch()) result.set(2, result.get(2) + 1);
         else if(winNumCount == Rank.FIRST.getCountOfMatch()) result.set(4, result.get(4) + 1);
+    }
+
+    public void printResult(List<Integer> result, int lottoCount) {
+        System.out.println("당첨 통계");
+        System.out.println("---------");
+        System.out.println("3개 일치 (5000원)- " + result.get(0) + "개");
+        System.out.println("4개 일치 (50000원)- " + result.get(1) + "개");
+        System.out.println("5개 일치 (1500000원)- " + result.get(2) + "개");
+        System.out.println("5개 일치, 보너스 볼 일치 (30000000원)- " + result.get(3) + "개");
+        System.out.println("6개 일치 (2000000000원)- " + result.get(4) + "개");
+
+        long resultPrice = (long) result.get(0) * Rank.FIFTH.getWinningMoney() +
+                (long) result.get(1) * Rank.FOURTH.getWinningMoney() +
+                (long) result.get(2) * Rank.THIRD.getWinningMoney() +
+                (long) result.get(3) * Rank.SECOND.getWinningMoney() +
+                (long) result.get(4) * Rank.FIRST.getWinningMoney();
+        long expense = lottoCount * 1000L;
+        System.out.printf("총 수익률은 %.2f%%입니다.\n", (float) (resultPrice - expense) / expense * 100);
     }
 }
