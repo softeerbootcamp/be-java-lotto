d3aac57168ec7dc4b9df4080137f74fba7ab76ca
Toa <ld980105@snu.ac.kr>
2023-01-04 12:19:51 +0900

    refactor : 클래스 내에서 사용한 값들 상수화

--- src/main/java/kr/codesquad/LottoService/LottoFactory/LottoFactory.java
+++ src/main/java/kr/codesquad/LottoService/LottoFactory/LottoFactory.java
@@ -9,23 +9,26 @@ import java.util.List;
 public class LottoFactory{
     private List<Integer> numbers;
     private static final int LOTTO_SIZE = 6;
+    private static final int LOTTO_START_NUMBER = 1;
     private static final int LOTTO_LAST_NUMBER = 45;
-
+    private static final String ILLEGALARGUMENT_MESSAGE = "유효하지 않은 로또 종류입니다";
+    private static final String AUTOMATIC = "automatic";
+    private static final String MANUAL = "manual";
     public LottoFactory() {
         numbers = new ArrayList<>();
-        for (int number = 1; number <= LOTTO_LAST_NUMBER; number++) {
+        for (int number = LOTTO_START_NUMBER; number <= LOTTO_LAST_NUMBER; number++) {
             numbers.add(number);
         }
     }
 
     public Lotto generateLotto(String type) {
-        if (type.equals("automatic")) {
+        if (type.equals(AUTOMATIC)) {
             Collections.shuffle(numbers);
             return new Lotto(new ArrayList<>(numbers.subList(0, LOTTO_SIZE)));
         }
-        if (type.equals("manual")) {
+        if (type.equals(MANUAL)) {
             return null;
         }
-        throw new IllegalArgumentException("유효하지 않은 로또 종류입니다");
+        throw new IllegalArgumentException(ILLEGALARGUMENT_MESSAGE);
     }
 }
