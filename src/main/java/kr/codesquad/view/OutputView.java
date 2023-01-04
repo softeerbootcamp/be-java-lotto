0e93744c85c24a66a6f469ac9458aae58ba3c70b
Soomin-Lim <augustth511@gmail.com>
2023-01-04 18:03:49 +0900

    feat: 수동으로 구매할 로또 번호를 입력하라는 메시지를 출력하는 기능 구현

--- src/main/java/kr/codesquad/view/OutputView.java
+++ src/main/java/kr/codesquad/view/OutputView.java
@@ -19,6 +19,9 @@ public class OutputView {
         System.out.println("수동으로 구매할 로또 수를 입력해 주세요.");
     }
 
+    public void printUserManualLottoReadMessage() {
+        System.out.println("수동으로 구매할 번호를 입력해 주세요.");
+    }
     public void printLottoCount(int manualLottoCount, int autoLottoCount) {
         System.out.println("수동으로 " + manualLottoCount + "장, 자동으로 "+ autoLottoCount + "개를 구매했습니다.");
     }
