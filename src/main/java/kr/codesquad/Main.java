5fc20bd95c8dd6d0235013c858b41b3d3af26b13
i5046821854 <o5046821854@gmail.com>
2023-01-04 11:57:35 +0900

    day3 - (16) Main함수에서 인풋을 받는 로직을 모두 삭제 (각 로직을 담당하는 클래스를 따로 생성하였으므로)

--- src/main/java/kr/codesquad/Main.java
+++ src/main/java/kr/codesquad/Main.java
@@ -1,50 +1,8 @@
 package kr.codesquad;
 
-import java.util.*;
-
-
-class UserInput {
-
-    private Scanner scan;
-    private UserInfo user;
-
-    public UserInput(UserInfo user){
-        this.user = user;
-        scan = new Scanner(System.in);
-    }
-
-    //구매 금액 입력
-    public void enterPurchasePrice(){
-        System.out.println("구입 금액을 입력해 주세요.");
-        int purchasedPrice = scan.nextInt();
-        int numOfLotto = purchasedPrice / 1000;
-        scan.nextLine();
-        user.setPurchasedPrice(purchasedPrice);
-        user.setNumOfLotto(numOfLotto);
-        System.out.println(numOfLotto + "개를 구매했습니다.");
-    }
-
-
-    //당첨 번호 입력현
-    public ArrayList<Integer> enterResultNumbers(){
-        System.out.println("\n당첨 번호를 입력해 주세요.");
-        String numStr = scan.nextLine();
-        List<String> split = Arrays.asList(numStr.split(","));
-        ArrayList<Integer> tempList = new ArrayList<Integer>();
-        if(split.size() != 6)
-            throw new RuntimeException("올바르지 않은 입력입니다");
-        split.forEach(item->tempList.add(Integer.parseInt(item)));
-        return tempList;
-    }
-
-    //보너스 번호 입력
-    public void enterBonusNum(){
-        System.out.println("보너스볼을 입력해 주세요.");
-        int bonusNum = scan.nextInt();
-        user.setBonus(bonusNum);
-    }
-
-}
+import kr.codesquad.impl.MyLotto;
+import kr.codesquad.impl.RandomLotto;
+import kr.codesquad.impl.ResultLotto;
 
 public class Main {
 
@@ -52,23 +10,25 @@ public class Main {
     private static void initMethod(){
 
         UserInfo user = new UserInfo();
-        UserInput userInput = new UserInput(user);
-        Lotto randomLotto = new Lotto();
-        Lotto myLotto = new Lotto();
+        UserConsole userConsole = new UserConsole(user);
+        RandomLotto randomLotto = new RandomLotto();
+        MyLotto myLotto = new MyLotto();
+        ResultLotto resultLotto = new ResultLotto();
         CalculateMatch calculateMatch = new CalculateMatch();
 
         //구매 금액 입력
-        userInput.enterPurchasePrice();
+        userConsole.enterPurchasePrice();
         //로또 번호 생성 (랜덤)
-        for(int i = 0 ; i < user.getNumOfLotto(); i++)
-            randomLotto.addRandomLotto();
+        randomLotto.startGeneration(user.getNumOfLottoAuto(), user.getNumOfLottoSudong());
+        //로또 번호 생성 (유저가 입력)
+        myLotto.generateLotto(user.getNumOfLottoSudong());
         //당첨 번호 입력
-        myLotto.addLotto(userInput.enterResultNumbers());
+        resultLotto.enterResultNumbers();
         //보너스 번호 입력
-        userInput.enterBonusNum();
+        resultLotto.enterBonusNum();
 
         //당첨 여부 확인
-        calculateMatch.startCalculate(randomLotto, myLotto, user.getNumOfLotto(), user.getBonus());
+        calculateMatch.startCalculate(randomLotto, myLotto, resultLotto);
         //결과 출력
         calculateMatch.printResult(user.getPurchasedPrice());
 
@@ -77,7 +37,6 @@ public class Main {
     public static void main(String[] args) {
         initMethod();
     }
-
 
 
 }
