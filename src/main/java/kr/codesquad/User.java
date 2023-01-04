6f1ca1590f18484a3945e3bf5cca4aa44ef6aff7
macbook16 <macbook16@macbook16ui-noteubug.local>
2023-01-04 11:50:47 +0900

    수동으로 로또 번호 입력할 수 있도록 구현

--- src/main/java/kr/codesquad/User.java
+++ src/main/java/kr/codesquad/User.java
@@ -6,11 +6,47 @@ import java.util.*;
 public class User{
 
     private ArrayList<Integer> inputs;
+
+    private ArrayList<ArrayList<Integer>> manualLottos;
+
     private int bonusBall;
 
     public User() {
+
         inputs = new ArrayList<>();
+        manualLottos=new ArrayList<>();
     }
+
+    public Integer getManualLottoCount(int amountOfMoney){
+        //to do : 총 가능한 로또 개수를 넘어서서 구입되지 않도록 막아야 함
+        System.out.println("수동으로 구매할 로또 수를 입력해 주세요. ");
+        Scanner sc = new Scanner(System.in);
+        int manualLottoCnt = sc.nextInt();
+        return manualLottoCnt;
+    }
+
+    public ArrayList<Integer> getManualLotto(){
+        Scanner sc = new Scanner(System.in);
+        //to-do : input 넘버가 중복이 되며 안 됨
+        String inputNumbers = sc.nextLine();
+        ArrayList<Integer> manualLotto=new ArrayList<>();
+        for(String input : inputNumbers.split(", ")){
+            manualLotto.add(Integer.valueOf(input));
+        }
+        return manualLotto;
+    }
+
+    public ArrayList<ArrayList<Integer>> getManualLottos(int manualLottoCnt){
+        System.out.println("수동으로 구매할 번호를 입력해주세요.");
+        for(int i=0;i<manualLottoCnt;i++){
+            System.out.println(i);
+            ArrayList<Integer> manualLotto = getManualLotto();
+            this.manualLottos.add(manualLotto);
+        }
+
+        return this.manualLottos;
+    }
+
 
     public ArrayList<Integer> getLottoInput(){
         System.out.println("당첨 번호를 입력해주세요.");
