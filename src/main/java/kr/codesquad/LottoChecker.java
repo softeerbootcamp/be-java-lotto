814e2e31c01db91fb810ee7eab1c75f500260a10
pjs0418 <tony180322@gmail.com>
2023-01-04 11:40:24 +0900

    로또 당첨 개수 확인하는 기능의 countWinNum 함수 구현

--- src/main/java/kr/codesquad/LottoChecker.java
+++ src/main/java/kr/codesquad/LottoChecker.java
@@ -13,9 +13,22 @@ public class LottoChecker {
     public void checkLotto(Lotto lotto, List<Integer> winNumList) {
         List<List<Integer>> lottoList = lotto.getLottoList();
 
-        for(List<Integer> eachLottoNum : lottoList)
-            setResult(result, countWinNum(eachLottoNum, winNumList), checkBonus(eachLottoNum, winNumList.get(6)));
+        for(List<Integer> eachLottoList : lottoList)
+            setResult(result, countWinNum(eachLottoList, winNumList), checkBonus(eachLottoList, winNumList.get(6)));
 
         printResult(result);
+    }
+
+    public int countWinNum(List<Integer> eachLottoList, List<Integer> winNumList) {
+        int winNumCount = 0;
+
+        if(eachLottoList.contains(winNumList.get(0))) winNumCount++;
+        if(eachLottoList.contains(winNumList.get(1))) winNumCount++;
+        if(eachLottoList.contains(winNumList.get(2))) winNumCount++;
+        if(eachLottoList.contains(winNumList.get(3))) winNumCount++;
+        if(eachLottoList.contains(winNumList.get(4))) winNumCount++;
+        if(eachLottoList.contains(winNumList.get(5))) winNumCount++;
+
+        return winNumCount;
     }
 }
