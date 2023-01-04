5c11b6854b7713a4efd0550e7eb5c170f98564b7
jun-min1623 <junmin1623@naver.com>
2023-01-04 17:44:09 +0900

    feat:userlotto생성, 인풋에 반영

--- src/main/java/kr/codesquad/UserMenu/UserInput.java
+++ src/main/java/kr/codesquad/UserMenu/UserInput.java
@@ -3,9 +3,13 @@ package kr.codesquad.UserMenu;
 import kr.codesquad.CustomException;
 import kr.codesquad.InputHandler;
 
-public class UserInput extends UserLotto {
+public class UserInput {
     private InputHandler inputHandler;
-
+    private UserLotto userLotto;
+    public UserInput(){
+        inputHandler = new InputHandler();
+        userLotto = new UserLotto();
+    }
     public int userHowManyLotto() throws CustomException {
         System.out.println("구입 금액을 입력해 주세요.");
         return inputHandler.getIntegerInput()/1000;
@@ -13,9 +17,10 @@ public class UserInput extends UserLotto {
     public int userHowManyManual() throws CustomException {
         System.out.printf("수동으로 구매할 로또 수를 입력해 주세요\n");
         int manualCount = inputHandler.getIntegerInput();
-        if(manualCount>getUserAllLottoCount()){
+        if(manualCount>userLotto.getUserAllLottoCount()){
             throw new CustomException("수동으로 구매하려는 갯수가 총 갯수보다 많습니다!!");
         }
-        return inputHandler.getIntegerInput();
+        return manualCount;
     }
+
 }
--- src/main/java/kr/codesquad/UserMenu/UserLotto.java
+++ src/main/java/kr/codesquad/UserMenu/UserLotto.java
@@ -3,7 +3,9 @@ package kr.codesquad.UserMenu;
 public class UserLotto {
 
     private int userAllLottoCount;
+    public UserLotto(){
 
+    }
     public int getUserAllLottoCount() {
         return this.userAllLottoCount;
     }
