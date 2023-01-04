e3c16d53d49f4ee3b974d9bcef29978537a37616
ajongs <wonjong1030@naver.com>
2023-01-05 02:42:49 +0900

    refactor(OutputView): printBoughtLottoCount 기능을 printIssuedLottoCount로 위임

--- src/main/java/kr/codesquad/view/OutputView.java
+++ src/main/java/kr/codesquad/view/OutputView.java
@@ -9,11 +9,10 @@ public class OutputView {
     public void printInputMoneyText() {
         System.out.println("구입금액을 입력해 주세요.");
     }
-
-    public void printBoughtLottoCount(long count) {
-        System.out.println(count + "개를 구매했습니다.");
+    
+    public void printIssuedLottoCount(Money money) {
+        System.out.println(money.calculateLottoCount() + "개를 구매했습니다.");
     }
-
     private void printNumbers(List<Integer> list) {
         for (int i = 0; i < list.size() - 1; i++) {
             System.out.print(list.get(i) + ", ");
