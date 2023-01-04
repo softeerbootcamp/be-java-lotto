a19d4780a0e4b2c857c3690b226058b3fb39f92b
macbook16 <macbook16@macbook16ui-noteubug.local>
2023-01-04 12:56:09 +0900

    에러 종류 enum으로 구현

--- src/main/java/kr/codesquad/exception/ErrorCode.java
+++ src/main/java/kr/codesquad/exception/ErrorCode.java
@@ -1,2 +1,14 @@
-package kr.codesquad.exception;public enum ErrorCode {
+package kr.codesquad.exception;
+
+public enum ErrorCode {
+
+    MONEY_NOT_ENOUGH("돈이 충분하지 않습니다.");
+    private final String message;
+    ErrorCode(String message) {
+        this.message = message;
+    }
+
+    public String getMessage(){
+        return this.message;
+    }
 }
