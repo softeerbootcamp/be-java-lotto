5e1207c66d4b04661ed5a00854db435ea156c745
jieonkim23 <kwldjs1023@gmail.com>
2023-01-05 02:08:16 +0900

    Lotto클래스의 생성자 변경 및 오타 수정, 불필요한 상수 제거: LottoGenerator를 따로 분리했기 때문에 생성자를 로또 숫자를 저장하는 것으로 수정함.

--- src/main/java/kr/codesquad/Lotto.java
+++ src/main/java/kr/codesquad/Lotto.java
@@ -5,27 +5,23 @@ import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
+import static kr.codesquad.AutoLottosGenerator.LOTTO_NUM_CNT;
 
 public class Lotto {
     private final List<Integer> nums;
-    private static final int LOTTO_NUM_MAX = 45;
-    private static final int LOTTO_NUM_CNT = 6;
 
-    Lotto(){
-        List<Integer> temp = IntStream.range(1, LOTTO_NUM_MAX+1).boxed().collect(Collectors.toList());
-        Collections.shuffle(temp);
-        nums = new ArrayList<>(temp.subList(0,LOTTO_NUM_CNT+1));
-        Collections.sort(nums);
+    Lotto(List<Integer> lottoNumber){
+        if (lottoNumber.size() != LOTTO_NUM_CNT) {
+            throw new IllegalArgumentException();
+        }
+
+        this.nums = lottoNumber;
     }
-
-    public int Match(Lotto lotto) {
+    public int match(Lotto lotto) {
         long cnt = lotto.nums.stream().filter(w -> nums.contains(w)).count();
         return (int)cnt;
     }
     public boolean checkBonus(int bonus){
         return nums.contains(bonus);
-    }
-    public void printLotto(){
-        System.out.println(nums);
     }
 }
