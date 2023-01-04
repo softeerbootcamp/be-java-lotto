55769de11bb5f7c0a50c28ec34b4fce251252f3e
ajongs <wonjong1030@naver.com>
2023-01-05 02:38:22 +0900

    fix(LottoResult): 생성자에서 resultMap을 초기화하여 NullpointerException을 해결함

--- src/main/java/kr/codesquad/domain/LottoResult.java
+++ src/main/java/kr/codesquad/domain/LottoResult.java
@@ -1,14 +1,17 @@
 package kr.codesquad.domain;
 
 
+import kr.codesquad.enums.Rank;
 import kr.codesquad.enums.Rank2;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class LottoResult {
     private Map<Rank2, Integer> resultMap;
 
     public LottoResult() {
+        resultMap = new HashMap<>();
         for (Rank2 value : Rank2.values()) {
             resultMap.put(value, 0);
         }
