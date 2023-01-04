a186003aca5694c35b8716e220e884defb7016cd
i5046821854 <o5046821854@gmail.com>
2023-01-04 10:35:29 +0900

    day3 - (3) 기계에 의해 생성되는 로또번호에 대하여, 난수 생성을 위한 ArrayList를 멤버 변수로 선언하고 생성자에서 값을 할당

--- src/main/java/kr/codesquad/impl/RandomLotto.java
+++ src/main/java/kr/codesquad/impl/RandomLotto.java
@@ -5,6 +5,14 @@ import kr.codesquad.templates.LottoTmpl;
 import java.util.ArrayList;
 
 public class RandomLotto implements LottoTmpl {
+
+    private ArrayList<Integer> allNums = new ArrayList<Integer>();
+
+    public RandomLotto(){
+        for(int i = 1; i < 46; i++)
+            allNums.add(i);
+    }
+
     @Override
     public void addLotto(ArrayList<Integer> lottoList) {
 
