72c8251a9729aecca115f08484db1ee1bdedcf30
ajongs <wonjong1030@naver.com>
2023-01-05 02:50:59 +0900

    refactor(Main): 리팩터링한 메소드를 사용해 실행하도록 main 메소드 수정

--- src/main/java/kr/codesquad/Main.java
+++ src/main/java/kr/codesquad/Main.java
@@ -1,6 +1,7 @@
 package kr.codesquad;
 
 import kr.codesquad.domain.Lotto;
+import kr.codesquad.domain.Money;
 import kr.codesquad.domain.WinningLotto;
 import kr.codesquad.enums.Rank2;
 import kr.codesquad.view.InputView;
@@ -10,9 +11,14 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
+        /*
         LottoService lottoService = new LottoService(new InputView(), new OutputView(), new Lotto(new NumberGenerator()));
         lottoService.buyLotto();
         lottoService.inputWinningNumber();
-        lottoService.calculateTotal();
+        lottoService.calculateTotal();*/
+        LottoController lottoController = new LottoController(new InputView(), new OutputView());
+        Money money = lottoController.inputMoney();
+        lottoController.issueLotto(money);
+        lottoController.printResult(lottoController.inputWinningLotto(), money);
     }
 }
