b23e47d0d2c67c633a81397c032f700d3d394897
pjs0418 <tony180322@gmail.com>
2023-01-04 10:49:05 +0900

    UserInput class 생성자 구현

--- src/main/java/kr/codesquad/UserInput.java
+++ src/main/java/kr/codesquad/UserInput.java
@@ -1,4 +1,12 @@
 package kr.codesquad;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class UserInput {
+    private final BufferedReader br;
+
+    public UserInput() {
+        this.br = new BufferedReader(new InputStreamReader(System.in));
+    }
 }
