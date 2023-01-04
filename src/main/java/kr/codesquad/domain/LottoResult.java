75bb2df3c35804a3fe50b680c3fff8cdaea7ba53
ajongs <wonjong1030@naver.com>
2023-01-05 02:48:36 +0900

    refactor(LottoResult): 일치 개수와 수익률을 메시지 포멧에 맞추는 기능을 OutputView에서 위임받음

--- src/main/java/kr/codesquad/domain/LottoResult.java
+++ src/main/java/kr/codesquad/domain/LottoResult.java
@@ -20,4 +20,44 @@ public class LottoResult {
     public void increaseCountOfRank(Rank2 rank) {
         resultMap.put(rank, resultMap.get(rank) + 1);
     }
+
+    public double calculateEarningRate(Money money) {
+        double earnMoney = calculateEarnedMoney();
+        return money.calculateEarningRate(earnMoney);
+    }
+
+    private long calculateEarnedMoney() {
+        long earnMoney = 0;
+        for (Rank2 rank : Rank2.values()) {
+            earnMoney += rank.getMoney() * resultMap.get(rank);
+        }
+        return earnMoney;
+    }
+
+    public String statisticToString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("당첨 통계\n");
+        sb.append("---------\n");
+
+        for (Rank2 rank : Rank2.values()) {
+            sb.append(matchDetailToString(rank, resultMap.get(rank)));
+        }
+        return sb.toString();
+    }
+
+    private String matchDetailToString(Rank2 rank, int count) {
+        if (rank == Rank2.SECOND) {
+            return rank.getScore() + "개 일치, 보너스 볼 일치(" + rank.getMoney() + "원)- " + count + "개\n";
+        }
+        return rank.getScore() + "개 일치 (" + rank.getMoney() + "원)- " + count + "개\n";
+    }
+
+    public String earnedRateToString(Money money) {
+        double rate = calculateEarningRate(money);
+        double rateOfIncome = Math.floor(rate * 100) / 100;
+        if (rate < 0) {
+            rateOfIncome = Math.ceil(rate * 100) / 100;
+        }
+        return "총 수익률은 " + rateOfIncome + "%입니다.\n";
+    }
 }
