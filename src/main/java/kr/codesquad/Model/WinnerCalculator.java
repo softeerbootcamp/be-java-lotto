5e74e900e382d02a6c5226771141ab56c5c17879
rentalhub <rentalhub@rentalhubui-MacBookPro.local>
2023-01-05 15:48:56 +0900

    fix updateResult method

--- src/main/java/kr/codesquad/Model/WinnerCalculator.java
+++ src/main/java/kr/codesquad/Model/WinnerCalculator.java
@@ -39,9 +39,10 @@ public class WinnerCalculator {
     }
 
     private void updateResult(Price price, User user){
-        if (price != null)
+        if (price != null) {
             updateWinnerCount(price);
             user.updateEarn(price);
+        }
     }
 
     public void updateWinnerCount(Price price) {
