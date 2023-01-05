d28416a1b691b5c7f0cabf4d7ac19dbccb023b08
i5046821854 <o5046821854@gmail.com>
2023-01-05 15:41:56 +0900

    day4 - (4) 콘솔창에서 유저가 입력하는 상황에 대한 Test Code 생성 완료

--- src/main/java/kr/codesquad/view/UserConsole.java
+++ src/main/java/kr/codesquad/view/UserConsole.java
@@ -1,5 +1,7 @@
 package kr.codesquad.view;
 
+import kr.codesquad.exception.InputFormatException;
+import kr.codesquad.exception.InputRangeException;
 import kr.codesquad.model.UserInfo;
 
 import java.util.ArrayList;
@@ -22,18 +24,24 @@ public class UserConsole {
         int purchasedPrice = 0;
         System.out.println("구입 금액을 입력해 주세요.");
         try{
-            purchasedPrice = scan.nextInt();
-        }catch (InputMismatchException e){
-            enterPurchasePrice();
-        }
+            purchasedPrice = scan.nextInt();}
+        catch (InputMismatchException e){
+            throw new InputFormatException("구입 금액의 형식은 정수형으로 작성하셔야 합니다");}
+        if(purchasedPrice < 1000)
+            throw new InputRangeException("구입 금액은 1000원 이상이어야 합니다.");
         return purchasedPrice;
     }
 
     public int enterSudongLottoNumber(){
         Scanner scan = new Scanner(System.in);
-
+        int numOfLottoSudong = 0;
         System.out.println("수동으로 구매할 로또 수를 입력해 주세요");
-        int numOfLottoSudong = scan.nextInt();
+        try{
+            numOfLottoSudong = scan.nextInt();}
+        catch (InputMismatchException e){
+            throw new InputFormatException("구입 금액의 형식은 정수형으로 작성하셔야 합니다");}
+        if(numOfLottoSudong < 0)
+            throw new InputRangeException("구입 금액은 0개 이상이어야 합니다");
         return numOfLottoSudong;
     }
 
@@ -41,14 +49,12 @@ public class UserConsole {
     public String enterSudongLottoList(){
         Scanner scan = new Scanner(System.in);
         System.out.println("수동으로 구매할 로또 번호를 입력해주세요.");
-        ArrayList<String> resultList = new ArrayList<>();
         String numStr = scan.nextLine();
         return numStr;
     }
 
     public String enterResultList(){
         Scanner scan = new Scanner(System.in);
-
         System.out.println("\n당첨 번호를 입력해 주세요.");
         String numStr = scan.nextLine();
         return numStr;
@@ -56,9 +62,14 @@ public class UserConsole {
 
     public int enterBonusNum(){
         Scanner scan = new Scanner(System.in);
-
+        int givenBonusNum = 0;
         System.out.println("보너스볼을 입력해 주세요.");
-        int givenBonusNum = scan.nextInt();
+        try{
+            givenBonusNum = scan.nextInt();}
+        catch (InputMismatchException e){
+            throw new InputFormatException("보너스볼은 정수형으로 입력하셔야 합니다");}
+        if(givenBonusNum <= 0 || givenBonusNum > 45)
+            throw new InputRangeException("보너스볼은 1이상 45이하의 자연수여야 합니다.");
         return givenBonusNum;
     }
 
--- src/test/java/UserConsoleTest.java
+++ src/test/java/UserConsoleTest.java
@@ -1,3 +1,5 @@
+import kr.codesquad.exception.InputFormatException;
+import kr.codesquad.exception.InputRangeException;
 import kr.codesquad.model.UserInfo;
 import kr.codesquad.view.UserConsole;
 import org.junit.jupiter.api.Assertions;
@@ -46,22 +48,33 @@ public class UserConsoleTest {
         assertEquals(purchasePrice, returnedNum);
     }
 
-/*    @DisplayName("구입 금액 입력 테스트 - 정수형으로 입력하지 않을 경우")
+    @DisplayName("구입 금액 입력 테스트 - 정수형으로 입력하지 않을 경우")
     @Test
     void enterPurchasePrice_NoInt_Test() {
 
         //given
-        String purchasePrice = "100"; //구입 금액
+        String purchasePrice = "hello"; //구입 금액
         InputStream in = new ByteArrayInputStream(purchasePrice.getBytes());
         System.setIn(in);
 
-        //when
-//        assertThrows()
-        int returnedNum = userConsole.enterPurchasePrice();
+        //then
+        assertThrows(InputFormatException.class, ()->userConsole.enterPurchasePrice());
+
+    }
+
+    @DisplayName("구입 금액 입력 테스트 - 범위를 초과한 입력")
+    @Test
+    void enterPurchasePrice_OutOfPossibleRange_Test() {
+
+        //given
+        int purchasePrice = -1234; //구입 금액
+        InputStream in = new ByteArrayInputStream(String.valueOf(purchasePrice).getBytes());
+        System.setIn(in);
 
         //then
-        assertEquals(purchasePrice, returnedNum);
-    } */
+        assertThrows(InputRangeException.class, ()->userConsole.enterPurchasePrice());
+
+    }
 
     @DisplayName("수동 로또 구매 수 입력 테스트 - 정상 동작")
     @Test
@@ -77,6 +90,32 @@ public class UserConsoleTest {
 
         //then
         assertEquals(numOfSudong, returnedNum);
+    }
+
+    @DisplayName("수동 로또 구매 수 입력 테스트 - 정수형이 아닐 경우")
+    @Test
+    void enterSudongLottoNumberTest_NoInt(){
+
+        //given
+        String numOfSudong = "hehe"; //수동 로또 구매 수
+        InputStream in = new ByteArrayInputStream(numOfSudong.getBytes());
+        System.setIn(in);
+
+        //then
+        assertThrows(InputFormatException.class, ()->userConsole.enterSudongLottoNumber());
+    }
+
+    @DisplayName("수동 로또 구매 수 입력 테스트 - 음수 입력 시")
+    @Test
+    void enterSudongLottoNumberTest_EnterMinus(){
+
+        //given
+        int numOfSudong = -123; //수동 로또 구매 수
+        InputStream in = new ByteArrayInputStream(String.valueOf(numOfSudong).getBytes());
+        System.setIn(in);
+
+        //then
+        assertThrows(InputRangeException.class, ()->userConsole.enterSudongLottoNumber());
     }
 
     @DisplayName("수동 로또 리스트 입력 테스트 - 정상 동작")
@@ -124,4 +163,30 @@ public class UserConsoleTest {
         assertEquals(returnedBonus, bonus);
     }
 
+    @DisplayName("보너스볼 입력 테스트 - 정수형이 아닐 경우")
+    @Test
+    void enterBonusNumTest_NoInt(){
+
+        //given
+        String bonus = "hello";
+        InputStream in = new ByteArrayInputStream(String.valueOf(bonus).getBytes());
+        System.setIn(in);
+
+        //then
+        assertThrows(InputFormatException.class, ()-> userConsole.enterBonusNum());
+
+    }
+
+    @DisplayName("보너스볼 입력 테스트 - 유효 로또 번호가 아닐 경우")
+    @Test
+    void enterBonusNumTest_NoPossibleNumber(){
+
+        //given
+        int bonus = 456;
+        InputStream in = new ByteArrayInputStream(String.valueOf(bonus).getBytes());
+        System.setIn(in);
+
+        //then
+        assertThrows(InputRangeException.class, ()-> userConsole.enterBonusNum());
+    }
 }
