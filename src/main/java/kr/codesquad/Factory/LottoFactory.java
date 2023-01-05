2e7c8fdef18a4c540b15376a3b9d3b2dad4bcb20
River-Mt <rivermt1998@gmail.com>
2023-01-05 11:43:05 +0900

    Feat: add RandomLottoFactory - 로또번호 자동생성

--- src/main/java/kr/codesquad/Factory/LottoFactory.java
+++ src/main/java/kr/codesquad/Factory/LottoFactory.java
@@ -3,5 +3,5 @@ package kr.codesquad.Factory;
 import java.util.List;
 
 public interface LottoFactory {
-    List<Integer> getLottoNumbers();
+    List<Integer> generateLottoNumbers();
 }
---
+++ src/main/java/kr/codesquad/Factory/RandomLottoFactory.java
@@ -0,0 +1,30 @@
+package kr.codesquad.Factory;
+
+import kr.codesquad.domain.Lottos;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.IntStream;
+
+public class RandomLottoFactory implements LottoFactory{
+
+    private static final int LOTTO_MIN_NUMBER = 1;
+    private static final int LOTTO_MAX_NUMBER = 45;
+    private static final int LOTTO_MIN_IDX = 0;
+    private static final int LOTTO_SIZE = 6;
+
+    private static final List<Integer> lottoNumbers = new ArrayList<>();
+
+    @Override
+    public List<Integer> generateLottoNumbers() {
+        initLottoNumbers();
+        Collections.shuffle(lottoNumbers);
+        return lottoNumbers.subList(LOTTO_MIN_IDX, LOTTO_SIZE);
+    }
+
+    public void initLottoNumbers() {
+        IntStream.rangeClosed(LOTTO_MIN_NUMBER, LOTTO_MAX_NUMBER)
+                .forEach(lottoNumbers::add);
+    }
+}
