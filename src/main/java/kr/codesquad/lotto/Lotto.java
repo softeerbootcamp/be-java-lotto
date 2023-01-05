a9922f0b2dda6b3b266db67fb0c786a39d2b20ff
solchan98 <dev.sol0427@gmail.com>
2023-01-05 14:42:41 +0900

    feat: 로또 생성 시, 서로 다른 번호 6개가 입력되었는지에 예외처리 부분 추가

--- src/main/java/kr/codesquad/lotto/Lotto.java
+++ src/main/java/kr/codesquad/lotto/Lotto.java
@@ -6,16 +6,19 @@ import java.util.stream.Collectors;
 
 public class Lotto {
 
-    private final Set<LottoNumber> numberSet;
+    private final Set<LottoNumber> numbers;
 
-    public Lotto(Set<LottoNumber> numberSet) {
-        this.numberSet = numberSet;
+    public Lotto(Set<LottoNumber> numbers) {
+        if (numbers.size() < 6) {
+            throw new IllegalArgumentException("로또 번호는 서로 다른 6개의 번호로 구성되어야 합니다.");
+        }
+        this.numbers = numbers;
     }
 
     public int countOfMatch(Lotto lotto) {
         int cnt = 0;
 
-        for (LottoNumber lottoNumber: numberSet) {
+        for (LottoNumber lottoNumber: numbers) {
             if (lotto.contains(lottoNumber)) cnt++;
         }
 
@@ -23,11 +26,11 @@ public class Lotto {
     }
 
     public boolean contains(LottoNumber lottoNumber) {
-        return this.numberSet.contains(lottoNumber);
+        return this.numbers.contains(lottoNumber);
     }
 
     public void print() {
-        List<Integer> numberList = numberSet.stream().map(LottoNumber::getNumber).collect(Collectors.toList());
+        List<Integer> numberList = numbers.stream().map(LottoNumber::getNumber).collect(Collectors.toList());
         System.out.println(numberList);
     }
 }
