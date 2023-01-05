4d185cf5a1fafcb40736cce63e5307f454097fc9
Toa <ld980105@snu.ac.kr>
2023-01-05 17:09:03 +0900

    feat : LottoResult에 결과를 출력하는 함수 추가

--- src/main/java/kr/codesquad/LottoService/LottoResult.java
+++ src/main/java/kr/codesquad/LottoService/LottoResult.java
@@ -1,18 +1,16 @@
 package kr.codesquad.LottoService;
 
-import kr.codesquad.Rank;
-
 import java.util.HashMap;
 import java.util.Map;
 
 public class LottoResult {
     private Map<Rank, Integer> result = new HashMap<>();
-
     public LottoResult(){
         for (Rank value : Rank.values()){
             result.put(value, 0);
         }
     }
+
     public void putRank(Rank rank){
         result.put(rank, result.get(rank) + 1);}
 
@@ -26,6 +24,17 @@ public class LottoResult {
             totalPrize += rank.prize(result.get(rank));
         }
         return totalPrize;
+    }
+
+    public void printResult(){
+        System.out.println("\n당첨 통계");
+        System.out.println("---------");
+        for (Rank rank : Rank.values()){
+            if (rank == Rank.MISS) continue;
+            System.out.print(rank.getCountOfMatch() + "개 일치");
+            if (rank == Rank.SECOND) System.out.print(", 보너스 볼 일치");
+            System.out.printf(" (%d원)- %d개\n",rank.getWinningMoney(), result.get(rank));
+        }
     }
 }
 
