5b89083e11e2c3cdeb549fb7b3eff5d31c04f9aa
pjs0418 <tony180322@gmail.com>
2023-01-05 17:51:09 +0900

    CustomException 에러 메시지 출력 부분 삭제

--- src/main/java/kr/codesquad/exception/CustomException.java
+++ src/main/java/kr/codesquad/exception/CustomException.java
@@ -3,6 +3,5 @@ package kr.codesquad.exception;
 public class CustomException extends RuntimeException{
     public CustomException(String message) {
         super(message);
-        System.out.println(message);
     }
 }
