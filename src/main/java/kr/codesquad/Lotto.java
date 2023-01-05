c61760226cc88fb11d2e66d2401e9555c681dbc9
pjs0418 <tony180322@gmail.com>
2023-01-05 15:21:04 +0900

    Lotto 생성자 구현

--- src/main/java/kr/codesquad/Lotto.java
+++ src/main/java/kr/codesquad/Lotto.java
@@ -1,13 +1,15 @@
 package kr.codesquad;
 
-import lombok.RequiredArgsConstructor;
-
 import java.util.List;
 
-@RequiredArgsConstructor
 public abstract class Lotto {
     private final List<List<Integer>> lottoList;
     private final Lotto nextLotto;
+
+    public Lotto(List<List<Integer>> lottoList, Lotto nextLotto) {
+        this.lottoList = lottoList;
+        this.nextLotto = nextLotto;
+    }
 
     public List<List<Integer>> getLottoList() {
         return nextLotto == null ? lottoList : nextLotto.getLottoList();
