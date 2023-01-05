e440a6f2d5c7b011b79f381c830d2aea7b27ba9d
roopre <benny1020@naver.com>
2023-01-05 15:47:44 +0900

    refactor: LottoGame에 뭉쳐있던 로직들을 LottoGenerateStrategy와 LottoIOManger로 분리

--- src/main/java/kr/codesquad/LottoGame.java
+++ src/main/java/kr/codesquad/LottoGame.java
@@ -1,20 +1,23 @@
 package kr.codesquad;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
+import java.util.Map;
 
 public class LottoGame {
 
+	private final LottoIOManager lottoIOManager;
+
+	private final Map<String, LottoGeneratorStrategy> lottoGeneratorStrategies;
+
+	private final LottoGenerator lottoGenerator;
+
 	private final static int LOTTO_PRICE = 1000;
 
-	public List<Integer> lottoNumbers;
-
-	public LottoGame() {
-		lottoNumbers = new ArrayList<>();
-		for (int i = 1; i <= 45; i++) {
-			lottoNumbers.add(i);
-		}
+	public LottoGame(LottoIOManager lottoIOManager, LottoGenerator lottoGenerator,
+		Map<String, LottoGeneratorStrategy> lottoGeneratorStrategies) {
+		this.lottoGeneratorStrategies = lottoGeneratorStrategies;
+		this.lottoIOManager = lottoIOManager;
+		this.lottoGenerator = lottoGenerator;
 	}
 
 	public void compareEachLotto(Lotto purchasedLotto, WinningLotto winningLotto, LottoResult lottoResult) {
@@ -25,74 +28,47 @@ public class LottoGame {
 	public LottoResult checkMyLotto(List<Lotto> purchasedLottoList, WinningLotto winningLotto) {
 		LottoResult lottoResult = new LottoResult();
 		for (Lotto lotto : purchasedLottoList) {
-			System.out.println(lotto);
 			compareEachLotto(lotto, winningLotto, lottoResult);
 		}
 
 		return lottoResult;
 	}
 
-
-	public int getPurchaseAmount() {
-		Scanner scanner = new Scanner(System.in);
-		System.out.println("구입금액을 입력해 주세요.");
-
-		return scanner.nextInt();
-	}
-
 	public int getLottoCount(int purchaseAmount) {
 		return purchaseAmount / LOTTO_PRICE;
 	}
 
-	public int getManualLottoCount() {
-		Scanner scanner = new Scanner(System.in);
-		System.out.println("수동으로 구매할 로또 수 를 입력해 주세요");
-		return scanner.nextInt();
-	}
-
-	public ArrayList<Lotto> buyLottoList() {
+	public List<Lotto> buy() {
 		// 금액 입력받기
-		int purchaseAmount = getPurchaseAmount();
+		int purchaseAmount = lottoIOManager.readPurchaseAmount();
 		// 수동 개수 입력
-		int manualLottoCount = getManualLottoCount();
+		int manualLottoCount = lottoIOManager.readManualLottoCount();
 		// 자동 개수
 		int autoLottoCount = getLottoCount(purchaseAmount) - manualLottoCount;
-		ArrayList<Lotto> lottoList = new ArrayList<Lotto>();
+		// 로또 발급
+		List<Lotto> lottos = generateLottos(manualLottoCount, autoLottoCount);
+		// 발급 결과 출력
+		printLottosInfo(manualLottoCount, autoLottoCount, lottos);
 
-		// 수동
-		generateLottoList(lottoList, manualLottoCount, new ManualLottoGenerator());
+		return lottos;
+	}
+
+	public void printLottosInfo(int manualLottoCount, int autoLottoCount, List<Lotto> lottos) {
+		lottoIOManager.printLottoCount(manualLottoCount, autoLottoCount);
+		lottoIOManager.printLottos(lottos);
+	}
+
+	public List<Lotto> generateLottos(int manualLottoCount, int autoLottoCount) {
+		lottoGenerator.setLottoGeneratorStrategy(lottoGeneratorStrategies.get("MANUAL"));
+		List<Lotto> lottos = lottoGenerator.generate(manualLottoCount);
 		// 자동
-		generateLottoList(lottoList, autoLottoCount, new AutoLottoGenerator());
-
-		System.out.println("수동으로 " + manualLottoCount + "장 자동으로 " + autoLottoCount + "장 구매했숩니다");
-
-		for (Lotto lotto : lottoList) {
-			System.out.println(lotto);
-		}
-
-		return lottoList;
-	}
-
-	public void generateLottoList(List<Lotto> lottoList, int lottoCount, LottoGenerator lottoGenerator) {
-		lottoList.addAll(lottoGenerator.generate(lottoCount));
-	}
-
-	public int getBonusBall() {
-		System.out.println("보너스 볼을 입력해주세요.");
-		Scanner scanner = new Scanner(System.in);
-
-		return scanner.nextInt();
+		lottoGenerator.setLottoGeneratorStrategy(lottoGeneratorStrategies.get("AUTO"));
+		lottos.addAll(lottoGenerator.generate(autoLottoCount));
+		return lottos;
 	}
 
 	public WinningLotto createWinningLottery() {
-		System.out.println("당첨 번호를 입력해주세요");
-		Scanner scanner = new Scanner(System.in);
-		String numbers = scanner.nextLine();
-		List<Integer> lottoNumbers = new ArrayList<Integer>();
-		for (String number : numbers.split(", ")) {
-			lottoNumbers.add(Integer.parseInt(number));
-		}
-
-		return WinningLotto.of(Lotto.of(lottoNumbers), getBonusBall());
+		return lottoIOManager.readWinningLotto();
 	}
+
 }
