976f56c2dfb49b0d0db22e58bcd03cf51e73fef0
i5046821854 <o5046821854@gmail.com>
2023-01-05 17:33:40 +0900

    day4 - (8) ConsoleHandler 단에서 나타날 수 있는 Exception에 관한 테스트 클래스 작성

---
+++ src/test/java/ConsoleHandlerTest.java
@@ -0,0 +1,69 @@
+import kr.codesquad.controller.ConsoleHandler;
+import kr.codesquad.exception.InputCountException;
+import kr.codesquad.model.UserInfo;
+import org.junit.jupiter.api.*;
+
+import java.io.*;
+
+public class ConsoleHandlerTest {
+
+    private UserInfo user;
+    private ConsoleHandler consoleHandler;
+    private OutputStream out;
+
+    @BeforeEach
+    void testSetUp(){
+        user = new UserInfo();
+        consoleHandler = new ConsoleHandler(user);
+        out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
+    }
+
+    protected void systemIn(String input){
+        System.setIn(new ByteArrayInputStream(input.getBytes()));
+    }
+
+    @DisplayName("수동 구매 로직 테스트 - 이상한 형태로 입력 시")
+    @Test
+    void sudongLottoPurchase_InvalidFormat(){
+
+        //given
+        String lottoStr = "a,b,c,d,e,1";
+        InputStream in = new ByteArrayInputStream(lottoStr.getBytes());
+        System.setIn(in);
+
+        //then
+        Assertions.assertThrows(InputCountException.class, ()->consoleHandler.enterSudongLottoList());
+
+    }
+
+    @DisplayName("수동 구매 로직 테스트 - 중복된 값 입력 시")
+    @Test
+    void sudongLottoPurchase_duplicatedVal(){
+
+        //given
+        String lottoStr = "1,1,2,3,4,5";
+        InputStream in = new ByteArrayInputStream(lottoStr.getBytes());
+        System.setIn(in);
+
+        //then
+        Assertions.assertThrows(InputCountException.class, ()->consoleHandler.enterSudongLottoList());
+
+    }
+
+    @DisplayName("수동 구매 로직 테스트 - 개수 초과 시")
+    @Test
+    void sudongLottoPurchase_moreThan6Nums(){
+
+        //given
+        String lottoStr = "1,2,3,4,5,6,7";
+        InputStream in = new ByteArrayInputStream(lottoStr.getBytes());
+        System.setIn(in);
+
+        //then
+        Assertions.assertThrows(InputCountException.class, ()->consoleHandler.enterSudongLottoList());
+
+    }
+
+
+}
