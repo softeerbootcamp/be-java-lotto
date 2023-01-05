e9ef0727a425c0754d372911de54e623048e4f47
pjs0418 <tony180322@gmail.com>
2023-01-05 15:48:17 +0900

    로또 정보 출력하도록 Main 수정

--- src/main/java/kr/codesquad/Main.java
+++ src/main/java/kr/codesquad/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
         LottoMachine lm = new LottoMachine(1000);
         Lotto lotto = lm.buyLotto();
+        lm.printLottoList(lotto.getLottoList());
 
         LottoChecker lc = new LottoChecker();
         lc.checkLotto(lotto);
