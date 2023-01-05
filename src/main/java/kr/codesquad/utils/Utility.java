73f6775835e1563b3da8ed03097f178958be89e2
macbook16 <macbook16@macbook16ui-noteubug.local>
2023-01-05 15:24:06 +0900

    입력받은 돈의 금액에 따라 예외 처리해주는 메서드 추가

--- src/main/java/kr/codesquad/utils/Utility.java
+++ src/main/java/kr/codesquad/utils/Utility.java
@@ -12,11 +12,21 @@ public class Utility {
         if(number > 45) throw new CustomException(ErrorCode.OUT_OF_RANGE);
     }
 
+    public static void checkAmountOfMoney(int amountOfMoney){
+        if(amountOfMoney < 0) throw new CustomException(ErrorCode.NEGATIVE_NUMBER_ERROR);
+        else if(amountOfMoney < 1000) throw new CustomException(ErrorCode.MONEY_NOT_ENOUGH);
+    }
+
     public static ArrayList<Integer> checkListContainsNumber(ArrayList<Integer> list,Integer number){
         checkNumberError(number);
         if(list.contains(number)) throw new CustomException(ErrorCode.NUMBER_ALREADY_EXIST);
         list.add(Integer.valueOf(number));
         return list;
+    }
+
+    public static ArrayList<ArrayList<Integer>> mergeLists(ArrayList<ArrayList<Integer>> manualLottos, ArrayList<ArrayList<Integer>> lottos){
+        manualLottos.addAll(lottos);
+        return manualLottos;
     }
 
     public static  ArrayList<Integer> getListFromInputBySplit(String inputNumbers){
