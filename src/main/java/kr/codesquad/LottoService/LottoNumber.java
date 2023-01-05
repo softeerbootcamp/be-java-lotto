e7140d331b1faea5e18c6844445460a41ff91ade
Toa <ld980105@snu.ac.kr>
2023-01-05 17:08:38 +0900

    feat : LottoNumber에 equals와 hashCode 메서드 추가

--- src/main/java/kr/codesquad/LottoService/LottoNumber.java
+++ src/main/java/kr/codesquad/LottoService/LottoNumber.java
@@ -1,10 +1,11 @@
 package kr.codesquad.LottoService;
 
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
 
-public class LottoNumber {
+public class LottoNumber implements Comparable<LottoNumber> {
     private static final Map<Integer, LottoNumber> lottoNos = new HashMap<>();
     private static final int LOTTO_START_NUMBER = 1;
     private static final int LOTTO_END_NUMBER = 45;
@@ -26,12 +27,12 @@ public class LottoNumber {
 
     static LottoNumber of(String value) {
         if (Objects.isNull(value)) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("비어있는 로또넘버가 존재합니다.");
         }
         try {
             return lottoNos.get(Integer.parseInt(value.trim()));
         }
-        catch (Exception e){
+        catch (NumberFormatException e){
             throw new IllegalArgumentException("로또 인자로는 숫자만 가능합니다.");
         }
     }
@@ -39,4 +40,26 @@ public class LottoNumber {
         return new LottoNumber(number);
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        LottoNumber that = (LottoNumber) o;
+        return no == that.no;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(no);
+    }
+
+    @Override
+    public String toString() {
+        return String.valueOf(no);
+    }
+
+    @Override
+    public int compareTo(LottoNumber o) {
+        return this.no - o.no;
+    }
 }
