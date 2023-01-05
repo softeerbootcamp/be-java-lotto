4a08c38b0c97bc66733939a72a425026c68537e7
Toa <ld980105@snu.ac.kr>
2023-01-05 17:30:24 +0900

    refactor : App에서 수동, 자동 로또를 리턴하는 것을 LottoStore 내부로 옮김

--- src/main/java/kr/codesquad/App.java
+++ src/main/java/kr/codesquad/App.java
@@ -15,11 +15,7 @@ public class App {
         User user = new User(ioManager.inputMoney());
         LottoStore lottoStore = new LottoStore(user, new LottoFactory(ioManager), ioManager);
         int manualLottoCount = ioManager.inputManualLottoCount();
-        int autoLottoCount = user.countOfBuying() - manualLottoCount;
-        user.setLottos(lottoStore.issueLotto(manualLottoCount));
-        ioManager.printLottoCount(manualLottoCount, autoLottoCount);
-        ioManager.printLottos(user.getLottos());
-        LottoGame lottoGame = new LottoGame(user.getLottos());
+        LottoGame lottoGame = new LottoGame(lottoStore.issueLotto(manualLottoCount));
         LottoResult result = lottoGame.match(lottoStore.issueWinningLotto());
         result.printResult();
         ioManager.printEarningRate((result.getPrize()-user.getMoney())/(float)user.getMoney());
--- src/main/java/kr/codesquad/LottoService/LottoStore.java
+++ src/main/java/kr/codesquad/LottoService/LottoStore.java
@@ -28,6 +28,8 @@ public class LottoStore {
         for (int i = 0 ; i < automaticCount ; i++) {
             lottos.add(lottoFactory.generateLotto("automatic"));
         }
+        ioManager.printLottoCount(automaticCount, manualLottoCount);
+        ioManager.printLottos(lottos);
         return lottos;
     }
 
