ec360d7cc3535d961338c3db0cdd358e26d95255
rohsik2 <rohsik2@gmail.com>
2023-01-05 11:19:43 +0900

    feat(WindowManager): 화면을 담당하는 ManagerClass 작성

--- src/main/java/kr/codesquad/UI/WindowManager.java
+++ src/main/java/kr/codesquad/UI/WindowManager.java
@@ -1,2 +1,32 @@
-package kr.codesquad.UI;public class WindowManager {
+package kr.codesquad.UI;
+
+import java.awt.*;
+
+public class WindowManager extends Frame{
+
+    private Panel currentPanel;
+
+    public WindowManager (){
+        super();
+        setSize(200,360);
+        setLayout(null);
+        setVisible(true);
+        setTitle("로또 시뮬레이터");
+
+        currentPanel = new MoneyInputView(this);
+        add(currentPanel);
+        currentPanel.setVisible(true);
+    }
+
+    public void setPanel(Panel nextPanel){
+        currentPanel.setVisible(false);
+        remove(currentPanel);
+        currentPanel = nextPanel;
+        add(currentPanel);
+        nextPanel.setVisible(true);
+    }
+
+    public static void main(String[] args){
+        WindowManager manager = new WindowManager();
+    }
 }
