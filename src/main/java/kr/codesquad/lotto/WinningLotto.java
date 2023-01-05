7142521b524f95f77a371c4efe4fe0ce1341e5f9
solchan98 <dev.sol0427@gmail.com>
2023-01-05 16:06:43 +0900

    refactor: 로또 보너스 체크 부분 로직 수정

--- src/main/java/kr/codesquad/lotto/WinningLotto.java
+++ src/main/java/kr/codesquad/lotto/WinningLotto.java
@@ -14,7 +14,8 @@ public class WinningLotto {
 
     public Rank match(Lotto lotto) {
         int countOfMatch = this.lotto.countOfMatch(lotto);
-        boolean matchBonus = this.lotto.contains(bonus);
+        boolean matchBonus = lotto.contains(bonus);
+        System.out.println(matchBonus);
         return Rank.valueOf(countOfMatch, matchBonus);
     }
 }
