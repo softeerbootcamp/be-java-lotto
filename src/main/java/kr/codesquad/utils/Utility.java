58dd578caa07809afb87f03e0fac291fd6a0b13f
macbook16 <macbook16@macbook16ui-noteubug.local>
2023-01-05 12:47:39 +0900

    입력된 숫자에 대한 예외 처리
    checkNumberError 메서드를 통해 해당 숫자의 범위 체크, checkListContainsNumber 메서드를 통해 중복 체크

--- src/main/java/kr/codesquad/utils/Utility.java
+++ src/main/java/kr/codesquad/utils/Utility.java
@@ -7,11 +7,23 @@ import java.util.ArrayList;
 
 public class Utility {
 
+    public static void checkNumberError(int number){
+        if(number < 0) throw new CustomException(ErrorCode.NEGATIVE_NUMBER_ERROR);
+        if(number > 45) throw new CustomException(ErrorCode.OUT_OF_RANGE);
+    }
+
+    public static ArrayList<Integer> checkListContainsNumber(ArrayList<Integer> list,Integer number){
+        checkNumberError(number);
+        if(list.contains(number)) throw new CustomException(ErrorCode.NUMBER_ALREADY_EXIST);
+        list.add(Integer.valueOf(number));
+        return list;
+    }
+
     public static  ArrayList<Integer> getListFromInputBySplit(String inputNumbers){
         ArrayList<Integer> resList = new ArrayList<>();
         if(inputNumbers.split(", ").length != 6) throw new CustomException(ErrorCode.ILLEGAL_LOTTO_NUM_COUNT);
         for(String input : inputNumbers.split(", ")){
-            resList.add(Integer.valueOf(input));
+            resList = checkListContainsNumber(resList,Integer.valueOf(input));
         }
         return resList;
     }
