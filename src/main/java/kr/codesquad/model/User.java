c7e7f544f31dc18447c12008c0f5d512313ef23b
Soomin-Lim <augustth511@gmail.com>
2023-01-05 15:51:26 +0900

    feat: User 클래스에 purchaseMoney, manualLottoCount, autoLottoCount 필드 생성
    - 각각 사용자의 로또 구입 금액, 수동으로 구매할 로또 개수, 자동으로 생성할 로또 개수를 저장
    - 생성자의 인자 수정
    - 사용자의 정보를 출력할 때 사용하는 getter 메서드 생성

--- src/main/java/kr/codesquad/model/User.java
+++ src/main/java/kr/codesquad/model/User.java
@@ -6,10 +6,28 @@ import java.util.Map;
 
 public class User {
 
+    private final int purchaseMoney;
+    private final int manualLottoCount;
+    private final int autoLottoCount;
     private final List<Lotto> lottos;
 
-    public User(List<Lotto> lottos) {
+    public User(int purchaseMoney, int manualLottoCount, int autoLottoCount, List<Lotto> lottos) {
+        this.purchaseMoney = purchaseMoney;
+        this.manualLottoCount = manualLottoCount;
+        this.autoLottoCount = autoLottoCount;
         this.lottos = lottos;
+    }
+
+    public int getPurchaseMoney() {
+        return purchaseMoney;
+    }
+
+    public int getManualLottoCount() {
+        return manualLottoCount;
+    }
+
+    public int getAutoLottoCount() {
+        return autoLottoCount;
     }
 
     public List<Lotto> getLottos() {
