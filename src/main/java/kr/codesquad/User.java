cb0bee58939f858ed2cb9f46272e57bcd264980e
Toa <ld980105@snu.ac.kr>
2023-01-05 17:29:45 +0900

    delete : User에서 LottoList 삭제

--- src/main/java/kr/codesquad/User.java
+++ src/main/java/kr/codesquad/User.java
@@ -9,27 +9,16 @@ import java.util.List;
 
 public class User {
     private static final int MONEY_PER_LOTTO = 1_000;
-    private int money;
-    private List<Lotto> lottos;
-
+    private final int money;
     public User(int money){
         if (money < MONEY_PER_LOTTO || money % MONEY_PER_LOTTO != 0){
             throw new IllegalArgumentException("유효하지 않은 금액입니다. 1000원 단위로 입력해주세요.");
         }
         this.money = money;
-        this.lottos = new ArrayList<>();
     }
 
     public int countOfBuying(){
         return money / MONEY_PER_LOTTO;
-    }
-
-    public void setLottos(List<Lotto> lottos){
-        this.lottos = lottos;
-    }
-
-    public List<Lotto> getLottos() {
-        return lottos;
     }
 
     public int getMoney() {
