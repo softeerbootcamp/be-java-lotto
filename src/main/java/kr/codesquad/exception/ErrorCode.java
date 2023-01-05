089ed2d857af3699016f6788f14358750ddafdff
macbook16 <macbook16@macbook16ui-noteubug.local>
2023-01-05 12:22:24 +0900

    ErroCode에 에러 경우 하나 더 추가

--- src/main/java/kr/codesquad/exception/ErrorCode.java
+++ src/main/java/kr/codesquad/exception/ErrorCode.java
@@ -3,7 +3,8 @@ package kr.codesquad.exception;
 public enum ErrorCode {
 
     MONEY_NOT_ENOUGH("돈이 충분하지 않습니다."),
-    ILLEGAL_LOTTO_NUM_COUNT("로또 번호의 수는 6개여야 합니다");
+    ILLEGAL_LOTTO_NUM_COUNT("로또 번호의 수는 6개여야 합니다"),
+    ILLEGAL_INPUT_FORMAT("입력 양식을 다시 확인해주세요");
     private final String message;
     ErrorCode(String message) {
         this.message = message;
