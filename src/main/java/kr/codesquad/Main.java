edf1bd4f68cebf502b5b83052af5c297439baf95
waterfog9580 <waterfog9580@gmail.com>
2023-01-05 11:33:57 +0900

    :poop: 의존성 주입 구현 포기

--- src/main/java/kr/codesquad/Main.java
+++ src/main/java/kr/codesquad/Main.java
@@ -1,13 +1,9 @@
 package kr.codesquad;
 
-import kr.codesquad.domain.lotto.LottoShop;
-import kr.codesquad.io.Console;
-
 public class Main {
 
   public static void main(String[] args) {
-    App app = new App(new Console(), new LottoShop());
-    app.run();
+    new App().run();
   }
 
 }
