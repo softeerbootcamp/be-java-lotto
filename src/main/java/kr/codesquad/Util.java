4b36609da2d92a88d71a771222cffbeab2258117
pjs0418 <tony180322@gmail.com>
2023-01-05 15:58:08 +0900

    Util 생성자 구현

--- src/main/java/kr/codesquad/Util.java
+++ src/main/java/kr/codesquad/Util.java
@@ -1,4 +1,11 @@
 package kr.codesquad;
 
 public class Util {
+    /*
+     * Util class의 인스턴스화를 막기 위해 생성자를 private으로 설정
+     * Util class 내부에서도 생성자를 사용하지 못하도록 예외 처리
+     */
+    private Util() throws InstantiationException {
+        throw new InstantiationException();
+    }
 }
