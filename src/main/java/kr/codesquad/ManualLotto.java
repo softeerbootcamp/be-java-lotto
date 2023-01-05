92b59081cbd2bece814b56124b33dfbbb06dc55e
pjs0418 <tony180322@gmail.com>
2023-01-05 12:03:46 +0900

    ManualLotto 생성자 구현

--- src/main/java/kr/codesquad/ManualLotto.java
+++ src/main/java/kr/codesquad/ManualLotto.java
@@ -1,4 +1,9 @@
 package kr.codesquad;
 
-public class ManualLotto {
+import java.util.List;
+
+public class ManualLotto extends Lotto {
+    public ManualLotto(final List<List<Integer>> lottoList, final Lotto nextLotto) {
+        super(lottoList, nextLotto);
+    }
 }
