9534134d51914cd238325a06b219cc7af72004f5
YunjaeGong <llegokid@naver.com>
2023-01-05 12:56:57 +0900

    refactor: 보너스 번호 필드 직접 접근 가능하도록 변경

--- src/main/java/kr/codesquad/WinningLotto.java
+++ src/main/java/kr/codesquad/WinningLotto.java
@@ -7,7 +7,7 @@ public class WinningLotto extends Lotto {
      * 내가 선택한 번호들의 조합을 나타내는 클래스
      */
 
-    private final int bonus;
+    final int bonus;
 
     public WinningLotto(List<Integer> myLotto, int bonus) {
         super(myLotto);  // 실제 로또 번호
