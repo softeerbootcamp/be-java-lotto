9bbc8b1108009671d0f9a17a3136efe24597b00d
pjs0418 <tony180322@gmail.com>
2023-01-05 12:30:05 +0900

    수동 구매 로또 번호를 입력받는 inputManualLottoList 함수 구현

--- src/main/java/kr/codesquad/UserInput.java
+++ src/main/java/kr/codesquad/UserInput.java
@@ -25,6 +25,23 @@ public class UserInput {
         return manualLottoCount;
     }
 
+    public List<List<Integer>> inputManualLottoList(int manualLottoCount) {
+        List<List<Integer>> manualLottoList = new ArrayList<>(manualLottoCount);
+
+        System.out.println("수동으로 구매할 번호를 입력해 주세요.");
+        for(int i = 0;i < manualLottoCount;i++) {
+            String[] eachManualLotto = sc.nextLine().split(", ");
+
+            int eachManualLottoCount = eachManualLotto.length;
+            List<Integer> manualLotto = new ArrayList<>(eachManualLottoCount);
+            for (String eachManualLottoNum : eachManualLotto) manualLotto.add(Integer.parseInt(eachManualLottoNum));
+
+            manualLottoList.add(manualLotto);
+        }
+
+        return manualLottoList;
+    }
+
     public List<Integer> inputWinNum() {
         System.out.println("지난 주 당첨 번호를 입력해 주세요.");
         String[] winNum = sc.nextLine().split(", ");
