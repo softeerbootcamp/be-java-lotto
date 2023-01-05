3387594ef9bdbaa95de66623a45151ffdf7adfc5
rentalhub-mac78 <kksy2423@naver.com>
2023-01-05 18:19:35 +0900

    Feat: 수동티켓 예외처리
    - 구매 수동티켓이 0보다 작은경우
    - 구매 수동티켓이 0인경우(테스트)
    - 구매 수동티켓이 구매금액보다 큰경우

--- src/main/java/kr/codesquad/model/Ticket.java
+++ src/main/java/kr/codesquad/model/Ticket.java
@@ -1,6 +1,7 @@
 package kr.codesquad.model;
 
 import kr.codesquad.model.lotto.Lotto;
+import kr.codesquad.util.error.TicketErrorMessage;
 
 public class Ticket {
     private final int manualTicketsCount;
@@ -12,9 +13,28 @@ public class Ticket {
     }
 
     public static int purchaseAutoTickets(Money money, int  manualTicketsCount) {
+        valid(money, manualTicketsCount);
         int change = money.getPurchaseMoney() - manualTicketsCount * Lotto.TICKET_PER_PRICE;
         int autoTickets = change / Lotto.TICKET_PER_PRICE;
         return autoTickets;
+    }
+
+    private static void valid(Money money, int manualTicketsCount) {
+        if (!isTicketCountOverZero(manualTicketsCount)) {
+            throw new IllegalArgumentException(TicketErrorMessage.NOT_TICKET_COUNT_NEGATIVE);
+        }
+        if (!isAvailBuyManualTicket(money,manualTicketsCount)) {
+            throw new IllegalArgumentException(TicketErrorMessage.NOT_AVAIL_MANUAL_TICKET);
+        }
+
+    }
+
+    private static boolean isAvailBuyManualTicket(Money money, int manualTicketsCount) {
+        return money.getPurchaseMoney() > manualTicketsCount * Lotto.TICKET_PER_PRICE;
+    }
+
+    private static boolean isTicketCountOverZero(int ticketCount) {
+        return ticketCount >= 0;
     }
 
 
---
+++ src/main/java/kr/codesquad/util/error/TicketErrorMessage.java
@@ -0,0 +1,7 @@
+package kr.codesquad.util.error;
+
+public class TicketErrorMessage {
+    public static final String NOT_TICKET_COUNT_NEGATIVE = "구매 티켓은 0개 이상 구매가 가능합니다.";
+    public static final String NOT_AVAIL_MANUAL_TICKET = "구입 금액이 부족합니다.";
+
+}
