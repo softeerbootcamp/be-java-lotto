134c85d5d298538e7caa3d95b3b982f97a39b39f
pjs0418 <tony180322@gmail.com>
2023-01-05 16:07:24 +0900

    여러 개의 숫자를 입력했을 때 파싱해주는 함수 구현

--- src/main/java/kr/codesquad/Util.java
+++ src/main/java/kr/codesquad/Util.java
@@ -1,5 +1,9 @@
 package kr.codesquad;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Util {
     /*
      * Util class의 인스턴스화를 막기 위해 생성자를 private으로 설정
@@ -7,5 +11,16 @@ public class Util {
      */
     private Util() throws InstantiationException {
         throw new InstantiationException();
+    }
+
+    public static List<Integer> parseInputNumString() {
+        Scanner sc = new Scanner(System.in);
+        String[] parseNumList = sc.nextLine().split(", ");
+
+        int numCount = parseNumList.length;
+        List<Integer> numList = new ArrayList<>(numCount);
+        for (String parseNum : parseNumList) numList.add(Integer.parseInt(parseNum));
+
+        return numList;
     }
 }
