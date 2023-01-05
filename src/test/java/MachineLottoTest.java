8f7a6e46bc0eb91b05ee787d270ff3245c4aaf63
macbook16 <macbook16@macbook16ui-noteubug.local>
2023-01-05 17:21:30 +0900

    입력 받은 구매 금액의 범위에 관련해서 예외 처리
    구매 금액이 음수이거나 1000미만이면 CustomException이 발생하는지 테스트 구현

--- src/test/java/MachineLottoTest.java
+++ src/test/java/MachineLottoTest.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class MachineLottoTest {
+import kr.codesquad.exception.CustomException;
+import kr.codesquad.utils.Utility;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+
+class MachineLottoTest {
+    @DisplayName("구매 금액이 음수이면 예외 처리")
+    @Test
+    void checkMoneyIfNegative() {
+        assertThatThrownBy(() -> Utility.checkAmountOfMoney(-2000))
+                .isInstanceOf(CustomException.class);
+    }
+
+    @DisplayName("구매 금액이 1000 미만이면 예외 처리")
+    @Test
+    void checkMoneyIfLack() {
+        assertThatThrownBy(() -> Utility.checkAmountOfMoney(900))
+                .isInstanceOf(CustomException.class);
+    }
 }
