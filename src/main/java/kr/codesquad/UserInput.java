e66776b5e9155f28c5e089d32a2ce008026b4bd6
pjs0418 <tony180322@gmail.com>
2023-01-05 11:59:28 +0900

    수동으로 구매할 로또의 개수를 입력받는 inputManualLottoCount 함수 구현

--- src/main/java/kr/codesquad/UserInput.java
+++ src/main/java/kr/codesquad/UserInput.java
@@ -18,6 +18,13 @@ public class UserInput {
         return money;
     }
 
+    public int inputManualLottoCount() {
+        System.out.println("수동으로 구매할 로또 수를 입력해 주세요");
+        int manualLottoCount = sc.nextInt();
+        sc.nextLine();
+        return manualLottoCount;
+    }
+
     public List<Integer> inputWinNum() {
         System.out.println("지난 주 당첨 번호를 입력해 주세요.");
         String[] winNum = sc.nextLine().split(", ");
