5b5c400ff1255720be8e1c49cd7abac0bed93ee4
Toa <ld980105@snu.ac.kr>
2023-01-05 17:22:51 +0900

    delete : LottoResult의 getValue 메서드 삭제

--- src/main/java/kr/codesquad/LottoService/LottoResult.java
+++ src/main/java/kr/codesquad/LottoService/LottoResult.java
@@ -14,10 +14,6 @@ public class LottoResult {
     public void putRank(Rank rank){
         result.put(rank, result.get(rank) + 1);}
 
-    public Integer getValue(Rank rank) {
-        return result.get(rank);
-    }
-
     public Integer getPrize(){
         int totalPrize = 0;
         for (Rank rank : result.keySet()){
