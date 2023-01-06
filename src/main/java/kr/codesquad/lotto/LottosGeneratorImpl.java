adec71146058add46bc7285d97d4953404ced585
solchan98 <dev.sol0427@gmail.com>
2023-01-06 12:26:28 +0900

    feat: 수동 로또 발급을 위한 ManualLottoGenerator 생성 및 LottosGeneratorImpl 수정 (LottoMachin은 변경하지 않고 새로운 기능을 추가한 것이 포인트)

--- src/main/java/kr/codesquad/lotto/LottosGeneratorImpl.java
+++ src/main/java/kr/codesquad/lotto/LottosGeneratorImpl.java
@@ -1,16 +1,22 @@
 package kr.codesquad.lotto;
 
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class LottosGeneratorImpl implements LottosGenerator {
 
-    private final int priceOfLotto = 1_000;
-
     @Override
     public List<Lotto> generator(int money) {
+        int priceOfLotto = 1_000;
         int countOfLotto = money / priceOfLotto;
+        List<Lotto> manualLottos = ManualLottoGenerator.generatorLottos(countOfLotto);
+        countOfLotto -= manualLottos.size();
 
         List<Lotto> autoLottos = AutoLottoGenerator.generatorLottos(countOfLotto);
-        return autoLottos;
+
+        return Stream.concat(
+                manualLottos.stream(), autoLottos.stream())
+                .collect(Collectors.toList());
     }
 }
--- src/main/java/kr/codesquad/lotto/issue/LottoIssueStrategy.java
+++
@@ -1,9 +0,0 @@
-package kr.codesquad.lotto.issue;
-
-import kr.codesquad.lotto.Lotto;
-
-import java.util.List;
-
-public interface LottoIssueStrategy {
-    List<Lotto> issue(int cnt);
-}
