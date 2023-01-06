75789e99abbac8492db3727ec3a6bbbc03ca999c
River-Mt <rivermt1998@gmail.com>
2023-01-06 14:06:10 +0900

    Feat: add checkNumbers - 수동으로 생성할 로또 예외 처리 - 중복된 로또 번호가 있을 경우 - 로또 사이즈가 6이 아닐 경우

--- src/main/java/kr/codesquad/domain/lotto/factory/ManualLottoFactory.java
+++ src/main/java/kr/codesquad/domain/lotto/factory/ManualLottoFactory.java
@@ -12,24 +12,23 @@ public class ManualLottoFactory implements LottoFactory {
 
     @Override
     public List<LottoNumber> generateLottoNumbers() {
-        List<LottoNumber> lottoNumbers = convertToLottoNumber();
-        checkNumbers(lottoNumbers);
-        return lottoNumbers;
+        return convertToLottoNumber();
     }
 
     private List<LottoNumber> convertToLottoNumber() {
         List<Integer> lottoNumbers = InputView.inputLotto();
+        checkNumbers(lottoNumbers);
         return lottoNumbers.stream()
                 .map(LottoNumber::new)
                 .collect(Collectors.toList());
     }
 
-    private void checkNumbers(List<LottoNumber> inputNumbers) {
+    private void checkNumbers(List<Integer> inputNumbers) {
         if (inputNumbers.size() != COUNT) {
-            throw new IllegalArgumentException("로또 숫자를 6개 입력해 주세요.");
+            throw new RuntimeException("로또 숫자를 6개 입력해 주세요.");
         }
         if (inputNumbers.stream().distinct().count() != COUNT) {
-            throw new IllegalArgumentException("중복된 로또 번호가 있습니다.");
+            throw new RuntimeException("중복된 로또 번호가 있습니다.");
         }
     }
 
