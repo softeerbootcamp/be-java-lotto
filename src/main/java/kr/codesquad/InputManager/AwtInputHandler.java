c9cfda685da290389ae52b98acec57cd5af6109c
rohsik2 <rohsik2@gmail.com>
2023-01-06 12:52:42 +0900

    feat(AwtInputHandler): error message 출력기능 추가

--- src/main/java/kr/codesquad/InputManager/AwtInputHandler.java
+++ src/main/java/kr/codesquad/InputManager/AwtInputHandler.java
@@ -23,30 +23,53 @@ public class AwtInputHandler implements UserInputHandler{
     public int getMoney() {
         try {
             int money = Utility.parseIntWithRange(textField.getText(), 0, Integer.MAX_VALUE);
+            if(money% 1000 != 0){
+                throw new CustomException("1000단위로 돈을 입력해야 합니다.");
+            }
             return money;
         }
         catch(Exception e){
             errLabel.setText(e.getMessage());
+            throw e;
         }
-        return 0;
     }
 
     @Override
     public List<Integer> getSixLottoNumber() {
-        return Utility.str2IntListWithRange(textField.getText(), 1, 45);
+        try {
+            return Utility.str2IntListWithRange(textField.getText(), 1, 45);
+        }
+        catch(Exception e){
+            errLabel.setText(e.getMessage());
+            throw e;
+        }
     }
 
     @Override
     public int getBonusNumber(List<Integer> winningNumbers) {
-        int bonus = Utility.parseIntWithRange(this.bonus.getText(),1, 45);
-        if (winningNumbers.contains(bonus)) {
-            throw new CustomException("Bonus와 winningNumbers 중복발생");
+        try {
+            int bonus = Utility.parseIntWithRange(this.bonus.getText(), 1, 45);
+            if (winningNumbers.contains(bonus)) {
+                throw new CustomException("보너스와 당첨번호는 중복되면 안됩니다.");
+            }
+            return bonus;
         }
-        return bonus;
+        catch(Exception e){
+            errLabel.setText(e.getMessage());
+            throw e;
+        }
+
     }
 
     @Override
     public int getManualLottoAmount(int max) {
-        return Utility.parseIntWithRange(textField.getText(), 0, max);
+        try{
+            return Utility.parseIntWithRange(textField.getText(), 0, max);
+        }
+        catch(Exception e){
+            errLabel.setText(e.getMessage());
+            throw e;
+        }
+
     }
 }
