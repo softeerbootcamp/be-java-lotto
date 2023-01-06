cf6b59dc7582bbef13caa59e7efb3378e948fb3e
ajongs <wonjong1030@naver.com>
2023-01-06 14:55:46 +0900

    feat(Validator): 구입금액 유효성 검증 기능 추가

--- src/main/java/kr/codesquad/enums/ExceptionMessage.java
+++ src/main/java/kr/codesquad/enums/ExceptionMessage.java
@@ -1,7 +1,8 @@
 package kr.codesquad.enums;
 
 public enum ExceptionMessage {
-    INVALID_MONEY("구입금액은 1000원 단위이고, 0원 이상 입력가능 합니다."),
+    INVALID_MONEY_BOUNDARY("0원 이상 입력가능 합니다."),
+    INVALID_MONEY_COUNT("구입금액은 1000원 단위입니다."),
     INVALID_LOTTO_NUM_BOUNDARY("로또 번호는 1부터 45까지 입니다."),
     INVALID_NUM("숫자를 입력해주세요."),
     CANNOT_INPUT_NULL("아무것도 입력되지 않았습니다"),
--- src/main/java/kr/codesquad/util/Validator.java
+++ src/main/java/kr/codesquad/util/Validator.java
@@ -5,32 +5,42 @@ import kr.codesquad.exception.DefaultException;
 
 public class Validator {
 
-    public boolean checkLottoNumBoundary(int num) {
+    public static boolean checkLottoNumBoundary(int num) {
         if (num < 1 || num > 45) {
             throw new DefaultException(ExceptionMessage.INVALID_LOTTO_NUM_BOUNDARY);
         }
         return true;
     }
-    public boolean checkTransformabilityToNumber(String input) {
+    public static boolean checkTransformabilityToNumber(String input) {
         if (!input.matches("^[0-9]*")) {
             throw new DefaultException(ExceptionMessage.INVALID_NUM);
         }
         checkNotNull(input);
         return true;
     }
-    public boolean checkNotNull(String input) {
+    public static boolean checkNotNull(String input) {
         if (input.matches("^$")) {
             throw new DefaultException(ExceptionMessage.CANNOT_INPUT_NULL);
         }
         return true;
     }
-    public boolean checkLottoNumbers(String[] input) {
+    public static boolean checkLottoNumbers(String[] input) {
         if (input.length < 6) {
             throw new DefaultException(ExceptionMessage.INVALID_LOTTO_NUM_COUNT);
         }
         for (String s : input) {
             checkTransformabilityToNumber(s);
             checkLottoNumBoundary(Integer.parseInt(s));
+        }
+        return true;
+    }
+    public static boolean checkMoneyBoundary(String money) {
+        checkTransformabilityToNumber(money);
+        int intMoney = Integer.parseInt(money);
+        if (intMoney % 1000 != 0) {
+            throw new DefaultException(ExceptionMessage.INVALID_MONEY_COUNT);
+        } else if (intMoney < 1) {
+            throw new DefaultException(ExceptionMessage.INVALID_MONEY_BOUNDARY);
         }
         return true;
     }
