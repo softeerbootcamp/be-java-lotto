d2086dc9922b63fd8448193ae3045e31187ec212
Hoyoung Jung <honux77@gmail.com>
2023-01-06 15:16:48 +0900

    Revert "[Step 4] 노현욱 step4 구현일부 - 화면모양 구현 및 메소드 이름 정의 (#107)" (#124)
    This reverts commit e8f725cbf6add132092c6df2d48cbb37ecc4f865.

--- src/main/java/kr/codesquad/Exceptions/CustomException.java
+++
@@ -1,8 +0,0 @@
-package kr.codesquad.Exceptions;
-
-public class CustomException extends IllegalArgumentException{
-    public CustomException(String message){
-        super(message);
-        System.out.println("Error : " + message);
-    }
-}
