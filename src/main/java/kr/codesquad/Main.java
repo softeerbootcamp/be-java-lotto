bb8580f274083cce07a76cfd03c399a99eea1914
Toa <ld980105@snu.ac.kr>
2023-01-06 12:53:03 +0900

    refactor : Controller에서 WinningLotto를 받아서 바로 생성하도록 변경

--- src/main/java/kr/codesquad/Main.java
+++ src/main/java/kr/codesquad/Main.java
@@ -1,10 +1,13 @@
 package kr.codesquad;
 
 import kr.codesquad.controller.LottoGameController;
+import kr.codesquad.domain.generate.LottoGenerator;
+import kr.codesquad.domain.generate.LottoGeneratorImpl;
 
 public class Main {
     public static void main(String[] args) {
-        LottoGameController lottoGameController = new LottoGameController();
+        LottoGenerator lottoGenerator = new LottoGeneratorImpl();
+        LottoGameController lottoGameController = new LottoGameController(lottoGenerator);
         lottoGameController.play();
     }
 }
