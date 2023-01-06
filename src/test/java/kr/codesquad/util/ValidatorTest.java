57500611aeca1b2458d6829e374447a7a44e5158
ajongs <wonjong1030@naver.com>
2023-01-06 14:03:49 +0900

    test(ValidatorTest): 입력값 숫자변환 테스트 코드 추가

--- src/test/java/kr/codesquad/util/ValidatorTest.java
+++ src/test/java/kr/codesquad/util/ValidatorTest.java
@@ -29,4 +29,17 @@ class ValidatorTest {
         Validator validator = new Validator();
         assertThat(validator.checkLottoNumBoundary(1)).isEqualTo(1);
     }
+    @Test
+    void 숫자로_변환할_없는_문자입력() {
+        Validator validator = new Validator();
+        Throwable exception = assertThrows(RuntimeException.class, () -> {
+            validator.convertInputToNumber("문자입력");
+        });
+        assertEquals(ExceptionMessage.INVALID_NUM.getMessage(), exception.getMessage());
+    }
+    @Test
+    void 입력한_문자로_숫자_변환() {
+        Validator validator = new Validator();
+        assertThat(validator.convertInputToNumber("231231")).isEqualTo(231231);
+    }
 }
