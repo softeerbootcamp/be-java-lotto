0a597eb46bbd121655cd5daebc1e3de93a564f54
YunjaeGong <llegokid@naver.com>
2023-01-06 12:39:18 +0900

    refactor : LottoProcedure 기능 분리로 더 이상 필요하지 않은 메서드 삭제

--- src/main/java/kr/codesquad/LottoProcedure.java
+++ src/main/java/kr/codesquad/LottoProcedure.java
@@ -10,13 +10,8 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class LottoProcedure {
-    final static int LEAST_MATCH = 3;  // 로또 상금 수령을 위한 동일해야하는 최소 숫자의 개수
-    final static int LOTTO_LENGTH = 6;  // 로또 번호 조합의 길이
+
     final static Scanner sc = new Scanner(System.in);
-
-    static boolean matchBonus = false;  // 보너스 번호 존재하는지
-
-    static int[] matches = new int[7];  // 몇 개의 로또 번호가 일치하는지. matches[3] -> 3개 이상의 번호가 일치하는 경우를 셈
 
     private Lotto winningLotto;  // 실제 로또 번호
 
@@ -112,54 +107,6 @@ public class LottoProcedure {
                 .map(Integer::parseInt)
                 .sorted()
                 .collect(Collectors.toList());
-    }
-
-     void matchLottoSequences(final Lotto winning, List<Lotto> given) {
-        // TODO: 로또 매칭 담당하는 클래스로 분리
-        for(Lotto lotto: given) {
-            int cnt = matchLotto(winning, lotto);  // 실제 당첨번호와 생성된 로또 번호 1대1 매칭 통해 대응되는 수의 쌍 개수 반환
-            matches[cnt] += 1;
-        }
-    }
-
-    int matchLotto (final Lotto winning, Lotto given) {  // 실제 당첨번호(입력), 생성된 번호
-        // TODO: 로또 매칭 담당하는 클래스로 분리
-        int cnt = 0;
-        matchBonus = false;
-
-        // 정렬된 로또 번호 기준으로, 실제 로또 번호와 비교
-        for(Integer num : given.getLotto()) {  // TODO: Lotto의 getter가 필요한가
-            cnt += winning.contains(num) ? 1:0;
-        }
-
-        // 보너스 조건 - 5개가 맞고 보너스 번호도 맞을 때
-        if(cnt == 5 && given.contains(bonus)) {
-            // 보너스이면 -> 2등
-            matchBonus = true;
-        }
-
-        return cnt;  // 동일한 번호의 갯수
-    }
-
-    void printStatistics() {
-        // TODO: 로또 매칭 담당하는 클래스로 분리
-        long prizeTotal = 0;
-
-        System.out.println("\n당첨 통계");
-        System.out.println("---------");
-
-        // TODO: 이해 쉽게
-        for (int i = LEAST_MATCH; i <= LOTTO_LENGTH; ++i) {  // 최소 3개 이상, 6개 이하 매치
-            Prize prize = Prize.valueOf(matches[i] > 0?i:0, matchBonus);
-            prizeTotal += prize.calculatePrize(matches[i]); // 상금 총액
-
-            System.out.println(i + "개 일치 (" + prize.getWinningMoney() + "원)- " + matches[i] + "개");
-            // TODO: Prize가 Miss인 경우 WinningMoney = 0 반환되는 에러 해걸
-        }
-
-        double returnRate = money.returnRate(prizeTotal);
-
-        System.out.println("총 수익률은 " + String.format("%.2f", returnRate) + "%입니다.)");
     }
 
     public Lotto getWinningLotto() {
