fd3ae3e13df5688e8d2254ef3872f8a4f41b7dd9
Toa <ld980105@snu.ac.kr>
2023-01-08 18:28:10 +0900

    test : 문자열과 리스트가 중복되어 Lotto 생성자로 들어갈 때의 테스트케이스 추가

--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -6,8 +6,10 @@ import org.junit.jupiter.api.Test;
 import java.util.Arrays;
 
 import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class LottoTest {
+    public static final String DUPLICATE_ERROR_MESSAGE = "로또에는 서로 다른  6개의 숫자가 필요합니다.";
     @Test
     public void match_2등(){
         Lotto userLotto = Lotto.of(Arrays.asList(1, 2, 3, 4, 5, 6));
@@ -22,5 +24,19 @@ public class LottoTest {
         Lotto winningLotto = Lotto.of(Arrays.asList(1, 2, 3, 4, 5, 6));
         int result = userLotto.match(winningLotto);
         assertThat(result).isEqualTo(6);
+    }
+
+    @Test
+    public void create_문자열중복(){
+        Exception exception = assertThrows(IllegalArgumentException.class, ()->{
+            Lotto.ofComma("1,2,3,4,5,5");});
+        assertThat(exception.getMessage()).isEqualTo(DUPLICATE_ERROR_MESSAGE);
+    }
+
+    @Test
+    public void create_리스트중복(){
+        Exception exception = assertThrows(IllegalArgumentException.class,
+                () -> Lotto.of(Arrays.asList(1, 2, 3, 4, 5, 5)));
+        assertThat(exception.getMessage()).isEqualTo(DUPLICATE_ERROR_MESSAGE);
     }
 }
