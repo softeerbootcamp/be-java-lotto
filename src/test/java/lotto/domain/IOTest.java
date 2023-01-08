6147a517761817e4e391ae24975addeb09221b95
Toa <ld980105@snu.ac.kr>
2023-01-08 23:42:48 +0900

    feat : console input을 테스트하기 위해 IOTest 추상클래스 구현

---
+++ src/test/java/lotto/domain/IOTest.java
@@ -0,0 +1,9 @@
+package lotto.domain;
+
+import java.io.ByteArrayInputStream;
+
+public abstract class IOTest {
+    protected void systemIn(String input){
+        System.setIn(new ByteArrayInputStream(input.getBytes()));
+    }
+}
