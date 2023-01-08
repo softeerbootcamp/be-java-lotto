7a4139e23e619bc3e2766d3fc6d881a7611893a9
Toa <ld980105@snu.ac.kr>
2023-01-08 22:46:45 +0900

    refactor : input을 validate하는 함수를 추가

--- src/main/java/kr/codesquad/view/InputView.java
+++ src/main/java/kr/codesquad/view/InputView.java
@@ -6,25 +6,12 @@ public final class InputView {
     private static final Scanner SCANNER = new Scanner(System.in);
     public static int inputMoney() {
         System.out.println("구입금액을 입력해 주세요.");
-        try{
-            return Integer.parseInt(SCANNER.nextLine());
-        }
-        catch (NumberFormatException e){
-            throw new IllegalArgumentException("숫자로 입력해주세요.");
-        }
+        return validateInput(SCANNER.nextLine());
     }
 
     public static int inputManualLottoCount() {
         System.out.println("\n수동으로 구매할 로또 수를 입력해 주세요.");
-        try{
-            int manualLottoCount = Integer.parseInt(SCANNER.nextLine());
-            if (manualLottoCount < 0)
-                throw new IllegalArgumentException("음수는 입력할 수 없습니다.");
-            return manualLottoCount;
-        }
-        catch (NumberFormatException e){
-            throw new IllegalArgumentException("숫자로 입력해주세요.");
-        }
+        return validateInput(SCANNER.nextLine());
     }
 
     public static String inputWinningNumber() {
@@ -38,11 +25,15 @@ public final class InputView {
 
     public static int inputBonusNumber() {
         System.out.println("보너스 볼을 입력해 주세요.");
-        try{
-            return Integer.parseInt(SCANNER.nextLine());
+        return validateInput(SCANNER.nextLine());
+    }
+
+    public static int validateInput(String input) {
+        try {
+            return Integer.parseInt(input);
         }
         catch (NumberFormatException e){
-            throw new IllegalArgumentException("숫자로 입력해주세요.");
+            throw new IllegalArgumentException(input+"은 숫자가 아닙니다."+" 숫자로 입력해주세요.");
         }
     }
 }
