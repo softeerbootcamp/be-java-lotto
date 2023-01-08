d08dd18db20cd513f4ae4fa1a6762ced9a8699e0
Toa <ld980105@snu.ac.kr>
2023-01-08 18:51:45 +0900

    refactor : LottoTest 메서드 이름 변경

--- src/test/java/lotto/domain/LottoTest.java
+++ src/test/java/lotto/domain/LottoTest.java
@@ -11,7 +11,7 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 public class LottoTest {
     public static final String DUPLICATE_ERROR_MESSAGE = "로또에는 서로 다른  6개의 숫자가 필요합니다.";
     @Test
-    public void match_2등(){
+    public void testLottoMatch(){
         Lotto userLotto = Lotto.of(Arrays.asList(1, 2, 3, 4, 5, 6));
         Lotto winningLotto = Lotto.of(Arrays.asList(1, 2, 3, 4, 5, 7));
         int result = winningLotto.match(userLotto);
@@ -19,7 +19,7 @@ public class LottoTest {
     }
 
     @Test
-    public void create_문자열로또(){
+    public void testOfComma(){
         Lotto userLotto = Lotto.ofComma("1,2,3,4,5,6");
         Lotto winningLotto = Lotto.of(Arrays.asList(1, 2, 3, 4, 5, 6));
         int result = userLotto.match(winningLotto);
@@ -27,14 +27,14 @@ public class LottoTest {
     }
 
     @Test
-    public void create_문자열중복(){
+    public void testDuplicateNumberString(){
         Exception exception = assertThrows(IllegalArgumentException.class, ()->{
             Lotto.ofComma("1,2,3,4,5,5");});
         assertThat(exception.getMessage()).isEqualTo(DUPLICATE_ERROR_MESSAGE);
     }
 
     @Test
-    public void create_리스트중복(){
+    public void testDuplicateNumberList(){
         Exception exception = assertThrows(IllegalArgumentException.class,
                 () -> Lotto.of(Arrays.asList(1, 2, 3, 4, 5, 5)));
         assertThat(exception.getMessage()).isEqualTo(DUPLICATE_ERROR_MESSAGE);
