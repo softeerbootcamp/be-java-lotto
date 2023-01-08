b6d3ee777de821bde83fff50b7c94237a67ced95
Toa <ld980105@snu.ac.kr>
2023-01-09 01:36:24 +0900

    feat : 수동으로 로또를 생성할 때, 유효하지 않은 input에 대한 예외처리 로직 구현

--- src/main/java/kr/codesquad/domain/generate/ManualLottoGenerator.java
+++ src/main/java/kr/codesquad/domain/generate/ManualLottoGenerator.java
@@ -1,6 +1,8 @@
 package kr.codesquad.domain.generate;
 
 import kr.codesquad.domain.Lotto;
+import kr.codesquad.domain.exception.LottoNumberException;
+import kr.codesquad.domain.exception.NumberCountException;
 import kr.codesquad.view.InputView;
 
 import java.util.ArrayList;
@@ -16,6 +18,12 @@ public final class ManualLottoGenerator {
     }
 
     public static Lotto generateManual(){
-        return Lotto.ofComma(InputView.inputManualNumber());
+        while (true) {
+            try {
+                return Lotto.ofComma(InputView.inputManualNumber());
+            } catch (NumberCountException | LottoNumberException e) {
+                System.out.println(e.getMessage());
+            }
+        }
     }
 }
